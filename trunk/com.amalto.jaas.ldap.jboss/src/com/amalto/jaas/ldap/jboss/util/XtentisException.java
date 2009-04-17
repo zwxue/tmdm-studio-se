6cc1ae3ad19a8d8574d4cf1033c7b01f4b011d81
amchen <amchen@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2009-04-17 17:08:41 +0000

    init
    git-svn-id: http://talendforge.org/svn/mdm/trunk@27642 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ trunk/com.amalto.jaas.ldap.jboss/src/com/amalto/jaas/ldap/jboss/util/XtentisException.java
@@ -0,0 +1,40 @@
+package com.amalto.jaas.ldap.jboss.util;
+
+/**
+ * @author bgrieder
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+public class XtentisException extends Exception {
+
+	/**
+	 * 
+	 */
+	public XtentisException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	/**
+	 * @param message
+	 */
+	public XtentisException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+	/**
+	 * @param message
+	 * @param cause
+	 */
+	public XtentisException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+	/**
+	 * @param cause
+	 */
+	public XtentisException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+}
