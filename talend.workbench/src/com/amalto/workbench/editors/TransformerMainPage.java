c59931c4e023893341ef6078236f38e7398db6fe
scorreia <scorreia@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2009-05-19 18:00:19 +0000

    open a dialog to select the xpath
    git-svn-id: http://talendforge.org/svn/mdm/trunk/trunk@27747 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- talend.workbench/src/com/amalto/workbench/editors/TransformerMainPage.java
+++ talend.workbench/src/com/amalto/workbench/editors/TransformerMainPage.java
@@ -72,6 +72,7 @@ import org.eclipse.ui.forms.widgets.FormToolkit;
 import com.amalto.workbench.dialogs.PluginDetailsDialog;
 import com.amalto.workbench.dialogs.ProcessResultsDialog;
 import com.amalto.workbench.dialogs.SetupTransformerInputVariablesDialog;
+import com.amalto.workbench.dialogs.XpathSelectDialog;
 import com.amalto.workbench.models.Line;
 import com.amalto.workbench.providers.XObjectEditorInput;
 import com.amalto.workbench.utils.Util;
@@ -152,6 +153,7 @@ public class TransformerMainPage extends AMainPageV2 {
 	private Button disabledButton;
 	protected WSTransformerV2 transformer;
 	private Composite specsComposite;
+	private XpathSelectDialog xpathSelectDialog;
 	
 	java.util.List<Line> cacheList; //remember the setup transformerinputvariablesdialog's input list
 
@@ -385,7 +387,7 @@ public class TransformerMainPage extends AMainPageV2 {
             	};
             });
                         
-            
+          
             //make the Page window a DropTarget - we need to dispose it
             windowTarget = new DropTarget(this.getPartControl(), DND.DROP_MOVE);
             windowTarget.setTransfer(new Transfer[]{TextTransfer.getInstance()});
