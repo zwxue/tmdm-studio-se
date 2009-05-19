439ea5090962825be160e3e78c972a4c9de8a5f9
hshu <hshu@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2009-05-19 17:15:10 +0000

    move test method
    git-svn-id: http://talendforge.org/svn/mdm/trunk/trunk@27745 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- talend.webapp.v3.synchronization/src/com/amalto/webapp/v3/synchronization/dwr/SynchronizationPlanDWR.java
+++ talend.webapp.v3.synchronization/src/com/amalto/webapp/v3/synchronization/dwr/SynchronizationPlanDWR.java
@@ -107,17 +107,5 @@ public class SynchronizationPlanDWR {
 
 	}
 	
-	public static void main(String[] args) {
-		List list=new ArrayList();
-		list.add("0");
-		list.add("1");
-		list.add("2");
-		list.add("3");
-		List sublist=list.subList(1, 4);
-		for (int i = 0; i < sublist.size(); i++) {
-			System.out.println(sublist.get(i));
-		}
-		
-	}
 	
 }
