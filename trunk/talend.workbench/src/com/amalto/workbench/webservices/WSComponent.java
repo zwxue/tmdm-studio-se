6a13c66eebfa5db16f11ed615505773fc1c32e11
scorreia <scorreia@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2009-06-01 17:50:07 +0000

    get ImageDescriptor from ImageCache
    git-svn-id: http://talendforge.org/svn/mdm/trunk@27826 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- trunk/talend.workbench/src/com/amalto/workbench/webservices/WSComponent.java
+++ trunk/talend.workbench/src/com/amalto/workbench/webservices/WSComponent.java
@@ -6,8 +6,8 @@
 package com.amalto.workbench.webservices;
 
 
-import java.util.Map;
 import java.util.HashMap;
+import java.util.Map;
 
 public class WSComponent {
     private java.lang.String value;
