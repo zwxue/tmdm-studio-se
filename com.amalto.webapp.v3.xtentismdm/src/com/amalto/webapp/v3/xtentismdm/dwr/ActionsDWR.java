064685448ed2abe1fd136a88a2a484317212ab92
hshu <hshu@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2009-06-03 16:22:55 +0000

    add system objects filter on action menu
    git-svn-id: http://talendforge.org/svn/mdm/trunk/trunk@27840 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- com.amalto.webapp.v3.xtentismdm/src/com/amalto/webapp/v3/xtentismdm/dwr/ActionsDWR.java
+++ com.amalto.webapp.v3.xtentismdm/src/com/amalto/webapp/v3/xtentismdm/dwr/ActionsDWR.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import com.amalto.webapp.core.bean.Configuration;
+import com.amalto.webapp.core.dwr.CommonDWR;
 import com.amalto.webapp.core.util.Util;
 import com.amalto.webapp.util.webservices.WSDataClusterPK;
 import com.amalto.webapp.util.webservices.WSDataModelPK;
@@ -19,46 +20,28 @@ public class ActionsDWR {
 			WSDataClusterPK[] wsDataClustersPK = Util.getPort().getDataClusterPKs(
 					new WSRegexDataClusterPKs("*")
 					).getWsDataClusterPKs();
-			for (int i = 0; i < wsDataClustersPK.length; i++) {
-				if(!"MDMCONF".equals(wsDataClustersPK[i].getPk())
-						&& !"Xtentis Common Reporting".equals(wsDataClustersPK[i].getPk())
-						&& !"PROVISIONING".equals(wsDataClustersPK[i].getPk())
-						&& !"amaltoOBJECTSjcaadapters".equals(wsDataClustersPK[i].getPk())
-						&& !"UpdateReport".equals(wsDataClustersPK[i].getPk())
-						&& !"b2box CROSSREFERENCING".equals(wsDataClustersPK[i].getPk())
-						)
-					map.put(wsDataClustersPK[i].getPk(),wsDataClustersPK[i].getPk());
-			}
+			CommonDWR.filterSystemClustersPK(wsDataClustersPK, map);
 			return  map;
 		} catch (Exception e) {
 			e.printStackTrace();
 			return null;
 		}
 	}
-	
+
 	public static Map<String, String> getModels(String value){
 		try {
 			Map<String, String> map = new HashMap<String, String>();
 			WSDataModelPK[] wsDataModelsPK = Util.getPort().getDataModelPKs(
 					new WSRegexDataModelPKs("*")
 					).getWsDataModelPKs();
-			for (int i = 0; i < wsDataModelsPK.length; i++) {
-				if(!"REPORTING".equals(wsDataModelsPK[i].getPk())
-						&& !"PROVISIONING".equals(wsDataModelsPK[i].getPk())
-						&& !"CONF".equals(wsDataModelsPK[i].getPk())
-						&& !"UpdateReport".equals(wsDataModelsPK[i].getPk())
-						&& !"Xtentis Common Reporting".equals(wsDataModelsPK[i].getPk())
-						&& !"b2box CROSSREFERENCING".equals(wsDataModelsPK[i].getPk())
-						)
-					map.put(wsDataModelsPK[i].getPk(), wsDataModelsPK[i].getPk());
-			}
+			CommonDWR.filterSystemDataModelsPK(wsDataModelsPK, map);
 			return  map;
 		} catch (Exception e) {
 			e.printStackTrace();
 			return null;
 		}
 	}
-	
+
 	public String getCluster(){
 		try {
 			Configuration config = Configuration.getInstance();
