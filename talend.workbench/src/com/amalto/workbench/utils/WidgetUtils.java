ec5009b1aa5346e0187637b8aed249cb461af1bc
scorreia <scorreia@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2009-06-09 15:23:04 +0000

    fix transformerMainpage disable button don't show
    git-svn-id: http://talendforge.org/svn/mdm/trunk/trunk@27872 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- talend.workbench/src/com/amalto/workbench/utils/WidgetUtils.java
+++ talend.workbench/src/com/amalto/workbench/utils/WidgetUtils.java
@@ -1,6 +1,8 @@
 package com.amalto.workbench.utils;
 
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.CCombo;
+import org.eclipse.swt.custom.StyledText;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
@@ -16,8 +18,14 @@ public class WidgetUtils {
 	public static void enable(Composite parent,boolean enabled){
 		parent.setEnabled(enabled);
 		for(Control control:parent.getChildren()){
-			if(control instanceof Text || control instanceof CCombo || control instanceof Combo|| control instanceof Button){
+			if(control instanceof Text || control instanceof CCombo || control instanceof Combo|| control instanceof Button || control instanceof StyledText){
 				control.setEnabled(enabled);
+				if(control instanceof StyledText){
+					if(!enabled)
+						control.setBackground(control.getParent().getBackground());
+					else
+						control.setBackground(control.getDisplay().getSystemColor(SWT.COLOR_WHITE));
+				}
 			}
 			if(control instanceof Composite){
 				enable((Composite)control, enabled);
