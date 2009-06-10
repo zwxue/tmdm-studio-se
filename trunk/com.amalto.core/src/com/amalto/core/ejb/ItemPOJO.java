1f6ea86b3425296f3c7013e26f4640fc515a457a
hshu <hshu@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2009-06-10 14:06:01 +0000

    add cluster description
    git-svn-id: http://talendforge.org/svn/mdm/trunk@27882 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- trunk/com.amalto.core/src/com/amalto/core/ejb/ItemPOJO.java
+++ trunk/com.amalto.core/src/com/amalto/core/ejb/ItemPOJO.java
@@ -477,7 +477,7 @@ public class ItemPOJO implements Serializable{
         try {
         	//init MDMItemsTrash Cluster
         	if(dataClusterCtrlLocal.existsDataCluster(new DataClusterPOJOPK("MDMItemsTrash"))==null){
-    			dataClusterCtrlLocal.putDataCluster(new DataClusterPOJO(null,"MDMItemsTrash"));
+    			dataClusterCtrlLocal.putDataCluster(new DataClusterPOJO("MDMItemsTrash","Holds logical deleted items",null));
     			org.apache.log4j.Logger.getLogger(ItemPOJO.class).info("Init MDMItemsTrash Cluster");
     		}
         	
