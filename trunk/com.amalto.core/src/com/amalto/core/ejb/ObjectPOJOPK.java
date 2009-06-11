d92149683fcda30241b31e71622a401a39d5fb39
bgrieder <bgrieder@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2009-06-11 20:17:23 +0000

    Fixed Routing Order already executing unnder load
    git-svn-id: http://talendforge.org/svn/mdm/trunk@27899 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- trunk/com.amalto.core/src/com/amalto/core/ejb/ObjectPOJOPK.java
+++ trunk/com.amalto.core/src/com/amalto/core/ejb/ObjectPOJOPK.java
@@ -8,11 +8,11 @@ import com.amalto.core.util.Util;
 public class ObjectPOJOPK implements Serializable{
 
 	String[] ids = null;
-	
+
 	public ObjectPOJOPK (String[] itemIds) {
 		this.ids = itemIds;
 	}
-	
+
 	public ObjectPOJOPK (String uniqueid) {
 		this(uniqueid == null ? null : uniqueid.split("\\.\\."));
 	}
@@ -31,15 +31,21 @@ public class ObjectPOJOPK implements Serializable{
 	public void setIds(String[] ids) {
 		this.ids = ids;
 	}
-	
+
 	public String toString() {
 		return Util.joinStrings(ids, "..");
 	}
-	
+
 	public String getUniqueId() {
 		return toString();
 	}
-	
-	
-	
+
+	@Override
+	public boolean equals(Object obj) {
+        if (obj instanceof ObjectPOJOPK) {
+        	return this.getUniqueId().equals(((ObjectPOJOPK)obj).getUniqueId());
+        }
+        return false;
+	}
+
 }
