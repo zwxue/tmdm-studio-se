0093c520b6420bf1b76c04a71276baa51f0f5f6e
scorreia <scorreia@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2009-07-03 12:57:27 +0000

    using AutoincrementGenerator to generate the last num
    git-svn-id: http://talendforge.org/svn/mdm/trunk/trunk@28090 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- com.amalto.core/src/com/amalto/core/util/AutoIncrementGenerator.java
+++ com.amalto.core/src/com/amalto/core/util/AutoIncrementGenerator.java
@@ -1,39 +1,65 @@
 package com.amalto.core.util;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.util.Properties;
 
 import com.amalto.core.objects.configurationinfo.util.XtentisConfiguration;
 
 /**
- * @deprecated use org.jboss.util.id.UID
+ * 
  * AutoIncrement to generate a num
+ * the autoincrement num is saved in auto_increment.conf file
  * @author achen
  *
  */
 public class AutoIncrementGenerator {
-	static volatile long  num=-1;
-	static final String AUTO_INCREMENT_NUM="auto_increment_num";
-	static Properties p= XtentisConfiguration.getConfiguration();	
+	//static volatile long  num=-1;	
+
+	static File file = new File("auto_increment.conf");
 	
+	private static Properties CONFIGURATION = null;
+	static{
+		//first try Current path
+		CONFIGURATION = new Properties();
+		if (file.exists()) {
+			try {
+				CONFIGURATION.load(new FileInputStream(file));				
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		} 	
+	}
 	/**
 	 * this is not a good alogrithm, need to find a better way
 	 * @return
 	 */
-	public synchronized static long  generateNum(){
-		if(num == -1){//read from configure file			
-			String n=p.getProperty(AUTO_INCREMENT_NUM);
-			if(n==null){
-				num=0;				
-			}else{
-				num=Long.valueOf(n).longValue();
-			}
-			if(num==-1) num++;
+	public synchronized static long  generateNum(String universe,String dataCluster,String conceptName){
+		String key=universe+"."+dataCluster+"."+conceptName;
+		long num=0;		
+		String n=CONFIGURATION.getProperty(key);
+		if(n==null){
+			num=0;				
 		}else{
-			++num;
+			num=Long.valueOf(n).longValue();
 		}
-		
-		p.setProperty(AUTO_INCREMENT_NUM, String.valueOf(num));		
-		XtentisConfiguration.save();
+		num++;
+			
+		CONFIGURATION.setProperty(key, String.valueOf(num));				
 		return num;
 	}
+	/**
+	 * save configure file
+	 */
+	public static void saveToFile(){
+		FileOutputStream out;
+		try {
+			out = new FileOutputStream(file);
+			CONFIGURATION.store(out, "auto_increment configure file");
+		} catch (Exception e) {
+			String err = "AUTOINCREMENT Configuration: unable to save the configuration in '"+file.getAbsolutePath()+"' :"+e.getMessage(); 
+			org.apache.log4j.Logger.getLogger(AutoIncrementGenerator.class).error(err,e);
+		}		
+	}	
 }
