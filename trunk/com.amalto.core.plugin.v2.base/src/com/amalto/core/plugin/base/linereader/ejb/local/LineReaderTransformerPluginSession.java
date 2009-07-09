92fb612688294ef508e9d65beb3d243bc52e6fca
scorreia <scorreia@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2009-07-09 16:25:48 +0000

    hshu add new transformer template
    git-svn-id: http://talendforge.org/svn/mdm/trunk@28131 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- trunk/com.amalto.core.plugin.v2.base/src/com/amalto/core/plugin/base/linereader/ejb/local/LineReaderTransformerPluginSession.java
+++ trunk/com.amalto.core.plugin.v2.base/src/com/amalto/core/plugin/base/linereader/ejb/local/LineReaderTransformerPluginSession.java
@@ -5,7 +5,7 @@ package com.amalto.core.plugin.base.linereader.ejb.local;
 
 /**
  * Session layer for LineReaderTransformerPlugin.
- * @xdoclet-generated at 18-08-08
+ * @xdoclet-generated at 9-07-09
  * @copyright The XDoclet Team
  * @author XDoclet
  * @version ${version}
