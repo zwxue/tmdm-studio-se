024499ae1054fa480137e623694ef8822cc9cbbd
scorreia <scorreia@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2009-07-16 13:03:11 +0000

    git-svn-id: http://talendforge.org/svn/mdm/trunk@28171 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ trunk/com.amalto.core/src/com/amalto/core/util/ITransformerConstants.java
@@ -0,0 +1,11 @@
+package com.amalto.core.util;
+
+public interface ITransformerConstants {
+	
+	public static final String VARIABLE_DEFAULT = "_DEFAULT_";
+	
+	public static final String VARIABLE_OUTPUT_OF_BEFORESAVINGTRANFORMER = "output_error_message";
+	
+	public static final String VARIABLE_OUTPUT_TO_ITEMDISPATCHERSERVICE = "output_to_itemdispatcher";
+	
+}
