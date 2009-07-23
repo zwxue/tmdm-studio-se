bcf70ccc0abc3236d16ab2dc3535934ad1f02ab7
scorreia <scorreia@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2009-07-23 10:04:29 +0000

    git-svn-id: http://talendforge.org/svn/mdm/trunk/trunk@28225 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- talend.workbench/src/com/amalto/workbench/actions/XSDNewSimpleTypeDefinition.java
+++ talend.workbench/src/com/amalto/workbench/actions/XSDNewSimpleTypeDefinition.java
@@ -39,7 +39,6 @@ public class XSDNewSimpleTypeDefinition extends UndoAction implements SelectionL
 		setText("Create a Simpe Type");
 		setToolTipText("Create a new simple Type");
 		setDescription(getToolTipText());
-		// TODO Auto-generated constructor stub
 	}
 
 	public IStatus doAction() {
@@ -53,32 +52,23 @@ public class XSDNewSimpleTypeDefinition extends UndoAction implements SelectionL
 				customTypes.add(type.getName());
 		}
 		
-//		ArrayList builtInTypes = new ArrayList();
 		for (Iterator iter =  schema.getSchemaForSchema().getTypeDefinitions().iterator(); iter.hasNext(); ) {
 			XSDTypeDefinition type = (XSDTypeDefinition) iter.next();
 			if (type instanceof XSDSimpleTypeDefinition)
 				builtInTypes.add(type.getName());
 		}
-		
 
 		dialog = new SimpleTypeInputDialog(this, page.getSite().getShell(),"New Simple Type", customTypes,builtInTypes);
-		
 		dialog.setBlockOnOpen(true);
 		int ret = dialog.open();
 		if (ret == Dialog.CANCEL) {
 			return Status.CANCEL_STATUS;
 		}
 		
-		
-		
-		
-		
     	XSDFactory factory = XSDSchemaBuildingTools.getXSDFactory();
     	XSDSimpleTypeDefinition typedef = factory.createXSDSimpleTypeDefinition();
     	//set new one
-		if (builtIn) {
-//			typedef.setBaseTypeDefinition(schema.resolveSimpleTypeDefinition(schema.getSchemaForSchemaNamespace(), typeName));
-		} else {
+		if (!builtIn) {
 			//check if simple type definition already exists
 			XSDSimpleTypeDefinition std = schema.resolveSimpleTypeDefinition(typeName);
 			if (!schema.getTypeDefinitions().contains(std)) {
@@ -90,50 +80,7 @@ public class XSDNewSimpleTypeDefinition extends UndoAction implements SelectionL
 		
 		//remove current facets
 		typedef.getFacetContents().removeAll(typedef.getFacetContents());
-		
 		typedef.updateElement();
-		
-//		XSDModelGroup group = factory.createXSDModelGroup();
-//		XSDParticle subParticle = null;
-//		XSDElementDeclaration subElement = null;
-//		
-//		subElement = factory.createXSDElementDeclaration();
-//		subElement.setName("subelement");
-//		subElement.setTypeDefinition(schema.resolveSimpleTypeDefinition(schema.getSchemaForSchemaNamespace(), "string"));
-//		
-//		subParticle = factory.createXSDParticle();
-//		subParticle.setMinOccurs(1);
-//		subParticle.setMaxOccurs(1);
-//		subParticle.setContent(subElement);
-//		subParticle.updateElement();
-		
-//		if (isChoice)
-//			group.setCompositor(XSDCompositor.CHOICE_LITERAL);
-//		else if (isAll)
-//			group.setCompositor(XSDCompositor.ALL_LITERAL);
-//		else
-//			group.setCompositor(XSDCompositor.SEQUENCE_LITERAL);
-//		group.getContents().add(0,subParticle);
-//		group.updateElement();
-   			
-//		XSDComplexTypeDefinition complexType = factory.createXSDComplexTypeDefinition();
-//		//complexType.setDerivationMethod(XSDDerivationMethod.EXTENSION_LITERAL);
-//		complexType.setName(typeName);
-//		schema.getContents().add(complexType);
-//		
-//		complexType.updateElement();
-//		
-
-		//add the group
-//		XSDParticle groupParticle  = factory.createXSDParticle();
-//		groupParticle.setMinOccurs(1);
-//		groupParticle.setMaxOccurs(1);
-//		groupParticle.setContent(group);
-//		groupParticle.updateElement();
-		
-//		complexType.setContent(groupParticle);       			
-//		complexType.updateElement();
-		
 		page.refresh();
 		page.markDirty();
 		return Status.OK_STATUS;
