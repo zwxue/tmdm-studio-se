9d5d2348cc7b3f3e35fade24c5082f9e604156f2
scorreia <scorreia@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2009-07-28 13:06:44 +0000

    add try me for smtp
    git-svn-id: http://talendforge.org/svn/mdm/trunk/trunk@28260 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- com.amalto.service.smtp/src/com/amalto/service/smtp/ejb/local/SmtpLocal.java
+++ com.amalto.service.smtp/src/com/amalto/service/smtp/ejb/local/SmtpLocal.java
@@ -5,7 +5,7 @@ package com.amalto.service.smtp.ejb.local;
 
 /**
  * Local interface for Smtp.
- * @xdoclet-generated at 31-05-09
+ * @xdoclet-generated at 28-07-09
  * @copyright The XDoclet Team
  * @author XDoclet
  * @version ${version}
@@ -27,6 +27,8 @@ public interface SmtpLocal
    public java.io.Serializable receiveFromOutbound( java.util.HashMap map ) throws com.amalto.core.util.XtentisException;
 
    public java.lang.String receiveFromInbound( com.amalto.core.ejb.ItemPOJOPK itemPK,java.lang.String routingOrderID,java.lang.String parameters ) throws com.amalto.core.util.XtentisException;
+
+   public java.lang.String sendSimpleMail( java.lang.String from,java.lang.String to,java.lang.String cc,java.lang.String bcc,java.lang.String subject,java.lang.String body ) throws com.amalto.core.util.XtentisException;
 
    public java.lang.String getDocumentation( java.lang.String twoLettersLanguageCode ) throws com.amalto.core.util.XtentisException;
 
