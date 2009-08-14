7a93cdd9117f1e8db8d04ac7e5f190e775ffc7f4
scorreia <scorreia@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2009-08-14 14:03:28 +0000

    git-svn-id: http://talendforge.org/svn/mdm/trunk@28400 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- trunk/talend.workbench/src/com/amalto/workbench/image/OverlayImageProvider.java
+++ trunk/talend.workbench/src/com/amalto/workbench/image/OverlayImageProvider.java
@@ -7,14 +7,9 @@ import com.amalto.workbench.image.OverlayImage.EPosition;
 import com.amalto.workbench.utils.EXObjectStatus;
 
 public class OverlayImageProvider {
-    private static final int BUTTOM_RIGHT = 0;
-
-    private static final int BUTTOM_LEFT = 1;
     
     public static Image getImageWithStatus(Image source, EXObjectStatus status) {
         ImageDescriptor statusOverlay;
-        int p = BUTTOM_RIGHT;
-
         switch (status) {
         case DEACTIVE:
             statusOverlay = ImageCache.getImage(EImage.WAITING_OVR.getPath());
@@ -25,13 +20,7 @@ public class OverlayImageProvider {
         }
         if(statusOverlay==null) return source;
 
-        EPosition position = null;
-        if (p == BUTTOM_RIGHT) {
-            position = EPosition.BOTTOM_RIGHT;
-        } else if (p == BUTTOM_LEFT) {
-            position = EPosition.BOTTOM_LEFT;
-        }
-        OverlayImage overlayImage = new OverlayImage(source, statusOverlay, position);
+        OverlayImage overlayImage = new OverlayImage(source, statusOverlay, EPosition.BOTTOM_RIGHT);
         return ImageProvider.getImage(overlayImage);
     }
 
