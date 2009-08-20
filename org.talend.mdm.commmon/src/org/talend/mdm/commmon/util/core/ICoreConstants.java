874ddbe7f79713b804c4304f472c16e52cd0b88a
achen <achen@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2009-08-20 01:20:21 +0000

    git-svn-id: http://talendforge.org/svn/mdm/trunk@28497 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.mdm.commmon/src/org/talend/mdm/commmon/util/core/ICoreConstants.java
@@ -0,0 +1,9 @@
+package org.talend.mdm.commmon.util.core;
+
+public interface ICoreConstants {
+	//schematron_rule
+	//static final String SCHEMATRON_RULE="schematron_rule";
+	//static final String SCHEMATRON_TAG="<schema xmlns=\"http://www.ascc.net/xml/schematron\" ns=\"http://xml.apache.cocoon/xmlform\">";
+	static final String X_Schematron="X_Schematron";
+	
+}
