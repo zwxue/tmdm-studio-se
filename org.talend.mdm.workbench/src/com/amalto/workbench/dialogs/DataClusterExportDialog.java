5d3d8a2e4ad4ef61e4a4bbd00e9df0cbab72e175
fliu <fliu@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2009-08-31 03:52:44 +0000

    add export capability for all xobject types
    0008842: Add import / export capability on object types 
    
    git-svn-id: http://talendforge.org/svn/mdm/trunk@28989 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.workbench/src/com/amalto/workbench/dialogs/DataClusterExportDialog.java
+++ org.talend.mdm.workbench/src/com/amalto/workbench/dialogs/DataClusterExportDialog.java
@@ -2,7 +2,12 @@ package com.amalto.workbench.dialogs;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
@@ -20,7 +25,9 @@ import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.forms.widgets.FormToolkit;
 
 import com.amalto.workbench.models.TreeObject;
+import com.amalto.workbench.models.TreeParent;
 import com.amalto.workbench.utils.EXtentisObjects;
+import com.amalto.workbench.utils.IConstants;
 import com.amalto.workbench.utils.Util;
 import com.amalto.workbench.widgets.FileSelectWidget;
 import com.amalto.workbench.widgets.LabelCombo;
@@ -30,6 +37,7 @@ public class DataClusterExportDialog extends Dialog{
 	FormToolkit toolkit=new WidgetFactory();
 	private LabelCombo comboDataCluster;
 	TreeObject xObject;
+	private Map<String , String> xpathMap = new HashMap<String, String>();
 	private FileSelectWidget fw;
 	private String server;
 	String dataCluster;
@@ -45,24 +53,55 @@ public class DataClusterExportDialog extends Dialog{
 		composite.setBackground(composite.getDisplay().getSystemColor(SWT.COLOR_WHITE));
 		GridLayout layout = (GridLayout)composite.getLayout();
 		layout.numColumns = 1;
+        
+		Matcher matcher;
+		final String dataCluster = "Data Cluster";
+		final String all = "ALL";
+		String xobjectName = "";
+		xobjectName = xObject.getDisplayName();
+		if (xobjectName.equals(dataCluster))
+			xobjectName = all;
 
-		comboDataCluster=new LabelCombo(toolkit,composite,"Data Cluster",SWT.BORDER,2);
+		comboDataCluster=new LabelCombo(toolkit,composite,dataCluster,SWT.BORDER,2);
 		comboDataCluster.getCombo().setEditable(false);
 		//List<String> dcs=Util.getCachedXObjectsNameSet(this.xObject, TreeObject.DATA_CLUSTER);
 		List<String> dcs=Util.getChildren(this.xObject.getServerRoot(), TreeObject.DATA_CLUSTER);
-		dcs.add(0,"ALL");
+		dcs.add(0,all);
+		for (String cluster : dcs) {
+			xpathMap.put(cluster, cluster);
+		}
+        for (TreeObject treeObj: xObject.getServerRoot().getChildren())
+        {
+        	if (treeObj.getDisplayName().startsWith(dataCluster))
+        		continue;
+        	String revision, xobject = "";
+        	String xpath = "";
+
+            matcher = filter(treeObj.getDisplayName());
+        	if (matcher.matches()) {
+				xobject = matcher.group(1).replace(" ", "");
+				revision = matcher.group(3);
+				if (revision.equals(IConstants.HEAD)) {
+					xpath = "amaltoOBJECTS" + xobject;
+				} else {
+					xpath = "R-" + revision + "/" + "amaltoOBJECTS" + xobject;
+				}
+			} else if (treeObj.getDisplayName().equals("Universe")) {
+				xobject = treeObj.getDisplayName();
+				xpath = "amaltoOBJECTS" + treeObj.getDisplayName();
+			}
+        	
+        	if (!xpath.equals("")) {
+        		xpathMap.put(treeObj.getDisplayName(), xpath);
+				dcs.add(treeObj.getDisplayName());
+			}
+        }
 		comboDataCluster.getCombo().setItems(dcs.toArray(new String[dcs.size()]));
+		int sel = Arrays.asList(comboDataCluster.getCombo().getItems()).indexOf(xobjectName);
+		comboDataCluster.getCombo().select(sel);
 		
 		//file
 		 
-		if(xObject!=null&& xObject.getDisplayName()!=null){
-			if(EXtentisObjects.DataCluster.getDisplayName().equalsIgnoreCase(xObject.getDisplayName())){
-				comboDataCluster.getCombo().select(0);
-			}else{				
-				comboDataCluster.getCombo().setText(xObject.getDisplayName());
-			}
-			//fw.getText().setText(xObject.getDisplayName());
-		}	
 		fw=new FileSelectWidget(composite,"Target      ",new String[]{"*.*","*.zip"}, comboDataCluster.getCombo().getText());
 		comboDataCluster.getCombo().addSelectionListener(new SelectionListener(){
 
@@ -78,6 +117,12 @@ public class DataClusterExportDialog extends Dialog{
 			}			
 		});
 		return composite;
+	}
+	
+	private Matcher filter(String name) {
+		Pattern bracket = Pattern.compile("(.*?)(\\s*)\\[(\\w+)\\]");
+		Matcher matcher = bracket.matcher(name);
+		return matcher;
 	}
 	
 	@Override
@@ -102,6 +147,7 @@ public class DataClusterExportDialog extends Dialog{
 			e1.printStackTrace();
 		}		
 		dataCluster=comboDataCluster.getCombo().getText();
+		dataCluster = xpathMap.get(dataCluster);
 		filename=fw.getText().getText();
 		Job job=new Job("Export Data Clusters : " + dataCluster+" ..."){
 			@Override
