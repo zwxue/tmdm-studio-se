cabe0cd6c2dd606bd8fbffe32d25d122a9cc3bb9
fliu <fliu@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2009-09-18 03:27:00 +0000

    add one webservice to get MDM.conf info from server
    bugID: 0009099
    
    git-svn-id: http://talendforge.org/svn/mdm/trunk@29887 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.commmon/src/org/talend/mdm/commmon/util/core/MDMConfiguration.java
+++ org.talend.mdm.commmon/src/org/talend/mdm/commmon/util/core/MDMConfiguration.java
@@ -40,10 +40,36 @@ public final class MDMConfiguration {
 			}
 		} 
 		
+		checkupPropertiesForXDBConf();
+		
 		return CONFIGURATION;
 		
 	}
 	
+	/**
+	 * check up xdb config properties to add default value if it is unavailable
+	 */
+	private static void checkupPropertiesForXDBConf()
+	{
+		if (CONFIGURATION == null) return;
+		
+		if (CONFIGURATION.getProperty("xmldb.server.name") == null)
+			CONFIGURATION.setProperty("xmldb.server.name", "localhost");
+		if (CONFIGURATION.getProperty("xmldb.server.port") == null)
+			CONFIGURATION.setProperty("xmldb.server.port", "8080");
+		if (CONFIGURATION.getProperty("xmldb.administrator.username") == null)
+			CONFIGURATION.setProperty("xmldb.administrator.username", "admin");
+		if (CONFIGURATION.getProperty("mldb.administrator.password") == null)
+			CONFIGURATION.setProperty("mldb.administrator.password", "1bc29b36f623ba82aaf6724fd3b16718");
+		if (CONFIGURATION.getProperty("xmldb.driver") == null)
+			CONFIGURATION.setProperty("xmldb.driver", "org.exist.xmldb.DatabaseImpl");
+		if (CONFIGURATION.getProperty("xmldb.dbid") == null)
+			CONFIGURATION.setProperty("xmldb.dbid", "exist");
+		if (CONFIGURATION.getProperty("xmldb.dburl") == null)
+			CONFIGURATION.setProperty("xmldb.dburl", "exist/xmlrpc/db");
+		if (CONFIGURATION.getProperty("xmldb.isupurl") == null)
+			CONFIGURATION.setProperty("xmldb.isupurl", "exist/");
+	}
 	/**
 	 * save configure file
 	 */
