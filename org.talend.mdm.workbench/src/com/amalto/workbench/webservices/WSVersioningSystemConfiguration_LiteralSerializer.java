e91f50636e2bbc60396a0357caf10cf20fec4124
achen <achen@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2009-10-13 02:04:26 +0000

    add feature 0009293: Auto svn check-in for Items. This must be done after 0009270.
    git-svn-id: http://talendforge.org/svn/mdm/trunk@30943 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.workbench/src/com/amalto/workbench/webservices/WSVersioningSystemConfiguration_LiteralSerializer.java
+++ org.talend.mdm.workbench/src/com/amalto/workbench/webservices/WSVersioningSystemConfiguration_LiteralSerializer.java
@@ -26,6 +26,8 @@ public class WSVersioningSystemConfiguration_LiteralSerializer extends LiteralOb
     private static final QName ns1_url_QNAME = new QName("", "url");
     private static final QName ns1_username_QNAME = new QName("", "username");
     private static final QName ns1_password_QNAME = new QName("", "password");
+    private static final QName ns1_autocommit_QNAME = new QName("", "autocommit");
+    private static final QName ns1_jndi_QNAME = new QName("", "jndi");
     
     public WSVersioningSystemConfiguration_LiteralSerializer(QName type, String encodingStyle) {
         this(type, encodingStyle, false);
@@ -48,8 +50,8 @@ public class WSVersioningSystemConfiguration_LiteralSerializer extends LiteralOb
         Object value;
         
         reader.nextElementContent();
-        elementName = reader.getName();
-        if (reader.getState() == XMLReader.START) {
+        while (reader.getState() == XMLReader.START) {
+            elementName = reader.getName();
             if (elementName.equals(ns1_name_QNAME)) {
                 member = ns3_myns3_string__java_lang_String_String_Serializer.deserialize(ns1_name_QNAME, reader, context);
                 if (member == null) {
@@ -57,73 +59,55 @@ public class WSVersioningSystemConfiguration_LiteralSerializer extends LiteralOb
                 }
                 instance.setName((java.lang.String)member);
                 reader.nextElementContent();
-            } else {
-                throw new DeserializationException("literal.unexpectedElementName", new Object[] { ns1_name_QNAME, reader.getName() });
             }
-        }
-        else {
-            throw new DeserializationException("literal.expectedElementName", reader.getName().toString());
-        }
-        elementName = reader.getName();
-        if (reader.getState() == XMLReader.START) {
-            if (elementName.equals(ns1_description_QNAME)) {
+            else if (elementName.equals(ns1_description_QNAME)) {
                 member = ns3_myns3_string__java_lang_String_String_Serializer.deserialize(ns1_description_QNAME, reader, context);
                 instance.setDescription((java.lang.String)member);
                 reader.nextElementContent();
-            } else {
-                throw new DeserializationException("literal.unexpectedElementName", new Object[] { ns1_description_QNAME, reader.getName() });
             }
-        }
-        else {
-            throw new DeserializationException("literal.expectedElementName", reader.getName().toString());
-        }
-        elementName = reader.getName();
-        if (reader.getState() == XMLReader.START) {
-            if (elementName.equals(ns1_url_QNAME)) {
+            else if (elementName.equals(ns1_url_QNAME)) {
                 member = ns3_myns3_string__java_lang_String_String_Serializer.deserialize(ns1_url_QNAME, reader, context);
                 if (member == null) {
                     throw new DeserializationException("literal.unexpectedNull");
                 }
                 instance.setUrl((java.lang.String)member);
                 reader.nextElementContent();
-            } else {
-                throw new DeserializationException("literal.unexpectedElementName", new Object[] { ns1_url_QNAME, reader.getName() });
             }
-        }
-        else {
-            throw new DeserializationException("literal.expectedElementName", reader.getName().toString());
-        }
-        elementName = reader.getName();
-        if (reader.getState() == XMLReader.START) {
-            if (elementName.equals(ns1_username_QNAME)) {
+            else if (elementName.equals(ns1_username_QNAME)) {
                 member = ns3_myns3_string__java_lang_String_String_Serializer.deserialize(ns1_username_QNAME, reader, context);
                 if (member == null) {
                     throw new DeserializationException("literal.unexpectedNull");
                 }
                 instance.setUsername((java.lang.String)member);
                 reader.nextElementContent();
-            } else {
-                throw new DeserializationException("literal.unexpectedElementName", new Object[] { ns1_username_QNAME, reader.getName() });
             }
-        }
-        else {
-            throw new DeserializationException("literal.expectedElementName", reader.getName().toString());
-        }
-        elementName = reader.getName();
-        if (reader.getState() == XMLReader.START) {
-            if (elementName.equals(ns1_password_QNAME)) {
+            else if (elementName.equals(ns1_password_QNAME)) {
                 member = ns3_myns3_string__java_lang_String_String_Serializer.deserialize(ns1_password_QNAME, reader, context);
                 if (member == null) {
                     throw new DeserializationException("literal.unexpectedNull");
                 }
                 instance.setPassword((java.lang.String)member);
                 reader.nextElementContent();
-            } else {
-                throw new DeserializationException("literal.unexpectedElementName", new Object[] { ns1_password_QNAME, reader.getName() });
             }
-        }
-        else {
-            throw new DeserializationException("literal.expectedElementName", reader.getName().toString());
+            else if (elementName.equals(ns1_autocommit_QNAME)) {
+                member = ns3_myns3_string__java_lang_String_String_Serializer.deserialize(ns1_autocommit_QNAME, reader, context);
+                if (member == null) {
+                    throw new DeserializationException("literal.unexpectedNull");
+                }
+                instance.setAutocommit((java.lang.String)member);
+                reader.nextElementContent();
+            }
+            else if (elementName.equals(ns1_jndi_QNAME)) {
+                member = ns3_myns3_string__java_lang_String_String_Serializer.deserialize(ns1_jndi_QNAME, reader, context);
+                if (member == null) {
+                    throw new DeserializationException("literal.unexpectedNull");
+                }
+                instance.setJndi((java.lang.String)member);
+                reader.nextElementContent();
+            }
+            else {
+                throw new DeserializationException("literal.unexpectedElementName", new Object[] { elementName, reader.getName()});
+            }
         }
         
         XMLReaderUtil.verifyReaderState(reader, XMLReader.END);
@@ -154,5 +138,11 @@ public class WSVersioningSystemConfiguration_LiteralSerializer extends LiteralOb
             throw new SerializationException("literal.unexpectedNull");
         }
         ns3_myns3_string__java_lang_String_String_Serializer.serialize(instance.getPassword(), ns1_password_QNAME, null, writer, context);
+        if (instance.getAutocommit() != null) {
+            ns3_myns3_string__java_lang_String_String_Serializer.serialize(instance.getAutocommit(), ns1_autocommit_QNAME, null, writer, context);
+        }
+        if (instance.getJndi() != null) {
+            ns3_myns3_string__java_lang_String_String_Serializer.serialize(instance.getJndi(), ns1_jndi_QNAME, null, writer, context);
+        }
     }
 }
