bc418ab267193290c1c6540147859716d2c02dc8
achen <achen@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2009-11-03 09:12:32 +0000

    rename .crossreferencing to crossreferencing
    git-svn-id: http://talendforge.org/svn/mdm/trunk@32087 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.webapp.crossreferencing/src/org/talend/mdm/webapp/crossreferencing/bean/Configuration.java
+++ org.talend.mdm.webapp.crossreferencing/src/org/talend/mdm/webapp/crossreferencing/bean/Configuration.java
@@ -1,8 +1,10 @@
 package org.talend.mdm.webapp.crossreferencing.bean;
 
+import org.talend.mdm.commmon.util.core.ICoreConstants;
+
 public class Configuration {
 
-	public static final String datacluster=".crossreferencing";
-	public static final String datamodel=".crossreferencing";
+	public static final String datacluster=ICoreConstants.CrossReferencing_datacluster;
+	public static final String datamodel=ICoreConstants.CrossReferencing_datamodel;
 
 }
