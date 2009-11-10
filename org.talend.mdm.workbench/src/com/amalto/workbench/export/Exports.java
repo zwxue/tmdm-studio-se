6f1b4f1d0844ef5f3436f3e1e009536653901ae1
achen <achen@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2009-11-10 01:53:44 +0000

    modification: export items
    git-svn-id: http://talendforge.org/svn/mdm/trunk@32419 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.mdm.workbench/src/com/amalto/workbench/export/Exports.java
@@ -0,0 +1,14 @@
+package com.amalto.workbench.export;
+
+public class Exports {
+	private ExportItem[] items;
+
+	public ExportItem[] getItems() {
+		return items;
+	}
+
+	public void setItems(ExportItem[] items) {
+		this.items = items;
+	}
+	
+}
