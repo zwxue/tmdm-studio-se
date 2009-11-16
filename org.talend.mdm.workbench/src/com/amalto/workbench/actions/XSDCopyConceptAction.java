b6428fe571406ebc8fa464835120838431100c2b
ymli <ymli@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2009-11-16 09:59:33 +0000

    fix the bug:0009770: Ability to copy and paste a Concept
    git-svn-id: http://talendforge.org/svn/mdm/trunk@32724 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.mdm.workbench/src/com/amalto/workbench/actions/XSDCopyConceptAction.java
@@ -0,0 +1,60 @@
+package com.amalto.workbench.actions;
+
+import java.util.Iterator;
+
+import org.eclipse.jface.action.Action;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.xsd.XSDElementDeclaration;
+
+import com.amalto.workbench.editors.DataModelMainPage;
+import com.amalto.workbench.image.EImage;
+import com.amalto.workbench.image.ImageCache;
+import com.amalto.workbench.utils.WorkbenchClipboard;
+
+public class XSDCopyConceptAction extends Action {
+	private DataModelMainPage page;
+
+	public XSDCopyConceptAction(DataModelMainPage page) {
+		super();
+		this.page = page;
+		setImageDescriptor(ImageCache.getImage(EImage.COPY.getPath()));
+		setText("Copy");
+		setToolTipText("Copy Concept/Concepts");
+	}
+
+	//public IStatus doAction() {
+	public void run() {
+		try {
+			WorkbenchClipboard.getWorkbenchClipboard().conceptsReset();
+			IStructuredSelection selection = (IStructuredSelection)page.getTreeViewer().getSelection();
+	 		for (Iterator<XSDElementDeclaration> iter = selection.iterator(); iter.hasNext(); ) {
+				XSDElementDeclaration concept = iter.next();
+				
+				if (concept instanceof XSDElementDeclaration) 
+					WorkbenchClipboard.getWorkbenchClipboard().add(concept);
+			}
+			
+		}catch (Exception e) {
+			e.printStackTrace();
+			MessageDialog.openError(
+					page.getSite().getShell(),
+					"Error", 
+					"An error occured trying to copy Concept: "+e.getLocalizedMessage()
+			);
+
+		}
+		//return true;
+	}
+	public boolean checkInCopyType(Object[] selectedObjs){
+		for (Object obj : selectedObjs) {
+			if (obj instanceof XSDElementDeclaration)
+				continue;
+			else 
+				return false;
+		}
+
+		return true;
+	}
+	
+}
