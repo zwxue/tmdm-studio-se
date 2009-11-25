6f13de0025339d0bf4929b0a603dac610571482a
hshu <hshu@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2009-11-25 09:47:18 +0000

    git-svn-id: http://talendforge.org/svn/mdm/trunk@33195 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.core.plugin.base/src/com/amalto/core/plugin/base/workflowtrigger/agent/WorkflowVariableSet.java
+++ org.talend.mdm.core.plugin.base/src/com/amalto/core/plugin/base/workflowtrigger/agent/WorkflowVariableSet.java
@@ -7,14 +7,19 @@ public class WorkflowVariableSet {
 		
 		//TODO support date here
 		if(variableType.equals("String")){
+			if(variableValue==null)variableValue="";//default
 			toSetVariableValue=variableValue;
 		}else if(variableType.equals("Boolean")){
+			if(variableValue==null)variableValue="false";//default
 			toSetVariableValue = variableValue.equals("true");
 		}else if(variableType.equals("Float")){
+			if(variableValue==null)variableValue="0.0";//default
 			toSetVariableValue=Float.valueOf(variableValue);
 		}else if(variableType.equals("Integer")){
+			if(variableValue==null)variableValue="0";//default
 			toSetVariableValue=Integer.valueOf(variableValue);
 		}else{
+			if(variableValue==null)variableValue="";//default
 			toSetVariableValue=variableValue;
 		}
 		
