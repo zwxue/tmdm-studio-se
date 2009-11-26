c7bbd478b7bb813e947b73c0b7ce64300bbcf1ff
rhou <rhou@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2009-11-26 05:58:31 +0000

    Add uploading Custom Type function for resource in workbench.
    git-svn-id: http://talendforge.org/svn/mdm/trunk@33239 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.workbench/src/com/amalto/workbench/utils/ResourcesUtil.java
+++ org.talend.mdm.workbench/src/com/amalto/workbench/utils/ResourcesUtil.java
@@ -1,5 +1,8 @@
 package com.amalto.workbench.utils;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.StringReader;
 import java.util.ArrayList;
@@ -7,34 +10,48 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 
+import org.apache.http.Header;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.protocol.HTTP;
+import org.apache.http.util.EntityUtils;
 import org.dom4j.Document;
 import org.dom4j.DocumentException;
-import org.dom4j.Node;
+import org.dom4j.Element;
 import org.dom4j.io.SAXReader;
 
 public class ResourcesUtil {
 	
 	// Handle it using an HTTP client connector
-	private static final String dmURI="/pubcomponent/secure/customTypesSets";
+	private static final String dmURI="/pubcomponent/secure/dataModels";
+	private static final String cmURI="/pubcomponent/secure/customTypesSets";
 	private static final String picURI="/pubcomponent/secure/pictures";
 
     
 	   
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
 
 //init();
-    
-//    	    	getXMLString("http://localhost:8080"+picURI);
-    	getResourcesNameListFromURI("http://localhost:8080"+picURI);
-//    	getResourcesMapFromURI("http://localhost:8080"+picURI);
+        
+//    	getXMLString("http://localhost:8080"+picURI);
+//getResourcesNameListFromURI("http://localhost:8080"+cmURI);
+//getResourcesMapFromURI("http://localhost:8080"+cmURI);
+try {
+	postResourcesFromFile("demo2","d:/bud.xsd","http://localhost:8080"+cmURI);
+} catch (Exception e) {
+	e.printStackTrace();
+}
     }
     
     
@@ -45,14 +62,12 @@ private static String getXMLString(String uri) {
     DefaultHttpClient httpclient = new DefaultHttpClient();
 
     httpclient.getCredentialsProvider().setCredentials(
-            new AuthScope("localhost", 8080), 
+            new AuthScope(getEndpointHost(uri),Integer.valueOf(getEndpointPort(uri))), 
             new UsernamePasswordCredentials("admin", "talend"));
     
     HttpGet httpget = new HttpGet(uri);
     
     System.out.println("executing request" + httpget.getRequestLine());
-//    HttpResponse response = httpclient.execute(httpget);
-//    HttpEntity entity = response.getEntity();
 
     // Create a response handler
     ResponseHandler<String> responseHandler = new BasicResponseHandler();
@@ -64,9 +79,6 @@ private static String getXMLString(String uri) {
 	} catch (IOException e) {
 		e.printStackTrace();
 	}
-    System.out.println(responseBody);
-    
-    System.out.println("----------------------------------------");
     httpclient.getConnectionManager().shutdown();      
     return responseBody;
 
@@ -80,13 +92,17 @@ private static String getXMLString(String uri) {
     	HashMap<String, String> contentMap=new HashMap<String, String>();
     	String responseBody=getXMLString(uri);
     	Document document=parsXMLString(responseBody);
-        List<Node> nodes = XmlUtil.queryList(document, "/list/entry");
-        for (Iterator iterator = nodes.iterator(); iterator.hasNext();) {
-  		Node node = (Node) iterator.next();
-  		contentMap.put(node.selectSingleNode("name").getStringValue(),node.selectSingleNode("uri").getStringValue());
-  		System.out.println(node.selectSingleNode("name").getStringValue());
-  		System.out.println(node.selectSingleNode("uri").getStringValue());
-  	}
+    	for (Iterator iterator = document.getRootElement().elementIterator("entry"); iterator.hasNext();) {
+			Element element = (Element) iterator.next();
+			Element nameElement=element.element("name");
+			Element uriElement=element.element("name");
+			if(nameElement!=null&&uriElement!=null){
+				contentMap.put(nameElement.getStringValue(),uriElement.getStringValue());
+			}
+			else{
+				contentMap.put(element.getStringValue(),uri+"/"+element.getStringValue());
+			}
+		}
 		return contentMap;   	
     }
 
@@ -100,28 +116,70 @@ private static String getXMLString(String uri) {
     	String responseBody=getXMLString(uri);
 //    	nameList=getNameList(responseBody);
     	Document document=parsXMLString(responseBody);
-        List<Node> nodes = XmlUtil.queryList(document, "/list/entry/name");
-        for (Iterator iterator = nodes.iterator(); iterator.hasNext();) {
-  		Node node = (Node) iterator.next();
-  		nameList.add(node.getStringValue());
-  		System.out.println(node.getStringValue());
-  	}
+    	for (Iterator iterator = document.getRootElement().elementIterator("entry"); iterator.hasNext();) {
+			Element element = (Element) iterator.next();
+			Element nameElement=element.element("name");
+			if(nameElement!=null){
+				nameList.add(nameElement.getStringValue());
+			}
+			else{
+				nameList.add(element.getStringValue());
+			}
+		}
     	return nameList; 
     	
     }
-/*
-    private static List<String> getNameList(String responseBody) {
-    	List<String> nameList=new ArrayList<String>();
-    	Document document=parsXMLString(responseBody);
-       List<Node> nodes = XmlUtil.queryList(document, "/list/entry/name");
-      for (Iterator iterator = nodes.iterator(); iterator.hasNext();) {
-		Node node = (Node) iterator.next();
-		nameList.add(node.getStringValue());
-		System.out.println(node.getStringValue());
-	}
-		return nameList;   
+	public static void postResourcesFromFile(String typeName,String pathName,String uri) throws Exception  {
+		File file=new File(pathName);
+		FileReader fr=new FileReader(file);
+		BufferedReader br = new BufferedReader(fr);
+		postResourcesFromContentString(br.readLine(),uri,typeName);
+		br.close();
+		fr.close();
 		
-	}*/
+	}
+	public static void postResourcesFromContentString(String content,String uri,String typeName)  throws Exception{
+		System.out.println(content);
+		HttpPost httppost = new HttpPost(uri);
+		List<BasicNameValuePair> nvps = new ArrayList<BasicNameValuePair>();
+		nvps.add(new BasicNameValuePair("domainObjectName", typeName));
+		nvps.add(new BasicNameValuePair("domainObjectContent", content));
+		httppost.setEntity(new UrlEncodedFormEntity( nvps, HTTP.UTF_8));
+		postContent(uri,httppost);
+	}
+	
+	private static void postContent(String uri, HttpPost httppost) throws Exception, IOException {
+	    DefaultHttpClient httpclient = new DefaultHttpClient();
+
+	    httpclient.getCredentialsProvider().setCredentials(
+	            new AuthScope(getEndpointHost(uri),Integer.valueOf(getEndpointPort(uri))), 
+	            new UsernamePasswordCredentials("admin", "talend"));
+	    
+	    System.out.println("executing request" + httppost.getRequestLine());
+	    HttpResponse response = httpclient.execute(httppost);
+	    HttpEntity entity = response.getEntity();
+
+	        entity = response.getEntity();                                                             
+	        try{
+	            Header[] headers = response.getAllHeaders();                    
+	            String responseString = null;
+	            if (response.getEntity() != null) {
+	            responseString = EntityUtils.toString(response.getEntity());      
+	            }
+	        } finally {
+	            if (entity != null)                          
+	            entity.consumeContent();                                                   // release connection gracefully
+	        }
+	        if (entity != null) {
+	        entity.consumeContent();
+	        }
+	    
+	    httpclient.getConnectionManager().shutdown();      
+	}
+
+
+
+
 
 	private static Document parsXMLString(String responseBody) {
         SAXReader saxReader = new SAXReader();   
@@ -139,11 +197,11 @@ private static String getXMLString(String uri) {
 	}
 
 
-	public String getEndpointHost(String uri) {
+	public static String getEndpointHost(String uri) {
 		
 		
 		if(uri!=null){
-			int startPos=uri.indexOf("://");
+			int startPos=uri.indexOf("//")+2;
 			int endPos=uri.indexOf(":",startPos);
 			if(endPos!=-1&&startPos!=-1)
 				return uri.substring(startPos, endPos);
@@ -152,12 +210,12 @@ private static String getXMLString(String uri) {
 		return uri;
 	}	
 	
-	public String getEndpointPort(String uri) {
+	public static String getEndpointPort(String uri) {
 		
 		
 		if(uri!=null){
-			String[] splitString=uri.split(":",2);
-			if(splitString[2]!=null)
+			String[] splitString=uri.split(":",3);
+			if(splitString[splitString.length-1]!=null)
 				return splitString[2].substring(0, splitString[2].indexOf("/"));
 		}
 		
