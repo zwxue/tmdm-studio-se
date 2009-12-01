8c0e406e270256859fe1d58e82f3ef61dba0486a
achen <achen@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2009-12-01 07:47:24 +0000

    add delete job action
    git-svn-id: http://talendforge.org/svn/mdm/trunk@33405 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.mdm.workbench/src/com/amalto/workbench/actions/DeleteJobAction.java
@@ -0,0 +1,53 @@
+package com.amalto.workbench.actions;
+
+import java.net.URL;
+
+import org.eclipse.jface.action.Action;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+
+import com.amalto.workbench.image.EImage;
+import com.amalto.workbench.image.ImageCache;
+import com.amalto.workbench.models.TreeObject;
+import com.amalto.workbench.utils.Util;
+import com.amalto.workbench.views.ServerView;
+import com.amalto.workbench.webservices.XtentisPort;
+
+public class DeleteJobAction extends Action{
+
+	private ServerView server = null;
+	private TreeObject xobject;
+
+				
+	public DeleteJobAction(ServerView serverView) {
+		super();
+		this.server = serverView;
+			
+		setImageDescriptor(ImageCache.getImage(EImage.DELETE_OBJ.getPath()));
+		setText("Delete");
+		setToolTipText("Delete");
+	}
+	
+	public void run() {
+		if (this.server != null) { //called from ServerView
+			ISelection selection = server.getViewer().getSelection();
+			xobject = (TreeObject)((IStructuredSelection)selection).getFirstElement();
+		}
+        
+        if (xobject.getType()!=TreeObject.JOB) return;
+       try{ 
+	//      Access to server and get port
+			XtentisPort port = Util.getPort(
+					new URL(xobject.getEndpointAddress()),
+					xobject.getUniverse(),
+					xobject.getUsername(),
+					xobject.getPassword()
+			);		
+			xobject.getParent().removeChild(xobject);
+
+       }catch(Exception e){
+    	   
+       }
+	}
+
+}
