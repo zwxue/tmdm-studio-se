d95c1c99d913d210bc9529c68fe263a2a6588728
hshu <hshu@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2009-12-11 04:18:00 +0000

    enforce data encode to UTF-8
    git-svn-id: http://talendforge.org/svn/mdm/trunk@34032 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.core/src/com/amalto/core/initdb/InitDBUtil.java
+++ org.talend.mdm.core/src/com/amalto/core/initdb/InitDBUtil.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -107,7 +108,15 @@ public class InitDBUtil {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		return sb.toString();
+		
+		String result="";
+		try {
+			result= new String(sb.toString().getBytes(),"UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+		
+		return result;
 	}
 	public static void main(String[] args) {
 		init();
