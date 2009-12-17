d5ab935245740e2dc7b3396a48d729760147c965
hshu <hshu@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2009-12-17 05:44:31 +0000

    0010688: When importing a bar file,the two bar files with the same name but different version
    git-svn-id: http://talendforge.org/svn/mdm/trunk@34310 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.workbench/src/com/amalto/workbench/dialogs/WorkflowDefaultTransformerGenerateWizard.java
+++ org.talend.mdm.workbench/src/com/amalto/workbench/dialogs/WorkflowDefaultTransformerGenerateWizard.java
@@ -87,7 +87,7 @@ public class WorkflowDefaultTransformerGenerateWizard extends Wizard {
 		parameter=parameter+"</parameters>";
 		steps[1]=new WSTransformerProcessStep("amalto/local/transformer/plugin/workflowtrigger","Generate the workflowtrigger step",parameter,input,output,false);	
 		
-		transformer.setName("Default_WorkflowProcess_"+uuid.getProcessName()+"_transformer");
+		transformer.setName("Default_WorkflowProcess_"+uuid.getProcessName()+"_"+uuid.getProcessVersion()+"_transformer");
 		transformer.setDescription(transformer.getName());
 		transformer.setProcessSteps(steps);
 		
