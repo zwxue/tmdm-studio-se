973022a214f7df02cdbf8d34551cf6e77ec9a1b3
hshu <hshu@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2009-12-23 09:22:17 +0000

    git-svn-id: http://talendforge.org/svn/mdm/trunk@34618 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.core/src/com/amalto/core/delegator/XtentisWSBeanDefaultDelegator.java
+++ org.talend.mdm.core/src/com/amalto/core/delegator/XtentisWSBeanDefaultDelegator.java
@@ -1,17 +1,9 @@
 package com.amalto.core.delegator;
 
 import java.rmi.RemoteException;
-import java.util.Collection;
-import java.util.Iterator;
 
-import com.amalto.core.objects.universe.ejb.UniversePOJOPK;
-import com.amalto.core.objects.universe.ejb.local.UniverseCtrlLocal;
-import com.amalto.core.objects.universe.ejb.local.UniverseCtrlUtil;
-import com.amalto.core.webservice.WSGetUniversePKs;
 import com.amalto.core.webservice.WSPing;
 import com.amalto.core.webservice.WSString;
-import com.amalto.core.webservice.WSUniversePK;
-import com.amalto.core.webservice.WSUniversePKArray;
 
 public abstract class XtentisWSBeanDefaultDelegator implements XtentisWSBeanDelegator{
 
