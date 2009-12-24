59a8f255ab51990b9104df82d6bb6bc3233012ff
achen <achen@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2009-12-24 07:48:09 +0000

    fix the bug:0010792:add the confirm
    git-svn-id: http://talendforge.org/svn/mdm/trunk@34657 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.workbench.enterprise/src/org/talend/mdm/workbench/enterprice/actions/ImportTISJobAction.java
+++ org.talend.mdm.workbench.enterprise/src/org/talend/mdm/workbench/enterprice/actions/ImportTISJobAction.java
@@ -32,145 +32,163 @@ import com.amalto.workbench.webservices.WSMDMNULL;
 import com.amalto.workbench.webservices.WSPUTMDMJob;
 import com.amalto.workbench.webservices.XtentisPort;
 
-public class ImportTISJobAction extends Action{
+public class ImportTISJobAction extends Action {
 
 	private ServerView server = ServerView.show();
 	private TreeParent xobject;
-				
+
 	public ImportTISJobAction() {
 		super();
-			
+
 		setImageDescriptor(ImageCache.getImage(EImage.PROCESS.getPath()));
 		setText("Import a TIS Job Archive");
 		setToolTipText("Import a TIS Job Archive");
 	}
-	
-	public void run() {
-		if (this.server != null) { //called from ServerView
-			ISelection selection = server.getViewer().getSelection();
-			xobject = (TreeParent)((IStructuredSelection)selection).getFirstElement();
-		}
-        
-        if (xobject.getType()!=TreeObject.JOB_REGISTRY) return;
-       try{ 
-	//      Access to server and get port
-			XtentisPort port = Util.getPort(
-					new URL(xobject.getEndpointAddress()),
-					xobject.getUniverse(),
-					xobject.getUsername(),
-					xobject.getPassword()
-			);		
-			FileDialog fileDialog = new FileDialog (server.getSite().getShell(), SWT.OPEN);				
-			fileDialog.setFilterExtensions (new String[]{"*.war"});				
-			String name=fileDialog.open();
-			
-	        
-			if(name!=null){
-				JobInfo info=getJobInfo(name);
-				WSMDMJobArray array = port.getMDMJob(new WSMDMNULL());
-				if(checkExist(array,info)){
-					MessageDialog
-					.openWarning(this.server.getSite().getShell(), "Warnning",
-							"This Job already exists!");
-	            	return;
-			}
-					
-					
-				String fileName = info.getJobname()+"_"+info.getJobversion()+".war";
-				String endpointaddress=xobject.getEndpointAddress();
-				String uploadURL = new URL(endpointaddress).getProtocol()+"://"+new URL(endpointaddress).getHost()+":"+new URL(endpointaddress).getPort()+"/datamanager/uploadFile?deployjob="+fileName;
-				String remoteFile = Util.uploadFileToAppServer(uploadURL, name,"admin","talend");	
-				//TODO
-				//save to db
-				port.putMDMJob(new WSPUTMDMJob(info.getJobname(),info.getJobversion()));
-				//parse file to get jobinfo
-				
-				TreeObject obj = new TreeObject(
-						//fileDialog.getFileName(),
-						info.getJobname()+"_"+info.getJobversion(),
-						xobject.getServerRoot(),
-						TreeObject.JOB,
-						info,
-						null   //no storage to save space
-				);
-				xobject.addChild(obj);
-		
-			
-			 LocalTreeObjectRepository.getInstance().mergeNewTreeObject(obj);
-			 XObjectEditor editpart=(XObjectEditor)server.getSite().getWorkbenchWindow().getActivePage().openEditor(
-	                    new XObjectEditorInput(obj,obj.getDisplayName()),
-	                    "com.amalto.workbench.editors.XObjectEditor"
-	           	);
-	            
 
-	           }  
-			
-       }catch(Exception e){
-    	   
-       }
+	public void run() {
+		if (this.server != null) { // called from ServerView
+			ISelection selection = server.getViewer().getSelection();
+			xobject = (TreeParent) ((IStructuredSelection) selection)
+					.getFirstElement();
+		}
+
+		if (xobject.getType() != TreeObject.JOB_REGISTRY)
+			return;
+		try {
+			// Access to server and get port
+			XtentisPort port = Util.getPort(new URL(xobject
+					.getEndpointAddress()), xobject.getUniverse(), xobject
+					.getUsername(), xobject.getPassword());
+			FileDialog fileDialog = new FileDialog(server.getSite().getShell(),
+					SWT.OPEN);
+			fileDialog.setFilterExtensions(new String[] { "*.war" });
+			String name = fileDialog.open();
+
+			boolean recover = false;
+			boolean exist = false;
+			if (name != null) {
+				JobInfo info = getJobInfo(name);
+				WSMDMJobArray array = port.getMDMJob(new WSMDMNULL());
+				exist = checkExist(array, info);
+				if (exist) {
+					
+					recover = MessageDialog
+							.openConfirm(
+									this.server.getSite().getShell(),
+									"Confirm",
+									"This job is already deployed on the server."+
+									System.getProperty("line.separator")+
+									"Are you sure you want to redeploy it with this new archive?");
+					if (!recover)
+						return;
+					else
+						MessageDialog.openWarning(this.server.getSite()	.getShell(), "Warnning","This Job is redeployed!");
+
+				}
+
+				String fileName = info.getJobname() + "_"
+						+ info.getJobversion() + ".war";
+				String endpointaddress = xobject.getEndpointAddress();
+				String uploadURL = new URL(endpointaddress).getProtocol()
+						+ "://" + new URL(endpointaddress).getHost() + ":"
+						+ new URL(endpointaddress).getPort()
+						+ "/datamanager/uploadFile?deployjob=" + fileName;
+				String remoteFile = Util.uploadFileToAppServer(uploadURL, name,"admin", "talend");
+				// TODO
+				// save to db
+				if (!exist) {
+					// if the job is redeployed, it will not be put in DB and not create a new treeobject.
+					port.putMDMJob(new WSPUTMDMJob(info.getJobname(), info
+							.getJobversion()));
+					// parse file to get jobinfo
+
+					TreeObject obj = new TreeObject(
+							// fileDialog.getFileName(),
+							info.getJobname() + "_" + info.getJobversion(),
+							xobject.getServerRoot(), TreeObject.JOB, info, null 
+					);
+					xobject.addChild(obj);
+					LocalTreeObjectRepository.getInstance().mergeNewTreeObject(
+							obj);
+					XObjectEditor editpart = (XObjectEditor) server.getSite().getWorkbenchWindow().getActivePage().openEditor(
+									new XObjectEditorInput(obj, obj.getDisplayName()),
+									"com.amalto.workbench.editors.XObjectEditor");
+				}
+
+			}
+
+		} catch (Exception e) {
+
+		}
 	}
+
 	/**
 	 * get the JobInfo:jobName and version
+	 * 
 	 * @param fileName
 	 * @return
 	 */
-	public JobInfo getJobInfo(String fileName){
-		JobInfo jobInfo = new JobInfo("","");
+	public JobInfo getJobInfo(String fileName) {
+		JobInfo jobInfo = new JobInfo("", "");
 		try {
-			
-			ZipInputStream in = new ZipInputStream(new FileInputStream(fileName));
-			
+
+			ZipInputStream in = new ZipInputStream(
+					new FileInputStream(fileName));
+
 			ZipEntry z = null;
-			
-			try{
-				String jobName="";
-				String jobVersion="";
-				while((z=in.getNextEntry())!=null){
+
+			try {
+				String jobName = "";
+				String jobVersion = "";
+				while ((z = in.getNextEntry()) != null) {
 					String dirName = z.getName();
-					//get job version
-					if(dirName.matches(".*?undeploy.wsdd")){
+					// get job version
+					if (dirName.matches(".*?undeploy.wsdd")) {
 						jobName = new File(dirName).getParentFile().getName();
-						if(jobName.matches(".*?_(\\d*_\\d*)")){
-						Pattern p = Pattern.compile(".*?_(\\d*_\\d*)");
-						Matcher m = p.matcher(jobName);
-						if(m.matches()){
-							jobVersion = m.group(m.groupCount());
+						if (jobName.matches(".*?_(\\d*_\\d*)")) {
+							Pattern p = Pattern.compile(".*?_(\\d*_\\d*)");
+							Matcher m = p.matcher(jobName);
+							if (m.matches()) {
+								jobVersion = m.group(m.groupCount());
+							}
 						}
 					}
-					}
-					//get job name
-					if(dirName.matches(".*?.wsdl")){
+					// get job name
+					if (dirName.matches(".*?.wsdl")) {
 						jobName = new File(dirName).getName();
-						jobName = jobName.substring(0,jobName.lastIndexOf("."));
+						jobName = jobName
+								.substring(0, jobName.lastIndexOf("."));
 					}
-					
+
 				}
 				jobInfo.setJobname(jobName);
 				jobInfo.setJobversion(jobVersion.replaceAll("_", "."));
-			}catch(FileNotFoundException e){
+			} catch (FileNotFoundException e) {
 				e.printStackTrace();
-			}
-			finally{
+			} finally {
 				in.close();
 			}
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-			catch(Exception e){
-				e.printStackTrace();
-			}
-			
+
 		return jobInfo;
 	}
-	//if the Job exist, return true,else return false
-	public boolean checkExist(WSMDMJobArray array, JobInfo jobInfo){
-		for(int i=0;i<array.getWsMDMJob().length;i++){
-			if(array.getWsMDMJob()[i].getJobName().equals(jobInfo.getJobname())&&array.getWsMDMJob()[i].getJobVersion().equals(jobInfo.getJobversion()))
+
+	// if the Job exist, return true,else return false
+	public boolean checkExist(WSMDMJobArray array, JobInfo jobInfo) {
+		for (int i = 0; i < array.getWsMDMJob().length; i++) {
+			if (array.getWsMDMJob()[i].getJobName()
+					.equals(jobInfo.getJobname())
+					&& array.getWsMDMJob()[i].getJobVersion().equals(
+							jobInfo.getJobversion()))
 				return true;
 		}
 		return false;
 	}
-	
+
 	public void runWithEvent(Event event) {
 		super.runWithEvent(event);
 	}
-	
+
 }
