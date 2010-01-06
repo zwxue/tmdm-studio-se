2859429e83cfe45295379ec8997374162ac0c33f
ctoum <ctoum@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2010-01-06 18:15:57 +0000

    tom duplicate
    git-svn-id: http://talendforge.org/svn/tem/trunk@35179 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.calljob.plugin/src/talend/core/transformer/plugin/v2/tiscall/webservices/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://talend.org", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package talend.core.transformer.plugin.v2.tiscall.webservices;
