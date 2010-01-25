37be29a55d442a6f14096ee5b0e28f13366855d2
jmsa <jmsa@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2010-01-25 20:27:42 +0000

    basic organization for Talend Open MDM project
    git-svn-id: http://talendforge.org/svn/tom/trunk@36324 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.workbench/src/com/amalto/workbench/utils/callback/ElementProcess.java
+++
@@ -1,8 +0,0 @@
-package com.amalto.workbench.utils.callback;
-
-import org.dom4j.Element;
-
-public interface ElementProcess {
-
-	    void process(Element element);
-}
--- org.talend.mdm.workbench/src/com/amalto/workbench/utils/callback/NodeProcess.java
+++
@@ -1,8 +0,0 @@
-package com.amalto.workbench.utils.callback;
-
-import org.dom4j.Node;
-
-public interface NodeProcess {
-
-	    void process(Node node);
-}
