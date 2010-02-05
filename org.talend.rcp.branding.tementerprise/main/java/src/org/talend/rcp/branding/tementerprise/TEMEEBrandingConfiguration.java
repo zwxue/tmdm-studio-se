f29846c7467ff4edb10f8be8c2862dc48e733827
ctoum <ctoum@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2010-02-05 14:22:43 +0000

    resurrecting tementerprise & tementerprise-feature from r36636
    git-svn-id: http://talendforge.org/svn/tem/trunk@37010 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.rcp.branding.tementerprise/main/java/src/org/talend/rcp/branding/tementerprise/TEMEEBrandingConfiguration.java
@@ -0,0 +1,27 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2009 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.rcp.branding.tementerprise;
+
+import org.talend.core.ui.branding.DefaultBrandingConfiguration;
+import org.talend.rcp.branding.tementerprise.i18n.Messages;
+
+/**
+ * hcheng class global comment. Detailled comment
+ */
+public class TEMEEBrandingConfiguration extends DefaultBrandingConfiguration {
+
+    @Override
+    public String getTISProductNameForWelcome() {
+        return Messages.getString("productshortname");
+    }
+}
