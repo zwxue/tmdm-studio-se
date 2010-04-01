c0ab4c1a2189c227b47550f1fb25dec37b5b6772
lliao <lliao@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2010-04-01 10:23:13 +0000

    add test methods
    git-svn-id: http://talendforge.org/svn/tom/trunk@39623 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.test/src/org/talend/mdm/test/benchmark/BenchmarkTestCase.java
+++ org.talend.mdm.test/src/org/talend/mdm/test/benchmark/BenchmarkTestCase.java
@@ -8,55 +8,116 @@ import org.talend.mdm.test.WebserviceTestCase;
 import urn_com_amalto_xtentis_webservice.WSDataCluster;
 import urn_com_amalto_xtentis_webservice.WSDataClusterPK;
 import urn_com_amalto_xtentis_webservice.WSDataModelPK;
+import urn_com_amalto_xtentis_webservice.WSDeleteItems;
+import urn_com_amalto_xtentis_webservice.WSGetItems;
+import urn_com_amalto_xtentis_webservice.WSInt;
 import urn_com_amalto_xtentis_webservice.WSPutDataCluster;
 import urn_com_amalto_xtentis_webservice.WSPutItem;
+import urn_com_amalto_xtentis_webservice.WSXPathsSearch;
 
 public class BenchmarkTestCase extends WebserviceTestCase {
 	WSDataModelPK dmpk=new WSDataModelPK("Order");
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
+	private static void main(String[] args) {
 	
 
 	}
-	public void testPutItemArray1k(){
-		PutItemArray(1000,10);
-		PutItemArray(1000,50);
-		PutItemArray(1000,100);
-		PutItemArray(1000,200);
-		PutItemArray(1000,500);
-	}
-	public void testPutItemArray100k() {
-		PutItemArray(100000,200);
-	}
-	public void testPutItemArray500k() {
-		PutItemArray(500000,200);
-	}
-	public void testPutItemArray1M() {
-		PutItemArray(1000000,200);
-	}
-	public void testPutItemArray3M() {
-		PutItemArray(3000000,200);
-	}
-	public void testPutItemArray10M() {
-		PutItemArray(10000000,200);
-	}
-	public void testPutItem100k() {
-		PutItem(100000);
-	}
-	public void testPutItem500k() {
-		PutItem(500000);
-	}	
-	public void testPutItem1M() {
-		PutItem(1000000);
-	}	
-	public void testPutItem3M() {
+//	public void testPutItemArray1k(){
+//		PutItemArray(1000,10);
+//		PutItemArray(1000,50);
+//		PutItemArray(1000,100);
+//		PutItemArray(1000,200);
+//		PutItemArray(1000,500);
+//	}
+//	public void testPutItemArray100k() {
+//		PutItemArray(100000,200);
+//	}
+//	public void testPutItemArray500k() {
+//		PutItemArray(500000,200);
+//	}
+//	public void testPutItemArray1M() {
+//		PutItemArray(1000000,200);
+//	}
+//	public void testPutItemArray3M() {
+//		PutItemArray(3000000,200);
+//	}
+//	public void testPutItemArray10M() {
+//		PutItemArray(10000000,200);
+//	}
+//	public void testPutItem100k() {
+//		PutItem(100000);
+//	}
+//	public void testPutItem500k() {
+//		PutItem(500000);
+//	}	
+//	public void testPutItem1M() {
+//		PutItem(1000000);
+//	}	
+	private void testPutItem3M() {
 		PutItem(3000000);
 	}	
-	public void testPutItem10M() {
-		PutItem(10000000);
+//	public void testPutItem10M() {
+//		PutItem(10000000);
+//	}	
+	private void testSearch(long total){
+		
+		String tk=total/1000+"k";
+		TimeMeasure.begin("getItems"+tk);
+		//getItems
+		this.getItems();
+		TimeMeasure.end("getItems"+tk);
+		TimeMeasure.begin("xPathSearch"+tk);
+		//xPathSearch()
+		this.xPathSearch();
+		TimeMeasure.end("xPathSearch"+tk);
+	}
+	private void testDelete(long total){
+		WSDeleteItems wsDeleteItems=new WSDeleteItems(); 
+		wsDeleteItems.setConceptName("Country");
+		wsDeleteItems.setWsDataClusterPK(new WSDataClusterPK("Order"));
+		try {
+			
+			String tk=total/1000+"k";
+			TimeMeasure.begin("deleteItems"+tk);
+			WSInt wsint=defaultPort.deleteItems(wsDeleteItems);
+			TimeMeasure.end("deleteItems"+tk);
+		} catch (RemoteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}	
+	public void testPerformance(){
+		int total=5000;//1000,5000,10000
+		PutItemArray(total,200);
+		testSearch(total);
+		testDelete(total);
+	}
+	private void xPathSearch(){
+		WSXPathsSearch wsXPathsSearch=new WSXPathsSearch(); 
+		wsXPathsSearch.setWsDataClusterPK(new WSDataClusterPK("Order"));
+		wsXPathsSearch.setPivotPath("Country/isoCode");
+		wsXPathsSearch.setViewablePaths(new String[]{"Country/isoCode"});
+		try {
+			String[] itemArray=defaultPort.xPathsSearch(wsXPathsSearch);
+			System.out.println(itemArray.length);
+		} catch (RemoteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	private void getItems(){
+		WSGetItems wsGetItems=new WSGetItems();
+		wsGetItems.setConceptName("Country");
+		wsGetItems.setWsDataClusterPK(new WSDataClusterPK("Order"));
+		try {
+			String[] itemArray=defaultPort.getItems(wsGetItems);
+			System.out.println(itemArray.length);
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		}
+	}
 	private void PutItemArray(long total, int arraysize) {
 		try {
 			//create datacluster Order100k
@@ -83,6 +144,7 @@ public class BenchmarkTestCase extends WebserviceTestCase {
 			e.printStackTrace();
 		}
 	}
+
 	private void PutItem(long total) {
 		try {
 			//create datacluster Order100k
