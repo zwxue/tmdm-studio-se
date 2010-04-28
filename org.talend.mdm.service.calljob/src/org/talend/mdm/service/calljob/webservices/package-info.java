9bc4f049b98ab96c997b4202fcaf68f1ef09b00b
achen <achen@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2010-04-28 03:18:23 +0000

    move to tom
    git-svn-id: http://talendforge.org/svn/tem/trunk@41271 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.service.calljob/src/org/talend/mdm/service/calljob/webservices/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://talend.org", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package org.talend.mdm.service.calljob.webservices;
