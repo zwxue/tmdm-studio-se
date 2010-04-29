d7389ffca9659a8e638f7571b72b41c8d2670ee1
achen <achen@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2010-04-29 02:36:31 +0000

    do license check when init mdm perspective
    git-svn-id: http://talendforge.org/svn/tom/trunk@41343 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.workbench/src/com/amalto/workbench/views/MDMPerspective.java
+++ org.talend.mdm.workbench/src/com/amalto/workbench/views/MDMPerspective.java
@@ -4,10 +4,14 @@ import org.eclipse.ui.IFolderLayout;
 import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory;
 
+import com.amalto.workbench.availablemodel.AvailableModelUtil;
+
 public class MDMPerspective implements IPerspectiveFactory {
 	public static final String PERPECTIVE_ID="org.talend.mdm.perspective";
 
 	public void createInitialLayout(IPageLayout layout) {
+		//scan availabelmodel extention point
+    	AvailableModelUtil.getAvailableModels();		
         // leftTopLayout
         IFolderLayout leftTopLayout = layout.createFolder("navigatorLayout", IPageLayout.LEFT, new Float(0.32), IPageLayout.ID_EDITOR_AREA);
         leftTopLayout.addView(ServerView.VIEW_ID);
