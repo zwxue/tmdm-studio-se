29b6acad0728875a55c502e680059cefb0a09b6a
rhou <rhou@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2010-07-15 09:01:24 +0000

    trunk:svn_tom:fix bug 0014342: When renaming an object, if it is currently open, the tab must close prior to the rename.
    git-svn-id: http://talendforge.org/svn/tom/trunk@45488 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.workbench/src/com/amalto/workbench/actions/RenameXObjectAction.java
+++ org.talend.mdm.workbench/src/com/amalto/workbench/actions/RenameXObjectAction.java
@@ -3,15 +3,18 @@ package com.amalto.workbench.actions;
 import org.eclipse.jface.action.Action;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.IWorkbenchPage;
 
 import com.amalto.workbench.image.EImage;
 import com.amalto.workbench.image.ImageCache;
 import com.amalto.workbench.models.TreeObject;
+import com.amalto.workbench.providers.XObjectEditorInput;
 import com.amalto.workbench.utils.TreeObjectUtil;
 import com.amalto.workbench.views.ServerView;
 
 public class RenameXObjectAction extends Action {
 	private ServerView view = null;
+	private IWorkbenchPage page;
 	public RenameXObjectAction(ServerView view) {
 		super();
 		this.view = view;
@@ -24,6 +27,8 @@ public class RenameXObjectAction extends Action {
 	public void run() {
 		ISelection selection = view.getViewer().getSelection();
         TreeObject xobject = (TreeObject)((IStructuredSelection)selection).getFirstElement();	
+        this.page = view.getSite().getWorkbenchWindow().getActivePage();
+        page.closeEditor(page.findEditor(new XObjectEditorInput(xobject,xobject.getDisplayName())),true);
         try {
 			if(TreeObjectUtil.renameTreeOjects(xobject, view))
 				TreeObjectUtil.deleteTreeObject(xobject, view);		
