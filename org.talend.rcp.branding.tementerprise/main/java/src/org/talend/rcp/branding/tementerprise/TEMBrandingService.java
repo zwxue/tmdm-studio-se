6fbd86c4d37b3f6efea89bac9bcd53a93d97dc61
achen <achen@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2010-09-01 09:07:26 +0000

    fix compile error
    git-svn-id: http://talendforge.org/svn/tem/trunk@47566 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.rcp.branding.tementerprise/main/java/src/org/talend/rcp/branding/tementerprise/TEMBrandingService.java
+++ org.talend.rcp.branding.tementerprise/main/java/src/org/talend/rcp/branding/tementerprise/TEMBrandingService.java
@@ -20,7 +20,7 @@ import org.eclipse.core.runtime.Path;
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.osgi.framework.Bundle;
-import org.talend.core.ui.branding.AbstractBrandingService;
+import org.talend.core.ui.branding.AbstractTalendBrandingService;
 import org.talend.core.ui.branding.IBrandingConfiguration;
 import org.talend.rcp.branding.tementerprise.i18n.Messages;
 
