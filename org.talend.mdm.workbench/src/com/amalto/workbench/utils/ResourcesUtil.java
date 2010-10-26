32103d4817ea7670b465157e2563ca0019cee683
achen <achen@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2010-10-26 05:26:57 +0000

    fix bug  	 0016579: Contextal 'open' workflow action may overwrite changes done to the workflow
    git-svn-id: http://talendforge.org/svn/tom/trunk@50483 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.workbench/src/com/amalto/workbench/utils/ResourcesUtil.java
+++ org.talend.mdm.workbench/src/com/amalto/workbench/utils/ResourcesUtil.java
@@ -13,9 +13,12 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 
+import org.apache.commons.httpclient.DefaultHttpMethodRetryHandler;
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.HttpException;
+import org.apache.commons.httpclient.HttpStatus;
 import org.apache.commons.httpclient.methods.GetMethod;
+import org.apache.commons.httpclient.params.HttpMethodParams;
 import org.apache.http.Header;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
@@ -39,230 +42,259 @@ import org.dom4j.io.SAXReader;
 import sun.misc.BASE64Encoder;
 
 public class ResourcesUtil {
-	
+
 	// Handle it using an HTTP client connector
-	private static final String dmURI="/pubcomponent/secure/dataModels";
-	private static final String cmURI="/pubcomponent/secure/customTypesSets";
-	private static final String picURI="/pubcomponent/secure/pictures";
+	private static final String dmURI = "/pubcomponent/secure/dataModels";
+	private static final String cmURI = "/pubcomponent/secure/customTypesSets";
+	private static final String picURI = "/pubcomponent/secure/pictures";
 
-    
-	   
+	public static void main(String[] args) {
 
-    public static void main(String[] args) {
+		// init();
 
-//init();
-        
-//    	getXMLString("http://localhost:8080"+picURI);
-//getResourcesNameListFromURI("http://localhost:8080"+cmURI);
-//getResourcesMapFromURI("http://localhost:8080"+cmURI);
-try {
-	postResourcesFromFile("demo2","d:/bud.xsd","http://localhost:8080"+cmURI);
-} catch (Exception e) {
-	e.printStackTrace();
-}
-    }
-    
-    
-
-public static InputStream getFile(String uri) {
-    HttpClient client = new HttpClient();  
-    GetMethod get = new GetMethod(uri);  
-    try {
-		client.executeMethod(get);
-		return get.getResponseBodyAsStream();
-	} catch (HttpException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	} catch (IOException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	} 
-	return null;
-}    
-    
-private static String getXMLString(String uri) {
-    DefaultHttpClient httpclient = new DefaultHttpClient();
-
-    httpclient.getCredentialsProvider().setCredentials(
-            new AuthScope(getEndpointHost(uri),Integer.valueOf(getEndpointPort(uri))), 
-            new UsernamePasswordCredentials("admin", "talend"));
-    
-    HttpGet httpget = new HttpGet(uri);
-    
-    System.out.println("executing request" + httpget.getRequestLine());
-    
-    // Create a response handler
-    ResponseHandler<String> responseHandler = new BasicResponseHandler();
-    String responseBody ="";
-	try {
-		responseBody = httpclient.execute(httpget, responseHandler);
-	} catch (ClientProtocolException e) {
-		e.printStackTrace();
-	} catch (IOException e) {
-		e.printStackTrace();
+		// getXMLString("http://localhost:8080"+picURI);
+		// getResourcesNameListFromURI("http://localhost:8080"+cmURI);
+		// getResourcesMapFromURI("http://localhost:8080"+cmURI);
+		try {
+			postResourcesFromFile("demo2", "d:/bud.xsd",
+					"http://localhost:8080" + cmURI);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
-    httpclient.getConnectionManager().shutdown();      
-    return responseBody;
+	public static byte[] getResourceFile(String uri) {
 
-}
+	     HttpClient httpClient = new HttpClient();
 
+	     GetMethod getMethod = new GetMethod(uri);
 
+	     getMethod.getParams().setParameter(HttpMethodParams.RETRY_HANDLER,new DefaultHttpMethodRetryHandler());
+	     try
+	     {
+	    	 httpClient.setConnectionTimeout(60000);
+	    	 httpClient.getState().setAuthenticationPreemptive(true);
+	    	 httpClient.getState().setCredentials(null,null, new org.apache.commons.httpclient.UsernamePasswordCredentials("admin","talend"));
 
+	         int statusCode = httpClient.executeMethod(getMethod);
+	         if (statusCode != HttpStatus.SC_OK)
+	         {
+	             System.err.println("Method failed: " + getMethod.getStatusLine());
+	         }
 
+	         byte[] responseBody = getMethod.getResponseBody();
+	         return responseBody;
+	     }
+         catch (HttpException e)
+         {
 
-    public static HashMap<String,String> getResourcesMapFromURI(String uri) {
-    	HashMap<String, String> contentMap=new HashMap<String, String>();
-    	String responseBody=getXMLString(uri);
-    	Document document=parsXMLString(responseBody);
-    	for (Iterator iterator = document.getRootElement().elementIterator("entry"); iterator.hasNext();) {
+             System.out.println("Please check your provided http address!");
+             e.printStackTrace();
+         }
+         catch (IOException e)
+         {
+             e.printStackTrace();
+         }
+         finally
+         {
+             getMethod.releaseConnection();
+         }
+         return null;
+	}
+	
+	public static InputStream getFile(String uri) {
+		HttpClient client = new HttpClient();
+		GetMethod get = new GetMethod(uri);
+		try {
+			client.executeMethod(get);
+			return get.getResponseBodyAsStream();
+		} catch (HttpException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	private static String getXMLString(String uri) {
+		DefaultHttpClient httpclient = new DefaultHttpClient();
+
+		httpclient.getCredentialsProvider().setCredentials(
+				new AuthScope(getEndpointHost(uri), Integer
+						.valueOf(getEndpointPort(uri))),
+				new UsernamePasswordCredentials("admin", "talend"));
+
+		HttpGet httpget = new HttpGet(uri);
+
+		System.out.println("executing request" + httpget.getRequestLine());
+
+		// Create a response handler
+		ResponseHandler<String> responseHandler = new BasicResponseHandler();
+		String responseBody = "";
+		try {
+			responseBody = httpclient.execute(httpget, responseHandler);
+		} catch (ClientProtocolException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		httpclient.getConnectionManager().shutdown();
+		return responseBody;
+
+	}
+
+	public static HashMap<String, String> getResourcesMapFromURI(String uri) {
+		HashMap<String, String> contentMap = new HashMap<String, String>();
+		String responseBody = getXMLString(uri);
+		Document document = parsXMLString(responseBody);
+		for (Iterator iterator = document.getRootElement().elementIterator(
+				"entry"); iterator.hasNext();) {
 			Element element = (Element) iterator.next();
-			Element nameElement=element.element("name");
-			Element uriElement=element.element("uri");
-			if(nameElement!=null&&uriElement!=null){
-				contentMap.put(nameElement.getStringValue(),uriElement.getStringValue());
-			}
-			else{
-				contentMap.put(element.getStringValue(),uri+"/"+element.getStringValue());
+			Element nameElement = element.element("name");
+			Element uriElement = element.element("uri");
+			if (nameElement != null && uriElement != null) {
+				contentMap.put(nameElement.getStringValue(), uriElement
+						.getStringValue());
+			} else {
+				contentMap.put(element.getStringValue(), uri + "/"
+						+ element.getStringValue());
 			}
 		}
-		return contentMap;   	
-    }
+		return contentMap;
+	}
 
-
-
-
-
-
-	public static List<String> getResourcesNameListFromURI(String uri)  throws Exception{
-    	List<String> nameList=new ArrayList<String>();
-    	String responseBody=getXMLString(uri);
-//    	nameList=getNameList(responseBody);
-    	Document document=parsXMLString(responseBody);
-    	for (Iterator iterator = document.getRootElement().elementIterator("entry"); iterator.hasNext();) {
+	public static List<String> getResourcesNameListFromURI(String uri)
+			throws Exception {
+		List<String> nameList = new ArrayList<String>();
+		String responseBody = getXMLString(uri);
+		// nameList=getNameList(responseBody);
+		Document document = parsXMLString(responseBody);
+		for (Iterator iterator = document.getRootElement().elementIterator(
+				"entry"); iterator.hasNext();) {
 			Element element = (Element) iterator.next();
-			Element nameElement=element.element("name");
-			if(nameElement!=null){
+			Element nameElement = element.element("name");
+			if (nameElement != null) {
 				nameList.add(nameElement.getStringValue());
-			}
-			else{
+			} else {
 				nameList.add(element.getStringValue());
 			}
 		}
-    	return nameList; 
-    	
-    }
-	public static void postResourcesFromFile(String typeName,String pathName,String uri) throws Exception  {
-		File file=new File(pathName);
-		FileReader fr=new FileReader(file);
+		return nameList;
+
+	}
+
+	public static void postResourcesFromFile(String typeName, String pathName,
+			String uri) throws Exception {
+		File file = new File(pathName);
+		FileReader fr = new FileReader(file);
 		BufferedReader br = new BufferedReader(fr);
-		postResourcesFromContentString(br.readLine(),uri,typeName);
+		postResourcesFromContentString(br.readLine(), uri, typeName);
 		br.close();
 		fr.close();
-		
+
 	}
-	//it does not work,there is already a method "uploadImageFile" in Util, developer can use that method to upload a image file.
-/*	public static void postPicFromFile(String typeName,String pathName,String uriPre) throws Exception  {
-		String content=readFileAsString(pathName);
-		String uri=uriPre+"imageserver/secure/ImageUploadServlet";
-//		String uri=uriPre+picURI;
-		postResourcesFromContentString(content,uri,typeName);
-	}*/
-	 private static String readFileAsString(String fileName) throws Exception {  
-		             FileInputStream fis = new FileInputStream(fileName);  
-		             BufferedInputStream in = new BufferedInputStream(fis);  
-		             byte buffer[] = new byte[256];  
-		             StringBuffer picStr=new StringBuffer();  
-		             BASE64Encoder base64=new BASE64Encoder();  
-		             while (in.read(buffer)>= 0){  
-		                 picStr.append(base64.encode(buffer));  
-		             }  
-		             fis.close();  
-		             fis=null;  
-		             in.close();  
-		             in=null;  
-		             buffer=null;  
-		             return picStr.toString();  
-		         }  
-	public static void postResourcesFromContentString(String content,String uri,String typeName)  throws Exception{
+
+	// it does not work,there is already a method "uploadImageFile" in Util,
+	// developer can use that method to upload a image file.
+	/*
+	 * public static void postPicFromFile(String typeName,String pathName,String
+	 * uriPre) throws Exception { String content=readFileAsString(pathName);
+	 * String uri=uriPre+"imageserver/secure/ImageUploadServlet"; // String
+	 * uri=uriPre+picURI; postResourcesFromContentString(content,uri,typeName);
+	 * }
+	 */
+	private static String readFileAsString(String fileName) throws Exception {
+		FileInputStream fis = new FileInputStream(fileName);
+		BufferedInputStream in = new BufferedInputStream(fis);
+		byte buffer[] = new byte[256];
+		StringBuffer picStr = new StringBuffer();
+		BASE64Encoder base64 = new BASE64Encoder();
+		while (in.read(buffer) >= 0) {
+			picStr.append(base64.encode(buffer));
+		}
+		fis.close();
+		fis = null;
+		in.close();
+		in = null;
+		buffer = null;
+		return picStr.toString();
+	}
+
+	public static void postResourcesFromContentString(String content,
+			String uri, String typeName) throws Exception {
 		System.out.println(content);
 		HttpPost httppost = new HttpPost(uri);
 		List<BasicNameValuePair> nvps = new ArrayList<BasicNameValuePair>();
 		nvps.add(new BasicNameValuePair("domainObjectName", typeName));
 		nvps.add(new BasicNameValuePair("domainObjectContent", content));
-		httppost.setEntity(new UrlEncodedFormEntity( nvps, HTTP.UTF_8));
-		postContent(uri,httppost);
-	}
-	
-	private static void postContent(String uri, HttpPost httppost) throws Exception, IOException {
-	    DefaultHttpClient httpclient = new DefaultHttpClient();
-
-	    httpclient.getCredentialsProvider().setCredentials(
-	            new AuthScope(getEndpointHost(uri),Integer.valueOf(getEndpointPort(uri))), 
-	            new UsernamePasswordCredentials("admin", "talend"));
-	    
-	    System.out.println("executing request" + httppost.getRequestLine());
-	    HttpResponse response = httpclient.execute(httppost);
-	    HttpEntity entity = response.getEntity();
-
-	        try{
-	            Header[] headers = response.getAllHeaders();                    
-	            String responseString = null;
-	            if (response.getEntity() != null) {
-	            responseString = EntityUtils.toString(response.getEntity());      
-	            }
-	        } finally {
-	            if (entity != null)                          
-	            entity.consumeContent();                                                   // release connection gracefully
-	        }
-	        if (entity != null) {
-	        entity.consumeContent();
-	        }
-	    
-	    httpclient.getConnectionManager().shutdown();      
+		httppost.setEntity(new UrlEncodedFormEntity(nvps, HTTP.UTF_8));
+		postContent(uri, httppost);
 	}
 
+	private static void postContent(String uri, HttpPost httppost)
+			throws Exception, IOException {
+		DefaultHttpClient httpclient = new DefaultHttpClient();
 
+		httpclient.getCredentialsProvider().setCredentials(
+				new AuthScope(getEndpointHost(uri), Integer
+						.valueOf(getEndpointPort(uri))),
+				new UsernamePasswordCredentials("admin", "talend"));
 
+		System.out.println("executing request" + httppost.getRequestLine());
+		HttpResponse response = httpclient.execute(httppost);
+		HttpEntity entity = response.getEntity();
 
+		try {
+			Header[] headers = response.getAllHeaders();
+			String responseString = null;
+			if (response.getEntity() != null) {
+				responseString = EntityUtils.toString(response.getEntity());
+			}
+		} finally {
+			if (entity != null)
+				entity.consumeContent(); // release connection gracefully
+		}
+		if (entity != null) {
+			entity.consumeContent();
+		}
+
+		httpclient.getConnectionManager().shutdown();
+	}
 
 	private static Document parsXMLString(String responseBody) {
-        SAXReader saxReader = new SAXReader();   
-        Document document = null;   
-        try  
-        {   
-            document = saxReader.read(new StringReader(responseBody));   
-        } catch (DocumentException e)   
-        {   
-            e.printStackTrace();   
-            return null;   
-        }
+		SAXReader saxReader = new SAXReader();
+		Document document = null;
+		try {
+			document = saxReader.read(new StringReader(responseBody));
+		} catch (DocumentException e) {
+			e.printStackTrace();
+			return null;
+		}
 		return document;
-		
+
 	}
 
-
 	public static String getEndpointHost(String uri) {
-		
-		
-		if(uri!=null){
-			int startPos=uri.indexOf("//")+2;
-			int endPos=uri.indexOf(":",startPos);
-			if(endPos!=-1&&startPos!=-1)
+
+		if (uri != null) {
+			int startPos = uri.indexOf("//") + 2;
+			int endPos = uri.indexOf(":", startPos);
+			if (endPos != -1 && startPos != -1)
 				return uri.substring(startPos, endPos);
 		}
-		
+
 		return uri;
-	}	
-	
+	}
+
 	public static String getEndpointPort(String uri) {
-		
-		
-		if(uri!=null){
-			String[] splitString=uri.split(":",3);
-			if(splitString[splitString.length-1]!=null)
+
+		if (uri != null) {
+			String[] splitString = uri.split(":", 3);
+			if (splitString[splitString.length - 1] != null)
 				return splitString[2].substring(0, splitString[2].indexOf("/"));
 		}
-		
+
 		return uri;
-	}		
+	}
 }
