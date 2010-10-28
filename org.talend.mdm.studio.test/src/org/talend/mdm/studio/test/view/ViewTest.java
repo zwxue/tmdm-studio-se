395b83fb55b8e50dd278933254b219195ce63029
achen <achen@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2010-10-28 07:21:54 +0000

    trunk:svn_tom:fix bug 0016511: Entity filtering in select multiple xpath dialog. Add SWTBot test case for this new feature.Please check out the project org.talend.mdm.studio.test from http://www.talendforge.org/svn/tom.
    git-svn-id: http://talendforge.org/svn/tom/trunk@50664 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.studio.test/src/org/talend/mdm/studio/test/view/ViewTest.java
+++ org.talend.mdm.studio.test/src/org/talend/mdm/studio/test/view/ViewTest.java
@@ -1,4 +1,5 @@
 package org.talend.mdm.studio.test.view;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotShell;
 import org.eclipse.swtbot.swt.finder.widgets.SWTBotTreeItem;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -11,12 +12,37 @@ public class ViewTest extends StudioTest{
 	}
 	@Test
 	public void canCreateView() throws Exception {
-		
+		//Create a new view to test the filter of the xpath.
 		SWTBotTreeItem viewNode=serverTree.expandNode("http://localhost:8080/talend/TalendPort [HEAD] admin").getNode("View [HEAD]");
 		viewNode.contextMenu("New").click();
 		//bot.sleep(1000);
-		bot.shell("New View").activate();		
-		bot.buttonWithLabel("...").click();	
+		SWTBotShell newViewShell=bot.shell("New View");
+		newViewShell.activate();		
+//		bot.buttonWithLabel("...").click();	
+	    bot.buttonWithTooltip("Select one Entity").click(); 
 		bot.shell("Select one Entity").activate();
+		
+		//Test filter of the system data model "PROVISIONING"
+		bot.comboBoxWithLabel("Data Models:").setSelection("PROVISIONING");
+		bot.textWithLabel("Filter:").setText("R");
+		bot.sleep(1000);
+		bot.textWithLabel("Filter:").setText("U");
+		bot.sleep(1000);
+		bot.textWithLabel("Filter:").setText("");
+		bot.sleep(1000);
+		
+	    //Test filter of the system data model "CONF"
+        bot.comboBoxWithLabel("Data Models:").setSelection("CONF");
+        bot.textWithLabel("Filter:").setText("Au");
+        bot.sleep(1000);
+        bot.textWithLabel("Filter:").setText("Con");
+        bot.sleep(1000);
+        bot.textWithLabel("Filter:").setText("");
+        bot.sleep(1000);
+        
+        bot.button("Cancel").click();
+//        newViewShell.activate();
+        bot.button("Cancel").click();
+        
 	}
 }
