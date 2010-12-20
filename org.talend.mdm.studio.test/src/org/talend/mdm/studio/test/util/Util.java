8801f42d1303553e19fba104f5f206e466e0b24e
rhou <rhou@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2010-12-20 07:44:54 +0000

    trunk:svn_tem:commit the omissive modifies.
    git-svn-id: http://talendforge.org/svn/tom/trunk@52901 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.studio.test/src/org/talend/mdm/studio/test/util/Util.java
+++ org.talend.mdm.studio.test/src/org/talend/mdm/studio/test/util/Util.java
@@ -3,12 +3,32 @@ package org.talend.mdm.studio.test.util;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 import java.util.Date;
 
+import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.URIUtil;
+
+/**
+ * DOC rhou class global comment. Detailled comment
+ */
 public class Util {
 
+    private static String SAMPLE_FOLDER_PATH = "/sample/";//$NON-NLS-1$
+
+    /**
+     * Copy a file with the FileChannel that will accelerate the process.
+     * 
+     * @param inputFile
+     * @param outFile
+     * @return
+     * @throws Exception
+     */
     public static long copyFile(File inputFile, File outFile) throws Exception {
         long time = new Date().getTime();
         int length = 2097152;
@@ -33,5 +53,33 @@ public class Util {
             outC.write(b);
             outC.force(false);
         }
+    }
+
+    /**
+     * Return a File instance pointing to the file or folder located below this plugin sample folder
+     * 
+     * @param subSamplePath, path located below /sample/, so subpath should never start with a /, example : test.xsl
+     * @return
+     * @throws IOException
+     * @throws URISyntaxException
+     */
+    public static File getFileFromCurrentPluginSampleFolder(String subSamplePath) throws IOException, URISyntaxException {
+        URL url = Util.class.getResource(SAMPLE_FOLDER_PATH + subSamplePath);
+        URI escapedURI = getFileUrl(url);
+        return URIUtil.toFile(escapedURI);
+    }
+
+    /**
+     * Convert any URL to a file URL
+     * 
+     * @param url,maybe an eclipse URL
+     * @return
+     * @throws IOException
+     * @throws URISyntaxException
+     */
+    private static URI getFileUrl(URL url) throws IOException, URISyntaxException {
+        URL unescapedURL = FileLocator.toFileURL(url);
+        URI escapedURI = new URI(unescapedURL.getProtocol(), unescapedURL.getPath(), unescapedURL.getQuery());
+        return escapedURI;
     }
 }
