f26dc7fc880a23847086a3f982ee35267f3e3a9c
yyun <yyun@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2010-12-24 08:42:06 +0000

    feature 17198 updated: fix the bug for not able to edit the foreign key filter in the detail view
    git-svn-id: http://talendforge.org/svn/tom/trunk@53117 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.workbench/src/com/amalto/workbench/detailtabs/sections/providers/ForeignKeyFilterCellModifier.java
+++ org.talend.mdm.workbench/src/com/amalto/workbench/detailtabs/sections/providers/ForeignKeyFilterCellModifier.java
@@ -5,9 +5,10 @@ import java.util.Arrays;
 import org.eclipse.jface.viewers.Viewer;
 
 import com.amalto.workbench.detailtabs.sections.model.annotationinfo.relationship.ForeignKeyFilterAnnoInfoDefUnit;
-import com.amalto.workbench.detailtabs.sections.model.annotationinfo.relationship.ForeignKeyAnnoInfo;
 import com.amalto.workbench.providers.ArrayComboCellEditorIndexExtractor;
+import com.amalto.workbench.providers.ArrayComboCellEditorValueModifier;
 import com.amalto.workbench.providers.CellEditorValueExtractor;
+import com.amalto.workbench.providers.CellEditorValueModifier;
 import com.amalto.workbench.providers.CommonTableCellModifier;
 import com.amalto.workbench.utils.IConstants;
 
@@ -34,10 +35,19 @@ public class ForeignKeyFilterCellModifier extends CommonTableCellModifier<Foreig
         prop2ValueIndexExtractor.put(COL_PROP_VALUE, new CellEditorValueExtractor<ForeignKeyFilterAnnoInfoDefUnit>("getValue"));
         prop2ValueIndexExtractor.put(COL_PROP_PREDICATE, new ArrayComboCellEditorIndexExtractor<ForeignKeyFilterAnnoInfoDefUnit>(
                 IConstants.PREDICATES, "getPredicate"));
+
+        prop2ValueModifier.put(COL_PROP_XPATH, new CellEditorValueModifier<ForeignKeyFilterAnnoInfoDefUnit>("setXpath",
+                "getXpath"));
+        prop2ValueModifier.put(COL_PROP_OPERATOR, new ArrayComboCellEditorValueModifier<ForeignKeyFilterAnnoInfoDefUnit>(
+                IConstants.VIEW_CONDITION_OPERATORS, "setOperator", "getOperator"));
+        prop2ValueModifier.put(COL_PROP_VALUE, new CellEditorValueModifier<ForeignKeyFilterAnnoInfoDefUnit>("setValue",
+                "getValue"));
+        prop2ValueModifier.put(COL_PROP_PREDICATE, new ArrayComboCellEditorValueModifier<ForeignKeyFilterAnnoInfoDefUnit>(
+                IConstants.PREDICATES, "setPredicate", "getPredicate"));
     }
 
     public boolean canModify(Object element, String property) {
-        return (element instanceof ForeignKeyAnnoInfo) && Arrays.asList(COLPROPS).contains(property);
+        return (element instanceof ForeignKeyFilterAnnoInfoDefUnit) && Arrays.asList(COLPROPS).contains(property);
     }
 
     @Override
