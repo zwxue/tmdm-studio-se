943c27fca65f71cbea27e9ebaaf743559b2382c5
rhou <rhou@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2010-12-30 09:52:53 +0000

    trunk:svn_tem:Add test cases for the operation of SychronizationContent,VersionItem and VersionParent.
    git-svn-id: http://talendforge.org/svn/tom/trunk@53350 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.mdm.studio.test/src/org/talend/mdm/studio/test/synchronization/SynchronizationPlanContentOperationTest.java
@@ -0,0 +1,91 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2010 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.mdm.studio.test.synchronization;
+
+import org.eclipse.swtbot.swt.finder.junit.SWTBotJunit4ClassRunner;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotShell;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotText;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotTreeItem;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.talend.mdm.studio.test.TalendSWTBotForMDM;
+
+/**
+ * 
+ * DOC rhou class global comment. Detailled comment
+ * 
+ * 
+ */
+@RunWith(SWTBotJunit4ClassRunner.class)
+public class SynchronizationPlanContentOperationTest extends TalendSWTBotForMDM {
+
+    private SWTBotTreeItem spParentItem;
+
+    private SWTBotTreeItem spItem;
+
+    @Before
+    public void runBeforeEveryTest() {
+        spParentItem = serverItem.getNode("Synchronization Plan [HEAD]");
+        spParentItem.expand();
+
+        spParentItem.contextMenu("New").click();
+        SWTBotShell newspShell = bot.shell("New Synchronization Plan");
+        newspShell.activate();
+        SWTBotText text = bot.textWithLabel("Enter a name for the New Instance");
+        text.setText("TestSynchronizationPlan");
+        sleep();
+        bot.buttonWithTooltip("OK").click();
+        spItem = spParentItem.getNode("TestSynchronizationPlan");
+        Assert.assertNotNull(spItem);
+        spItem.doubleClick();
+        sleep(2);
+    }
+
+    @After
+    public void runAfterEveryTest() {
+        spParentItem.getNode("TestSynchronizationPlan").contextMenu("Delete").click();
+        sleep();
+        bot.button("OK").click();
+        sleep();
+    }
+
+    @Test
+    public void setDescriptionTest() {
+        String des = "This is a stored procedure";
+        bot.textWithLabel("Descriptions").setText(des);
+        Assert.assertEquals(des, bot.textWithLabel("Descriptions").getText());
+    }
+
+    @Test
+    public void checkServerTest() {
+        bot.textWithLabel("Name").setText("Test");
+        bot.textWithLabel("URL").setText("http://localhost:8080/talend/TalendPort");
+        bot.textWithLabel("Username").setText("admin");
+        bot.textWithLabel("Password").setText("talend");
+        bot.button("Check").click();
+    }
+
+    @Test
+    public void addRecordsSPTest() {
+        // TODO:need more detailed test codes with version after the version is created.
+    }
+
+    @Test
+    public void checkActionTest() {
+        bot.buttonWithTooltip("Start Full").click();
+        // TODO:need assertion;
+    }
+}
---
+++ org.talend.mdm.studio.test/src/org/talend/mdm/studio/test/version/VersionParentOperationTest.java
@@ -0,0 +1,79 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2010 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.mdm.studio.test.version;
+
+import org.eclipse.swtbot.swt.finder.junit.SWTBotJunit4ClassRunner;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotShell;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotText;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotTreeItem;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.talend.mdm.studio.test.TalendSWTBotForMDM;
+
+/**
+ * 
+ * DOC rhou class global comment. Detailled comment
+ */
+@RunWith(SWTBotJunit4ClassRunner.class)
+public class VersionParentOperationTest extends TalendSWTBotForMDM {
+
+    private SWTBotTreeItem sversionParentItem;
+
+    @Before
+    public void runBeforeEveryTest() {
+        sversionParentItem = serverItem.getNode("Version");
+        sversionParentItem.expand();
+    }
+
+    @Test
+    public void versionCreationTest() {
+        sversionParentItem.contextMenu("New").click();
+        // bot.sleep(1000);
+        SWTBotShell shell = bot.shell("New Version");
+        shell.activate();
+        SWTBotText text = bot.textWithLabel("Enter a name for the new instance");
+        text.setText("TestVersion");
+        bot.buttonWithTooltip("OK").click();
+        Assert.assertNotNull(sversionParentItem.getNode("TestVersion"));
+        sleep(2);
+    }
+
+    @Test
+    public void versionCategoryCreationTest() {
+        sversionParentItem.contextMenu("New Category").click();
+        // bot.sleep(1000);
+        SWTBotShell newCategoryShell = bot.shell("New Category");
+        newCategoryShell.activate();
+        SWTBotText text = bot.textWithLabel("Enter a name for the New Category");
+        text.setText("TestVersionCategory");
+        bot.button("OK").click();
+        Assert.assertNotNull(sversionParentItem.getNode("TestVersionCategory"));
+        sleep(2);
+    }
+
+    @After
+    public void runAfterEveryTest() {
+        sversionParentItem.getNode("TestVersion").contextMenu("Delete").click();
+        sleep();
+        bot.button("OK").click();
+        sleep();
+
+        sversionParentItem.getNode("TestVersionCategory").contextMenu("Delete").click();
+        sleep();
+        bot.button("OK").click();
+        sleep();
+    }
+}
