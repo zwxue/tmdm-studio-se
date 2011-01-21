1193786be3829f71f69463cb9f41ed9909af821c
fliu <fliu@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-01-21 02:59:11 +0000

    feature 0018022  added:Run a job directly from the job repository node
    git-svn-id: http://talendforge.org/svn/tem/trunk@54175 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.jobrepository/src/org/talend/mdm/jobrepository/availablemodel/JobAvailableModel.java
+++ org.talend.mdm.jobrepository/src/org/talend/mdm/jobrepository/availablemodel/JobAvailableModel.java
@@ -22,6 +22,7 @@ import org.talend.core.model.repository.IRepositoryViewObject;
 import org.talend.mdm.jobrepository.JobRepositoryUtil;
 import org.talend.mdm.jobrepository.actions.DeployJobAction;
 import org.talend.mdm.jobrepository.actions.OpenJobAction;
+import org.talend.mdm.jobrepository.actions.RunTisJobAction;
 
 import com.amalto.workbench.availablemodel.AbstractAvailableModel;
 import com.amalto.workbench.models.TreeObject;
@@ -43,34 +44,33 @@ public class JobAvailableModel extends AbstractAvailableModel {
 
         // TIS JOB
         try {
-        	List<IRepositoryViewObject> listJobs=JobRepositoryUtil.getAllTISRepositoryJobs();
-        	if(listJobs!=null && listJobs.size()>0) {
-	            TreeParent tosJob = new TreeParent("Source Jobs", serverRoot, TreeObject.BUILT_IN_CATEGORY_FOLDER, null, null);
-	            for (IRepositoryViewObject o : listJobs) {
-	                Item item = o.getProperty().getItem();
-	                if (item instanceof ProcessItem) {
-	                    String name = o.getLabel() + "_" + o.getProperty().getVersion();
-	                    //add the category information for the jobs.
-	                    String path=((ProcessItem)item).getState().getPath();
-	                    System.out.println("name-->"+name+ " path--->"+path);
-	                    TreeObject obj = new TreeObject(name, serverRoot, TreeObject.TIS_JOB, o, null);
-	                    obj.setWsObject(new OpenJobAction());
+            List<IRepositoryViewObject> listJobs = JobRepositoryUtil.getAllTISRepositoryJobs();
+            if (listJobs != null && listJobs.size() > 0) {
+                TreeParent tosJob = new TreeParent("Source Jobs", serverRoot, TreeObject.BUILT_IN_CATEGORY_FOLDER, null, null);
+                for (IRepositoryViewObject o : listJobs) {
+                    Item item = o.getProperty().getItem();
+                    if (item instanceof ProcessItem) {
+                        String name = o.getLabel() + "_" + o.getProperty().getVersion();
+                        // add the category information for the jobs.
+                        String path = ((ProcessItem) item).getState().getPath();
+                        System.out.println("name-->" + name + " path--->" + path);
+                        TreeObject obj = new TreeObject(name, serverRoot, TreeObject.TIS_JOB, o, null);
+                        obj.setWsObject(new OpenJobAction());
                         if (path != null && !"".equalsIgnoreCase(path)) {
                             String[] categories = path.split("/");
                             TreeParent parentFolder = null;
                             for (String folder : categories) {
                                 if (parentFolder == null)
                                     parentFolder = tosJob;
-                                parentFolder = addCategory(serverRoot,
-                                        parentFolder, folder);
+                                parentFolder = addCategory(serverRoot, parentFolder, folder);
                             }
                             parentFolder.addChild(obj);
                         } else
                             tosJob.addChild(obj);
-	                }
-	            }
-	            jobs.addChild(tosJob);
-        	}
+                    }
+                }
+                jobs.addChild(tosJob);
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -78,12 +78,12 @@ public class JobAvailableModel extends AbstractAvailableModel {
         monitor.worked(1);
     }
 
-    private TreeParent addCategory(TreeParent serverRoot,TreeParent parentFolder, String folder) {
-           TreeObject jobFolder =parentFolder.findObject(TreeObject.BUILT_IN_CATEGORY_FOLDER, folder);
-               if(jobFolder==null)
-                   jobFolder=new TreeParent(folder, serverRoot, TreeObject.BUILT_IN_CATEGORY_FOLDER, null, null);
-               parentFolder.addChild(jobFolder,false);
-        return (TreeParent)jobFolder;
+    private TreeParent addCategory(TreeParent serverRoot, TreeParent parentFolder, String folder) {
+        TreeObject jobFolder = parentFolder.findObject(TreeObject.CATEGORY_FOLDER, folder);
+        if (jobFolder == null)
+            jobFolder = new TreeParent(folder, serverRoot, TreeObject.CATEGORY_FOLDER, null, null);
+        parentFolder.addChild(jobFolder, false);
+        return (TreeParent) jobFolder;
     }
 
     @Override
@@ -92,6 +92,7 @@ public class JobAvailableModel extends AbstractAvailableModel {
             manager.removeAll();
             manager.add(new DeployJobAction());
             manager.add(new OpenJobAction());
+            manager.add(new RunTisJobAction());
         }
     }
 
