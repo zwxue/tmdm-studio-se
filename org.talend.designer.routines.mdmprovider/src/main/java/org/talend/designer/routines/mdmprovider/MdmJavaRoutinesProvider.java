e2195a59a6b55ef09d9aefc9e95b6c21f744ddc2
achen <achen@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-04-07 08:58:23 +0000

    fix bug  	 0017822: System MDM routines included out of the box
    git-svn-id: http://talendforge.org/svn/tem/trunk@58108 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.designer.routines.mdmprovider/src/main/java/org/talend/designer/routines/mdmprovider/MdmJavaRoutinesProvider.java
+++ org.talend.designer.routines.mdmprovider/src/main/java/org/talend/designer/routines/mdmprovider/MdmJavaRoutinesProvider.java
@@ -44,11 +44,11 @@ public class MdmJavaRoutinesProvider implements IRoutinesProvider {
      * @see org.talend.core.model.routines.IRoutinesProvider#getTalendRoutinesFolder()
      */
     public URL getTalendRoutinesFolder() throws IOException {
-        URL url = Activator.getDefault().getBundle().getEntry("resources/java/routines/system"); //$NON-NLS-1$
+        URL url = Activator.getDefault().getBundle().getEntry("resources/java/routines"); //$NON-NLS-1$
         return FileLocator.resolve(url);
     }
 
     public List<URL> getTalendRoutines() {
-        return FilesUtils.getFilesFromFolder(Activator.getDefault().getBundle(), "resources/java/routines/system", ".java"); //$NON-NLS-1$ //$NON-NLS-2$
+        return FilesUtils.getFilesFromFolder(Activator.getDefault().getBundle(), "resources/java/routines", ".java"); //$NON-NLS-1$ //$NON-NLS-2$
     }
 }
