a513d3e746108727f00e81745153d864299f25c4
pbabin <pbabin@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-04-21 07:01:13 +0000

    Feature 18845 : Fix NPE, warnings,...
    git-svn-id: http://talendforge.org/svn/tom/trunk@58960 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.workbench/src/com/amalto/workbench/utils/MDMServerHelper.java
+++ org.talend.mdm.workbench/src/com/amalto/workbench/utils/MDMServerHelper.java
@@ -14,7 +14,6 @@ package com.amalto.workbench.utils;
 
 import java.io.File;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 import org.apache.commons.logging.Log;
@@ -29,32 +28,36 @@ import org.eclipse.core.runtime.Platform;
 import com.amalto.workbench.dialogs.LoginDialog;
 
 public class MDMServerHelper {
-    private static String workbenchConfigFile = Platform.getInstanceLocation().getURL().getPath() + "/mdm_workbench_config.xml";//$NON-NLS-1$
+
+    private static String workbenchConfigFile = Platform.getInstanceLocation().getURL().getPath() + "/mdm_workbench_config.xml"; //$NON-NLS-1$
+
     private static Log log = LogFactory.getLog(LoginDialog.class);
 
-    public static List<MDMServerDef> getServers(){
-    	SAXReader reader = new SAXReader();
-        Element root = null;
-        Document logininfoDocument=null;
+    public static List<MDMServerDef> getServers() {       
+        List<MDMServerDef> defs = new ArrayList<MDMServerDef>();        
+        Element root;
+        Document logininfoDocument = null;
         if (new File(workbenchConfigFile).exists()) {
             try {
+                SAXReader reader = new SAXReader();
                 logininfoDocument = reader.read(new File(workbenchConfigFile));
             } catch (DocumentException e) {
                 log.error(e.getMessage(), e);
+                return defs;
             }
             root = logininfoDocument.getRootElement();
         } else {
             logininfoDocument = DocumentHelper.createDocument();
-            root = logininfoDocument.addElement("MDMServer");//$NON-NLS-1$
+            root = logininfoDocument.addElement("MDMServer"); //$NON-NLS-1$
         }
-        List<MDMServerDef> defs=new ArrayList<MDMServerDef>();
-        List properties = root.elements("properties");//$NON-NLS-1$
-        for (Iterator iterator = properties.iterator(); iterator.hasNext();) {
-            Element ele = (Element) iterator.next();
-            String password=ele.element("password").getText();
-            password=PasswordUtil.decryptPassword(password);
-            MDMServerDef def=MDMServerDef.parse(ele.element("url").getText(), ele.element("user").getText(), password, ele.element("desc").getText());
-            defs.add(def);            
+        
+        List<Element> properties = root.elements("properties"); //$NON-NLS-1$
+        for (Element ele : properties) {
+            String password = ele.element("password").getText(); //$NON-NLS-1$
+            password = PasswordUtil.decryptPassword(password);
+            MDMServerDef def = MDMServerDef.parse(ele.element("url").getText(), ele.element("user").getText(), password, ele //$NON-NLS-1$ //$NON-NLS-2$
+                    .element("desc").getText()); //$NON-NLS-1$
+            defs.add(def);
         }
         return defs;
     }
