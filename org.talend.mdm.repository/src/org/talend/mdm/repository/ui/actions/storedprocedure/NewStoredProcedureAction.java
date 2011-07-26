767b52b9e7f4251a007da120cfc4fef9900c9482
jsxie <jsxie@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-07-26 10:00:30 +0000

    finish the version part of bug 23401
    git-svn-id: http://talendforge.org/svn/tom/trunk@64998 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.mdm.repository/src/org/talend/mdm/repository/ui/actions/storedprocedure/NewStoredProcedureAction.java
@@ -0,0 +1,82 @@
+// ============================================================================
+//
+// Talend Community Edition
+//
+// Copyright (C) 2006-2011 Talend ¨C www.talend.com
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+// ============================================================================
+package org.talend.mdm.repository.ui.actions.storedprocedure;
+
+import org.talend.core.model.properties.ItemState;
+import org.talend.core.model.properties.PropertiesFactory;
+import org.talend.mdm.repository.i18n.Messages;
+import org.talend.mdm.repository.model.mdmproperties.MdmpropertiesFactory;
+import org.talend.mdm.repository.model.mdmproperties.WSStoredProcedureItem;
+import org.talend.mdm.repository.model.mdmserverobject.MdmserverobjectFactory;
+import org.talend.mdm.repository.model.mdmserverobject.WSStoredProcedureE;
+import org.talend.mdm.repository.ui.actions.AbstractSimpleAddAction;
+import org.talend.mdm.repository.utils.RepositoryResourceUtil;
+
+/**
+ * DOC class global comment. Detailled comment <br/>
+ * 
+ */
+public class NewStoredProcedureAction extends AbstractSimpleAddAction {
+
+
+    public NewStoredProcedureAction() {
+        super();
+    }
+
+
+    @Override
+    protected String getDialogTitle() {
+        return Messages.NewStoredProcedureAction_newStoredProcedure;
+    }
+
+    private WSStoredProcedureE newStoredProcedure(String key) {
+
+
+        WSStoredProcedureE storedProcedure = MdmserverobjectFactory.eINSTANCE.createWSStoredProcedureE();
+
+        storedProcedure.setName(key);
+        storedProcedure.setDescription(""); //$NON-NLS-1$
+        storedProcedure.setProcedure("");//$NON-NLS-1$
+        storedProcedure.setRefreshCache(false);
+
+        return storedProcedure;
+    }
+
+    protected boolean createServerObject(String key) {
+
+        WSStoredProcedureItem item = MdmpropertiesFactory.eINSTANCE.createWSStoredProcedureItem();
+
+        ItemState itemState = PropertiesFactory.eINSTANCE.createItemState();
+        item.setState(itemState);
+        //
+        WSStoredProcedureE storedProcedure = newStoredProcedure(key);
+        item.setWsStoredProcedure(storedProcedure);
+
+        if (parentItem != null) {
+            item.getState().setPath(parentItem.getState().getPath());
+            return RepositoryResourceUtil.createItem(item, key);
+        }
+        return true;
+    }
+
+
+}
