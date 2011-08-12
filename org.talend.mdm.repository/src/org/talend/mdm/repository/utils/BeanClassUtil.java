c42c6ad2d721fe0e17bc244839140eb005da098c
hbhong <hbhong@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-08-12 09:46:00 +0000

    fix feature TMDM-2283: fix can't convert primitive type in java class
    git-svn-id: http://talendforge.org/svn/tom/trunk@65958 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.repository/src/org/talend/mdm/repository/utils/BeanClassUtil.java
+++ org.talend.mdm.repository/src/org/talend/mdm/repository/utils/BeanClassUtil.java
@@ -132,6 +132,8 @@ class BeanClassUtil {
     }
 
     private boolean isJavaClass(Class cls) {
+        if (cls.isPrimitive())
+            return true;
         return getComponentType(cls).getName().startsWith("java"); //$NON-NLS-1$
     }
 
