c2be3a0b10808d6d56aa8eb49faf408da88686d7
hbhong <hbhong@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-09-07 09:29:32 +0000

    add feature TMDM-2450: Adjust view layout in the MDM perspective 1.hide server view 2.adjust MDM Repository + MDM Server explorer view location and radio 3.reset properties view location
    git-svn-id: http://talendforge.org/svn/tom/trunk@67468 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.workbench/src/com/amalto/workbench/views/MDMPerspective.java
+++ org.talend.mdm.workbench/src/com/amalto/workbench/views/MDMPerspective.java
@@ -12,7 +12,6 @@
 // ============================================================================
 package com.amalto.workbench.views;
 
-import org.eclipse.ui.IFolderLayout;
 import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory;
 
@@ -32,13 +31,14 @@ public class MDMPerspective implements IPerspectiveFactory {
     }
 
     public void createInitialLayout(IPageLayout layout) {
-
+        // from 5.0 m4 it will disappear from perspective
         // leftTopLayout
-        IFolderLayout leftTopLayout = layout.createFolder("navigatorLayout", IPageLayout.LEFT, new Float(0.32),//$NON-NLS-1$
-                IPageLayout.ID_EDITOR_AREA);
-        leftTopLayout.addView(ServerView.VIEW_ID);
+        //        IFolderLayout leftTopLayout = layout.createFolder("navigatorLayout", IPageLayout.LEFT, new Float(0.32),//$NON-NLS-1$
+        // IPageLayout.ID_EDITOR_AREA);
 
-        layout.addPlaceholder(VIEWID_PROPERTYVIEW, IPageLayout.BOTTOM, 0.7f, IPageLayout.ID_EDITOR_AREA);
+        // leftTopLayout.addView(ServerView.VIEW_ID);
+
+        // layout.addPlaceholder(VIEWID_PROPERTYVIEW, IPageLayout.BOTTOM, 0.7f, IPageLayout.ID_EDITOR_AREA);
 
         layout.addPlaceholder(VIEWID_OUTLINE, IPageLayout.RIGHT, 0.7f, IPageLayout.ID_EDITOR_AREA);
 
