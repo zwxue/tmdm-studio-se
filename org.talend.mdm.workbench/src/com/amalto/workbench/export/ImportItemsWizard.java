dd48741a3a408d6912f623d3fbdd725f967ba93c
jsxie <jsxie@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-09-21 05:45:22 +0000

    add //$NON-NLS-3$ for string for talend code standard
    git-svn-id: http://talendforge.org/svn/tom/trunk@68396 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.workbench/src/com/amalto/workbench/export/ImportItemsWizard.java
+++ org.talend.mdm.workbench/src/com/amalto/workbench/export/ImportItemsWizard.java
@@ -1252,7 +1252,7 @@ public class ImportItemsWizard extends Wizard {
             zipBtn = new Button(composite, SWT.RADIO);
             zipBtn.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false, 1, 1));
             zipBtn.setText("Select archive file:");
-            zip = new FileSelectWidget(composite, "", new String[] { "*.zip" }, "", true);//$NON-NLS-1$//$NON-NLS-2$
+            zip = new FileSelectWidget(composite, "", new String[] { "*.zip" }, "", true);//$NON-NLS-1$//$NON-NLS-2$ //$NON-NLS-3$
             // zip.getCmp().setLayoutData(new GridData(SWT.FILL, SWT.FILL, true,
             // false, 1, 1));
             ((GridData) zip.getText().getLayoutData()).widthHint = 200;
