b84374b62fadf790e78ca0289bf30068e9f5d73c
pbabin <pbabin@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-09-26 02:04:17 +0000

    Delete unneeded project
    git-svn-id: http://talendforge.org/svn/tom/trunk@68764 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.webapp.gwt.message/src/main/java/org/talend/mdm/webapp/gwt/message/client/MessageCallback.java
+++
@@ -1,9 +0,0 @@
-package org.talend.mdm.webapp.gwt.message.client;
-
-import com.google.gwt.core.client.JavaScriptObject;
-
-
-public interface MessageCallback {
-
-    public void handler(String msgId, JavaScriptObject data);
-}
