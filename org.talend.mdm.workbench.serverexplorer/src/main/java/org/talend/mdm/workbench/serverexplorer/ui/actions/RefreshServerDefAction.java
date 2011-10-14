1736733553d86ef7fcba8281a6e07ee5822dbb15
pbabin <pbabin@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-10-14 13:24:35 +0000

    TMDM-2481 Move back
    git-svn-id: http://talendforge.org/svn/tom/trunk@70184 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.mdm.workbench.serverexplorer/src/main/java/org/talend/mdm/workbench/serverexplorer/ui/actions/RefreshServerDefAction.java
@@ -0,0 +1,41 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2011 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.mdm.workbench.serverexplorer.ui.actions;
+
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.ui.IViewActionDelegate;
+import org.eclipse.ui.IViewPart;
+import org.talend.mdm.workbench.serverexplorer.ui.views.ServerExplorer;
+
+public class RefreshServerDefAction implements IViewActionDelegate {
+
+    private ServerExplorer serverExplorer;
+
+    public void run(IAction action) {
+        if (serverExplorer != null) {
+            serverExplorer.refreshServerDefs();
+        }
+    }
+
+    public void selectionChanged(IAction action, ISelection selection) {
+    }
+
+    public void init(IViewPart view) {
+
+        if (view instanceof ServerExplorer) {
+            serverExplorer = (ServerExplorer) view;
+        }
+    }
+
+}
