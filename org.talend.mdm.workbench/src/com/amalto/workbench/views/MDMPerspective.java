936a83dcbd4e37cd45ecff760611c36d00207320
pbabin <pbabin@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-10-20 05:57:40 +0000

    TMDM-2687: Temporary back to MDMServer View
    git-svn-id: http://talendforge.org/svn/tom/trunk@70530 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.workbench/src/com/amalto/workbench/views/MDMPerspective.java
+++ org.talend.mdm.workbench/src/com/amalto/workbench/views/MDMPerspective.java
@@ -12,6 +12,7 @@
 // ============================================================================
 package com.amalto.workbench.views;
 
+import org.eclipse.ui.IFolderLayout;
 import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory;
 
@@ -33,12 +34,12 @@ public class MDMPerspective implements IPerspectiveFactory {
     public void createInitialLayout(IPageLayout layout) {
         // from 5.0 m4 it will disappear from perspective
         // leftTopLayout
-        //        IFolderLayout leftTopLayout = layout.createFolder("navigatorLayout", IPageLayout.LEFT, new Float(0.32),//$NON-NLS-1$
-        // IPageLayout.ID_EDITOR_AREA);
+        IFolderLayout leftTopLayout = layout.createFolder("navigatorLayout", IPageLayout.LEFT, new Float(0.32),//$NON-NLS-1$
+                IPageLayout.ID_EDITOR_AREA);
 
-        // leftTopLayout.addView(ServerView.VIEW_ID);
+        leftTopLayout.addView(ServerView.VIEW_ID);
 
-        // layout.addPlaceholder(VIEWID_PROPERTYVIEW, IPageLayout.BOTTOM, 0.7f, IPageLayout.ID_EDITOR_AREA);
+        layout.addPlaceholder(VIEWID_PROPERTYVIEW, IPageLayout.BOTTOM, 0.7f, IPageLayout.ID_EDITOR_AREA);
 
         layout.addPlaceholder(VIEWID_OUTLINE, IPageLayout.RIGHT, 0.7f, IPageLayout.ID_EDITOR_AREA);
 
