434a022541a157bdf58f81a696b82a1c48bb731b
achen <achen@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-12-06 06:34:32 +0000

    TMDM-2502: when importing from an archive zip file, the created object flagged as 'new'
    git-svn-id: http://talendforge.org/svn/tom/trunk@73836 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.repository/src/main/java/org/talend/mdm/repository/ui/wizards/imports/MDMImportRepositoryItemsWizard.java
+++ org.talend.mdm.repository/src/main/java/org/talend/mdm/repository/ui/wizards/imports/MDMImportRepositoryItemsWizard.java
@@ -43,6 +43,8 @@ import org.eclipse.ui.internal.wizards.datatransfer.TarLeveledStructureProvider;
 import org.eclipse.ui.internal.wizards.datatransfer.ZipLeveledStructureProvider;
 import org.eclipse.ui.progress.UIJob;
 import org.talend.core.model.properties.Item;
+import org.talend.mdm.repository.core.command.CommandManager;
+import org.talend.mdm.repository.core.command.ICommand;
 import org.talend.mdm.repository.i18n.Messages;
 import org.talend.mdm.repository.model.mdmproperties.MDMServerObjectItem;
 import org.talend.mdm.repository.model.mdmserverobject.MDMServerObject;
@@ -82,8 +84,10 @@ public class MDMImportRepositoryItemsWizard extends ImportItemsWizard {
                 itemRecords.add((ItemRecord) obj);
             }
         }
+        List<Item> items = new LinkedList<Item>();
         for (ItemRecord itemRec : itemRecords) {
             Item item = itemRec.getProperty().getItem();
+            items.add(item);
             MDMServerObject serverObj = null;
             if (item instanceof MDMServerObjectItem) {
                 serverObj = ((MDMServerObjectItem) item).getMDMServerObject();
@@ -93,7 +97,16 @@ public class MDMImportRepositoryItemsWizard extends ImportItemsWizard {
             }
         }
         repositoryUtil.importItemRecords(manager, itemRecords, monitor, isOverride, null, ""); //$NON-NLS-1$
-
+        // flagged as new
+        for (Item item : items) {
+            MDMServerObject serverObj = null;
+            if (item instanceof MDMServerObjectItem) {
+                serverObj = ((MDMServerObjectItem) item).getMDMServerObject();
+                String name = serverObj.getName() == null ? item.getProperty().getLabel() : serverObj.getName();
+                // flagged as new
+                CommandManager.getInstance().pushCommand(ICommand.CMD_ADD, item.getProperty().getId(), name);
+            }
+        }
 
     }
 
