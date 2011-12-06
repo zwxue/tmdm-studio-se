c5d9fbc72cb68c0a81f0d40121f6d0c86d44943f
jsxie <jsxie@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-12-06 07:59:52 +0000

    fix the bug TMDM-2760 can't set access rights and roles in Property sheet
    git-svn-id: http://talendforge.org/svn/tom/trunk@73846 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.mdm.repository/src/main/java/org/talend/mdm/repository/core/service/MDMRepositoryViewServiceExt.java
@@ -0,0 +1,28 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2011 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.mdm.repository.core.service;
+
+import java.util.List;
+
+import com.amalto.workbench.detailtabs.sections.IMDMRepositoryViewServiceExt;
+
+
+ 
+public class MDMRepositoryViewServiceExt implements IMDMRepositoryViewServiceExt {
+
+    public List<String> findAllRoleNames() {
+        return RepositoryQueryService.findAllRoleNames();
+    }
+
+
+}
