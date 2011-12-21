2645a9d64b7bf3e3285a45b6f54f4b6e6b829e29
jsxie <jsxie@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-12-21 01:50:56 +0000

    optimize the code for bug TMDM-3157 error shows up while renaming the default items view
    git-svn-id: http://talendforge.org/svn/tom/trunk@74962 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.workbench/src/com/amalto/workbench/dialogs/AddBrowseItemsWizard.java
+++ org.talend.mdm.workbench/src/com/amalto/workbench/dialogs/AddBrowseItemsWizard.java
@@ -350,8 +350,10 @@ public class AddBrowseItemsWizard extends Wizard {
                         List<Line> lines = browseItemToRoles.get(BROWSE_ITEMS + elem.getName());
                         browseItemToRoles.remove(BROWSE_ITEMS + elem.getName());
                         int prex = value.toString().trim().indexOf(BROWSE_ITEMS);
-                        if ((prex + BROWSE_ITEMS.length()) <= value.toString().trim().length())
-                        elem.setName(value.toString().trim().substring(prex + BROWSE_ITEMS.length()));
+
+                        if ((prex + BROWSE_ITEMS.length()) <= value.toString().trim().length()) {
+                            elem.setName(value.toString().trim().substring(prex + BROWSE_ITEMS.length()));
+                        }
                         browseItemToRoles.put(value.toString().trim(), lines);
                         refreshRoleView(BROWSE_ITEMS + elem.getName());
                         browseViewer.update(elem, null);
