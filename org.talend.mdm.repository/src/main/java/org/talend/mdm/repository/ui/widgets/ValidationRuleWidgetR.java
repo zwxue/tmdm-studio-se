5b0d26027bdaba4f6318b5cbceb8f72255cb7ee5
jsxie <jsxie@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-02-02 09:06:55 +0000

    fix the bug TMDM-3370
    Xpath in validation Rule Expression does not work
    
    git-svn-id: http://talendforge.org/svn/tom/trunk@77632 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.mdm.repository/src/main/java/org/talend/mdm/repository/ui/widgets/ValidationRuleWidgetR.java
@@ -0,0 +1,34 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2012 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.mdm.repository.ui.widgets;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Shell;
+
+import com.amalto.workbench.models.TreeParent;
+import com.amalto.workbench.widgets.ValidationRuleWidget;
+
+
+
+public class ValidationRuleWidgetR extends ValidationRuleWidget {
+
+    public ValidationRuleWidgetR(Composite parent, TreeParent treeParent, String conceptName) {
+        super(parent, treeParent, conceptName);
+    }
+
+    protected void createValidationRuleExcpressDialog(Shell shell) {
+        dlg = new ValidationRuleExcpressDialogR(shell, treeParent, "Build Validation Rule Expression ", text.getText(),
+        conceptName, false, true);
+    }
+
+}
