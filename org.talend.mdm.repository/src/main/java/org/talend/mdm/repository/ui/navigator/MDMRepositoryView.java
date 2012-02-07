27d2020931f73990906c111d979cdc2fdf4a2048
xdzhang <xdzhang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-02-07 06:53:44 +0000

    TMDM-3357:lock flag is show when editor is opened.
    git-svn-id: http://talendforge.org/svn/tom/trunk@77947 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.repository/src/main/java/org/talend/mdm/repository/ui/navigator/MDMRepositoryView.java
+++ org.talend.mdm.repository/src/main/java/org/talend/mdm/repository/ui/navigator/MDMRepositoryView.java
@@ -245,6 +245,37 @@ public class MDMRepositoryView extends CommonNavigator {
     IPartListener editorListener = new IPartListener() {
 
         public void partActivated(IWorkbenchPart part) {
+            if (part instanceof IEditorPart) {
+                IEditorInput input = ((IEditorPart) part).getEditorInput();
+                if (input != null && input instanceof IRepositoryViewEditorInput) {
+                    Item item = ((IRepositoryViewEditorInput) input).getInputItem();
+                    if (item != null) {
+                        try {
+                            if (factory.isEditableAndLockIfPossible(item)) {
+                                factory.lock(item);
+                            }
+                            Property property = item.getProperty();
+                            final IRepositoryViewObject viewObject = ContainerCacheService.get(property);
+                            if (viewObject != null) {
+                                Display.getDefault().asyncExec(new Runnable() {
+
+                                    public void run() {
+                                        if (!getCommonViewer().getTree().isDisposed()) {
+                                            getCommonViewer().refresh(viewObject);
+                                        }
+                                    }
+                                });
+
+                            }
+                        } 
+                        catch (PersistenceException e) {
+                            log.error(e.getMessage(), e);
+                        } catch (LoginException e) {
+                            log.error(e.getMessage(), e);
+                        }
+                    }
+                }
+            }
         }
 
         public void partBroughtToTop(IWorkbenchPart part) {
