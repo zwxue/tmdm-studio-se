1cd835e7c6f239dbb17fceaff14c1e534266374c
hbhong <hbhong@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-03-28 02:09:06 +0000

    merge r80450 from trunk to branch 5.0 TMDM-3650:fix can't extract proc file when importing workflow
    git-svn-id: http://talendforge.org/svn/tom/branches/branch-5_0@80453 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.repository/src/main/java/org/talend/mdm/repository/ui/wizards/imports/ImportServerObjectWizard.java
+++ org.talend.mdm.repository/src/main/java/org/talend/mdm/repository/ui/wizards/imports/ImportServerObjectWizard.java
@@ -125,6 +125,7 @@ public class ImportServerObjectWizard extends Wizard {
 
     MDMServerDef serverDef;
 
+
     private LabelCombo comboVersion;
 
     private Text txtServer;
@@ -256,61 +257,57 @@ public class ImportServerObjectWizard extends Wizard {
         HttpGet httpget = new HttpGet(workflowURL);
         // System.out.println("executing request" + httpget.getRequestLine());
         HttpResponse response = httpclient.execute(httpget);
-        HttpEntity entity = response.getEntity();
+        if (response.getStatusLine().getStatusCode() == 200) {
+            HttpEntity entity = response.getEntity();
 
-        String encodedID = URLEncoder.encode(treeObj.getDisplayName(), "UTF-8");//$NON-NLS-1$
-        File tempFolder = IOUtil.getTempFolder();
-        String filename = tempFolder.getAbsolutePath() + File.separator + encodedID + ".bar";//$NON-NLS-1$
-        InputStream is = entity.getContent();
-        OutputStream os = null;
-        try {
+            String encodedID = URLEncoder.encode(treeObj.getDisplayName(), "UTF-8");//$NON-NLS-1$
+            File tempFolder = IOUtil.getTempFolder();
+            String filename = tempFolder.getAbsolutePath() + File.separator + encodedID + ".bar";//$NON-NLS-1$
+            InputStream is = entity.getContent();
+            OutputStream os = null;
             File barFile = new File(filename);
-            os = new FileOutputStream(barFile);
-            IOUtils.copy(is, os);
-            byte[] procBytes = extractBar(barFile);
-            if (procBytes != null) {
-                WSWorkflowE workflow = MdmserverobjectFactory.eINSTANCE.createWSWorkflowE();
-                workflow.setName(wsKey.getProcessName());
-                workflow.setFileContent(procBytes);
-                return workflow;
+            try {
+
+                os = new FileOutputStream(barFile);
+                int copy = IOUtils.copy(is, os);
+
+            } catch (Exception e) {
+                log.error(e.getMessage(), e);
+                return null;
+            } finally {
+                if (is != null) {
+                    try {
+                        is.close();
+                    } catch (Exception e) {
+                    }
+                }
+                if (os != null) {
+                    try {
+                        os.close();
+                    } catch (Exception e) {
+                    }
+                }
+
             }
-        } catch (Exception e) {
-            log.error(e.getMessage(), e);
-        } finally {
-            if (is != null) {
+            if (barFile.exists()) {
                 try {
-                    is.close();
-                } catch (Exception e) {
+                    byte[] procBytes = extractBar(barFile);
+                    if (procBytes != null) {
+                        WSWorkflowE workflow = MdmserverobjectFactory.eINSTANCE.createWSWorkflowE();
+                        workflow.setName(wsKey.getProcessName());
+                        workflow.setFileContent(procBytes);
+                        return workflow;
+                    }
+                } finally {
+                    IOUtil.cleanFolder(tempFolder);
                 }
             }
-            if (os != null) {
-                try {
-                    os.close();
-                } catch (Exception e) {
-                }
-            }
-            IOUtil.cleanFolder(tempFolder);
+
         }
         return null;
     }
 
     private byte[] extractBar(File barFile) {
-        // BusinessArchive archive = BusinessArchiveFactory.getBusinessArchive(barFile);
-        //
-        // ProcessDefinitionUUID uuid = archive.getProcessUUID();
-        //
-        // String procFileName = RepositoryWorkflowUtil.getProcFileName(uuid.getProcessName(),
-        // uuid.getProcessVersion());
-        // IFolder folder = RepositoryResourceUtil.getFolder(IServerObjectRepositoryType.TYPE_WORKFLOW);
-        // IFile procFile = folder.getFile(procFileName);
-        // Set<String> barResources = archive.getResources().keySet();
-        // byte[] bytes = new byte[0];
-        // for (String barResource : barResources) {
-        //            if (barResource.endsWith(".proc")) { //$NON-NLS-1$
-        // bytes = archive.getResources().get(barResource);
-        // break;
-        // }
-        // }
         ZipFile zipFile = null;
         InputStream entryInputStream = null;
         byte[] processBytes = null;
@@ -390,7 +387,7 @@ public class ImportServerObjectWizard extends Wizard {
                 String uniqueName = getUniqueName(treeObj, treeObjName);
                 IRepositoryViewObject viewObject = RepositoryResourceUtil.findViewObjectByName(type, uniqueName);
                 if (viewObject != null)
-                viewObjs.add(viewObject);
+                    viewObjs.add(viewObject);
             }
         }
         LockedObjectDialog lockDialog = new LockedObjectDialog(getShell(), Messages.ImportServerObjectWizard_lockedObjectMessage,
