aff9155f0c84ab86021b310d54efdd770ce825c9
hbhong <hbhong@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-05-07 10:49:02 +0000

    TMDM-3908:fix the following: 1.when import demo project, not remove the last server info for job object 2.not mark job object as new object
    git-svn-id: http://talendforge.org/svn/tom/trunk@82980 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.repository/src/main/java/org/talend/mdm/repository/ui/actions/importproject/MarkNodeAsNewAction.java
+++ org.talend.mdm.repository/src/main/java/org/talend/mdm/repository/ui/actions/importproject/MarkNodeAsNewAction.java
@@ -19,6 +19,7 @@ import org.apache.log4j.Logger;
 import org.talend.commons.exception.PersistenceException;
 import org.talend.core.model.general.Project;
 import org.talend.core.model.properties.Item;
+import org.talend.core.model.properties.Property;
 import org.talend.core.model.repository.ERepositoryObjectType;
 import org.talend.core.model.repository.IRepositoryViewObject;
 import org.talend.core.runtime.CoreRuntimePlugin;
@@ -27,6 +28,7 @@ import org.talend.mdm.repository.core.command.CommandManager;
 import org.talend.mdm.repository.core.command.ICommand;
 import org.talend.mdm.repository.model.mdmproperties.MDMServerObjectItem;
 import org.talend.mdm.repository.model.mdmserverobject.MDMServerObject;
+import org.talend.mdm.repository.utils.RepositoryResourceUtil;
 import org.talend.repository.model.IProxyRepositoryFactory;
 import org.talend.repository.ui.utils.IAfterImportProjectAction;
 
@@ -34,14 +36,14 @@ import org.talend.repository.ui.utils.IAfterImportProjectAction;
  * 
  * Mark the imported project nodes as 'new' status
  */
-public class MarkNodeAsNewAction implements IAfterImportProjectAction{
+public class MarkNodeAsNewAction implements IAfterImportProjectAction {
 
-    private static Logger log=Logger.getLogger(MarkNodeAsNewAction.class);
-    
+    private static Logger log = Logger.getLogger(MarkNodeAsNewAction.class);
+
     public void run(Project project) {
-        if(project==null) 
+        if (project == null)
             return;
-        List<ERepositoryObjectType> types=new ArrayList<ERepositoryObjectType>();
+        List<ERepositoryObjectType> types = new ArrayList<ERepositoryObjectType>();
         types.add(IServerObjectRepositoryType.TYPE_DATAMODEL);
         types.add(IServerObjectRepositoryType.TYPE_CUSTOM_FORM);
         types.add(IServerObjectRepositoryType.TYPE_DATACLUSTER);
@@ -55,35 +57,51 @@ public class MarkNodeAsNewAction implements IAfterImportProjectAction{
         types.add(IServerObjectRepositoryType.TYPE_VIEW);
         types.add(IServerObjectRepositoryType.TYPE_UNIVERSE);
         types.add(IServerObjectRepositoryType.TYPE_WORKFLOW);
+        types.add(ERepositoryObjectType.PROCESS);
         IProxyRepositoryFactory factory = CoreRuntimePlugin.getInstance().getProxyRepositoryFactory();
-        for(ERepositoryObjectType type: types){
+        for (ERepositoryObjectType type : types) {
             List<IRepositoryViewObject> objects;
             try {
-                objects = factory.getAll(project,type);
+                objects = factory.getAll(project, type);
             } catch (PersistenceException e1) {
-               log.error(e1.getLocalizedMessage(),e1);
-               return ;
+                log.error(e1.getLocalizedMessage(), e1);
+                return;
             }
-            MDMServerObject serverObj = null;
-            if(objects!=null && objects.size()>0){
-                for(IRepositoryViewObject object: objects){
-                    if (object.getProperty().getItem() instanceof MDMServerObjectItem) {
-                        Item item = object.getProperty().getItem();
+
+            if (objects != null && objects.size() > 0) {
+                for (IRepositoryViewObject object : objects) {
+
+                    Property property = object.getProperty();
+                    Item item = property.getItem();
+                    boolean needRemoved = false;
+                    MDMServerObject serverObj = null;
+                    if (item instanceof MDMServerObjectItem) {
+
                         serverObj = ((MDMServerObjectItem) item).getMDMServerObject();
                         if (serverObj.getLastServerDef() != null) {
-                            serverObj.setLastServerDef(null);
-                            try {
-                                factory.save(project,item);
-                            } catch (PersistenceException e) {
-                                log.error(e.getMessage(), e);
-                            }
+                            needRemoved = true;
                         }
-                        String name = serverObj.getName() == null ? item.getProperty().getLabel() : serverObj.getName();
-                        // flagged as new
-                        CommandManager.getInstance().pushCommand(ICommand.CMD_ADD, item.getProperty().getId(), name);
+                    } else {
+                        Object value = property.getAdditionalProperties().get(RepositoryResourceUtil.PROP_LAST_SERVER_DEF);
+                        if (value != null) {
+                            needRemoved = true;
+                        }
                     }
+                    if (needRemoved) {
+                        RepositoryResourceUtil.setLastServerDef(item, null);
+                        try {
+                            factory.save(project, item);
+                        } catch (PersistenceException e) {
+                            log.error(e.getMessage(), e);
+                        }
+                    }
+                    String name = (serverObj == null || serverObj.getName() == null) ? item.getProperty().getLabel() : serverObj
+                            .getName();
+                    // flagged as new
+                    CommandManager.getInstance().pushCommand(ICommand.CMD_ADD, item.getProperty().getId(), name);
+
                 }
             }
-        }                
+        }
     }
 }
