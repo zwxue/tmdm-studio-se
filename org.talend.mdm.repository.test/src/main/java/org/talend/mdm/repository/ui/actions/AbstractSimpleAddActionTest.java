93b3a25ff2531c9aaedcd2ee461ccae3077d42c1
hbhong <hbhong@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-05-23 07:30:42 +0000

    TMDM-4022:Share project "org.talend.mdm.repository.test" to branch
    git-svn-id: http://talendforge.org/svn/tom/branches/branch-5_0@84035 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.mdm.repository.test/src/main/java/org/talend/mdm/repository/ui/actions/AbstractSimpleAddActionTest.java
@@ -0,0 +1,96 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2012 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.mdm.repository.ui.actions;
+
+import static org.mockito.Matchers.anyObject;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.jface.resource.ImageRegistry;
+import org.eclipse.jface.resource.JFaceResources;
+import org.powermock.api.mockito.PowerMockito;
+import org.talend.core.context.RepositoryContext;
+import org.talend.core.model.general.Project;
+import org.talend.core.model.properties.Item;
+import org.talend.core.model.properties.User;
+import org.talend.core.repository.model.IRepositoryFactory;
+import org.talend.core.repository.model.ProxyRepositoryFactory;
+import org.talend.core.repository.utils.XmiResourceManager;
+import org.talend.core.runtime.CoreRuntimePlugin;
+import org.talend.mdm.repository.core.IRepositoryNodeConfiguration;
+import org.talend.mdm.repository.core.IRepositoryNodeResourceProvider;
+import org.talend.mdm.repository.core.impl.recyclebin.RecycleBinNodeConfiguration;
+import org.talend.mdm.repository.extension.RepositoryNodeConfigurationManager;
+import org.talend.repository.ProjectManager;
+import org.talend.repository.model.IProxyRepositoryFactory;
+
+import com.amalto.workbench.image.ImageCache;
+
+/**
+ * DOC hbhong  class global comment. Detailled comment
+ */
+public class AbstractSimpleAddActionTest {
+
+    protected void mockSimpleAddActionClass() throws Exception {
+        PowerMockito.mockStatic(JFaceResources.class);
+        ImageRegistry registry = mock(ImageRegistry.class);
+        when(JFaceResources.getImageRegistry()).thenReturn(registry);
+
+        PowerMockito.mockStatic(ImageCache.class);
+        ImageDescriptor imgDesc = mock(ImageDescriptor.class);
+        when(ImageCache.getImage(anyString())).thenReturn(imgDesc);
+        //
+
+        PowerMockito.mockStatic(CoreRuntimePlugin.class);
+        CoreRuntimePlugin coreRuntimePlugin = mock(CoreRuntimePlugin.class);
+        when(CoreRuntimePlugin.getInstance()).thenReturn(coreRuntimePlugin);
+        IProxyRepositoryFactory repositoryFactory = mock(IProxyRepositoryFactory.class);
+        when(CoreRuntimePlugin.getInstance().getProxyRepositoryFactory()).thenReturn(repositoryFactory);
+
+        RepositoryContext ontextMock = mock(RepositoryContext.class);
+        when(repositoryFactory.getRepositoryContext()).thenReturn(ontextMock);
+
+        PowerMockito.mockStatic(ProjectManager.class);
+        ProjectManager pmMock = mock(ProjectManager.class);
+        Project pMock = mock(Project.class);
+        User userMock = mock(User.class);
+        when(userMock.getLogin()).thenReturn("a@b.cn"); //$NON-NLS-1$
+        when(pMock.getAuthor()).thenReturn(userMock);
+        when(pmMock.getCurrentProject()).thenReturn(pMock);
+        when(ProjectManager.getInstance()).thenReturn(pmMock);
+        when(ontextMock.getUser()).thenReturn(userMock);
+
+        RecycleBinNodeConfiguration recycleBinNodeConfiguration = mock(RecycleBinNodeConfiguration.class);
+        PowerMockito.whenNew(RecycleBinNodeConfiguration.class).withNoArguments().thenReturn(recycleBinNodeConfiguration);
+
+        PowerMockito.mockStatic(ProxyRepositoryFactory.class);
+        ProxyRepositoryFactory proxyRepositoryFactory = mock(ProxyRepositoryFactory.class);
+        when(ProxyRepositoryFactory.getInstance()).thenReturn(proxyRepositoryFactory);
+        IRepositoryFactory repositoryFactoryMock = mock(IRepositoryFactory.class);
+        when(proxyRepositoryFactory.getRepositoryFactoryFromProvider()).thenReturn(repositoryFactoryMock);
+        XmiResourceManager xmiResourceManager = mock(XmiResourceManager.class);
+        when(repositoryFactoryMock.getResourceManager()).thenReturn(xmiResourceManager);
+
+        PowerMockito.mockStatic(RepositoryNodeConfigurationManager.class);
+        IRepositoryNodeConfiguration rncMock = mock(IRepositoryNodeConfiguration.class);
+        when(RepositoryNodeConfigurationManager.getConfiguration((Item) anyObject())).thenReturn(rncMock);
+
+        IRepositoryNodeResourceProvider resourceProviderM = mock(IRepositoryNodeResourceProvider.class);
+        when(rncMock.getResourceProvider()).thenReturn(resourceProviderM);
+
+        when(resourceProviderM.needSaveReferenceFile()).thenReturn(true);
+        when(repositoryFactory.isEditableAndLockIfPossible((Item) anyObject())).thenReturn(true);
+    }
+}
