d2e647f9fc1af811a3e8973791cbf41c08580505
sbliu <sbliu@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-07-05 06:08:30 +0000

    TMDM-4198:remove the auto commit function and transform the filter string.
    git-svn-id: http://talendforge.org/svn/tom/trunk@86798 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.workbench/src/com/amalto/workbench/providers/XSDTreeLabelProvider.java
+++ org.talend.mdm.workbench/src/com/amalto/workbench/providers/XSDTreeLabelProvider.java
@@ -14,6 +14,7 @@ package com.amalto.workbench.providers;
 
 import java.util.Iterator;
 
+import org.apache.commons.lang.StringEscapeUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.eclipse.emf.common.util.EList;
@@ -239,7 +240,11 @@ public class XSDTreeLabelProvider extends LabelProvider {
                         }
 
                         if (source.equals("X_ForeignKey_Filter")) {//$NON-NLS-1$
-                            return "Foreign Key Filter:  " + e.getChildNodes().item(0).getNodeValue();
+                            String nodeValue = e.getChildNodes().item(0).getNodeValue();
+                            if(nodeValue.startsWith("$CFFP:")) {//$NON-NLS-1$
+                                nodeValue = StringEscapeUtils.unescapeXml(nodeValue).substring(6);
+                            }
+                            return "Foreign Key Filter:  " + nodeValue;
                         } else {
                             return source + ": " + Util.nodeToString((Element) obj);//$NON-NLS-1$
                         }
