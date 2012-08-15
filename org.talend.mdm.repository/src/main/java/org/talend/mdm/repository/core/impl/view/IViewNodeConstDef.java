fa0b0555edc2f86cac337779db271af6b2fbcdaf
sbliu <sbliu@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-08-15 07:07:15 +0000

    TMDM-4390:Add IViewNodeConstDef class for some constants references.
    git-svn-id: http://talendforge.org/svn/tom/trunk@89165 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.mdm.repository/src/main/java/org/talend/mdm/repository/core/impl/view/IViewNodeConstDef.java
@@ -0,0 +1,34 @@
+// ============================================================================
+//
+// Talend Community Edition
+//
+// Copyright (C) 2006-2012 Talend ¨C www.talend.com
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+// ============================================================================
+package org.talend.mdm.repository.core.impl.view;
+
+
+public interface IViewNodeConstDef {
+    public static final String TYPE_WEBFILTER = "webfilter";//$NON-NLS-1$
+    public static final String TYPE_SEARCHFILTER = "searchfilter";//$NON-NLS-1$
+    public static final String TYPE_VIEW = "view";//$NON-NLS-1$
+    
+    public static final String ViewPrefix = "Browse_items_";//$NON-NLS-1$
+    
+    public static final String PATH_WEBFILTER = "web";
+    public static final String PATH_SEARCHFILTER = "search";
+}
