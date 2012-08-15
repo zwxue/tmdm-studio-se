45c88324aae1a78084fb4b6e9fd4521da4ceb9e0
hshu <hshu@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-08-15 08:15:46 +0000

    TMDM-4289: Remove patch tools from CE
    git-svn-id: http://talendforge.org/svn/tom/trunk@89174 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.patchtool/scripts/patchtool.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-rem ###############################################
-rem ## MDM Patch Tool
-rem ## usage: patchtool D:\Patch_TMDM_V5
-rem ## Please make sure all the updated files are under D:\Patch_TMDM_V5\todeploy folder
-rem ###############################################
-java -cp patchtool.jar; org.talend.mdm.patchtool.PatchTool %1
--- org.talend.mdm.patchtool/scripts/patchtool.sh
+++
@@ -1,6 +0,0 @@
-###############################################
-## MDM Patch Tool
-## usage: patchtool \opt\Patch_TMDM_V5
-## Please make sure all the updated files are under \opt\todeploy folder
-###############################################
-java -cp patchtool.jar; org.talend.mdm.patchtool.PatchTool $1
