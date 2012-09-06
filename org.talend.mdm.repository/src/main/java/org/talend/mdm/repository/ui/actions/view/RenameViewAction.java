c90a0a0d5fbb21919704d47c840b8133a96b6fd8
sbliu <sbliu@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-09-06 08:24:34 +0000

    TMDM-4369:put all the name valid judgment into ValidateUtil class and let other class to call it to verify name.
    git-svn-id: http://talendforge.org/svn/tom/trunk@90286 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.repository/src/main/java/org/talend/mdm/repository/ui/actions/view/RenameViewAction.java
+++ org.talend.mdm.repository/src/main/java/org/talend/mdm/repository/ui/actions/view/RenameViewAction.java
@@ -12,8 +12,6 @@
 // ============================================================================
 package org.talend.mdm.repository.ui.actions.view;
 
-import java.util.regex.Pattern;
-
 import org.apache.log4j.Logger;
 import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.jface.dialogs.IInputValidator;
@@ -34,6 +32,7 @@ import org.talend.mdm.repository.ui.actions.RenameObjectAction;
 import org.talend.mdm.repository.ui.dialogs.RenameViewDialog;
 import org.talend.mdm.repository.ui.dialogs.RenameViewDialog2;
 import org.talend.mdm.repository.utils.RepositoryResourceUtil;
+import org.talend.mdm.repository.utils.ValidateUtil;
 import org.talend.repository.model.IProxyRepositoryFactory;
 
 import com.amalto.workbench.image.EImage;
@@ -134,13 +133,7 @@ public class RenameViewAction extends AbstractRepositoryAction {
                     if (newText == null || newText.trim().length() == 0)
                         return Messages.Common_nameCanNotBeEmpty;
                     
-                    String regex = "\\w*(#|-|\\.|\\w*)+\\w+";//$NON-NLS-1$
-                    Pattern pattern = Pattern.compile(regex);
-                    Pattern p2 = Pattern.compile(".*\\w+");//$NON-NLS-1$
-                    if(!p2.matcher(newText).matches())
-                        return Messages.Common_nameInvalid;
-                        
-                    if (!pattern.matcher(newText).matches()) {
+                    if (!ValidateUtil.matchCommonRegex(newText)) {
                         return Messages.Common_nameInvalid;
                     }
                     //
