4755164bf2bbdcd41584ce2acf87befdc9428536
sbliu <sbliu@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-09-06 08:23:38 +0000

    TMDM-4369:put all the name valid judgment into ValidateUtil class and let other class to call it to verify name.
    git-svn-id: http://talendforge.org/svn/tom/trunk@90284 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.repository/src/main/java/org/talend/mdm/repository/ui/actions/view/NewViewAction.java
+++ org.talend.mdm.repository/src/main/java/org/talend/mdm/repository/ui/actions/view/NewViewAction.java
@@ -21,8 +21,6 @@
 // ============================================================================
 package org.talend.mdm.repository.ui.actions.view;
 
-import java.util.regex.Pattern;
-
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.jface.dialogs.IInputValidator;
 import org.eclipse.jface.window.Window;
@@ -45,6 +43,7 @@ import org.talend.mdm.repository.model.mdmserverobject.WSViewE;
 import org.talend.mdm.repository.ui.actions.AbstractSimpleAddAction;
 import org.talend.mdm.repository.ui.dialogs.ViewInputDialog2;
 import org.talend.mdm.repository.utils.RepositoryResourceUtil;
+import org.talend.mdm.repository.utils.ValidateUtil;
 
 /**
  * DOC class global comment. Detailled comment <br/>
@@ -176,7 +175,7 @@ public class NewViewAction extends AbstractSimpleAddAction {
             public String isValid(String newText) {
                 if (newText == null || newText.trim().length() == 0)
                     return Messages.Common_nameCanNotBeEmpty;
-                if (!matchRegex(newText)) {
+                if (!ValidateUtil.matchViewProcessRegex(newText)) {
                     return Messages.Common_nameInvalid;
                 }
                 if (RepositoryResourceUtil.isExistByName(parentItem.getRepObjType(), newText.trim())) {
@@ -186,17 +185,7 @@ public class NewViewAction extends AbstractSimpleAddAction {
             };
         };
     }
-    private boolean matchRegex(String newText) {
-        String regex = "\\w*(#|\\.|\\w*)+\\w+";//$NON-NLS-1$
-        String tailRegex = ".*\\w+";//$NON-NLS-1$
-        
-        Pattern p1 = Pattern.compile(regex);
-        Pattern p2 =Pattern.compile(tailRegex);
-        if(!p2.matcher(newText).matches())
-            return false;
-        
-        return p1.matcher(newText).matches();
-    }
+    
     public void createNewView(String viewName) {
         RepositoryResourceUtil.removeViewObjectPhysically(IServerObjectRepositoryType.TYPE_VIEW, viewName, null, null);
         createServerObject(viewName);
