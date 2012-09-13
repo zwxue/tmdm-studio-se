0d512dd0dbebbea63b6e708505a8f9436bb8d7d1
hbhong <hbhong@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-09-13 08:55:46 +0000

    TMDM-4476:fixed can't execute Job via Right-click job object in MDM repository view
    git-svn-id: http://talendforge.org/svn/tom/trunk@90644 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.repository/src/main/java/org/talend/mdm/repository/ui/actions/job/RunProcessAction.java
+++ org.talend.mdm.repository/src/main/java/org/talend/mdm/repository/ui/actions/job/RunProcessAction.java
@@ -12,23 +12,96 @@
 // ============================================================================
 package org.talend.mdm.repository.ui.actions.job;
 
+import org.apache.log4j.Logger;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
+import org.talend.commons.exception.PersistenceException;
+import org.talend.commons.ui.runtime.exception.ExceptionHandler;
+import org.talend.core.model.general.Project;
+import org.talend.core.model.properties.Property;
+import org.talend.core.runtime.CoreRuntimePlugin;
 import org.talend.designer.core.ui.action.RunProcess;
 import org.talend.mdm.repository.core.bridge.AbstractBridgeRepositoryAction;
+import org.talend.repository.ProjectManager;
+import org.talend.repository.model.IProxyRepositoryFactory;
+import org.talend.repository.model.IRepositoryNode;
+import org.talend.repository.model.RepositoryNode;
 
 /**
  * DOC hbhong class global comment. Detailled comment
  */
 public class RunProcessAction extends AbstractBridgeRepositoryAction {
 
+    static Logger log = Logger.getLogger(RunProcessAction.class);
+    private static final String PROCESS_VIEW_ID = "org.talend.designer.runprocess.ui.views.processview"; //$NON-NLS-1$
+    static class MDMRunProcess extends RunProcess {
 
+        @Override
+        protected RepositoryNode getCurrentRepositoryNode() {
+            ISelection selection = getSelection();
+            if (selection == null || selection.isEmpty() || !(selection instanceof IStructuredSelection))
+                return null;
+            Object object = ((IStructuredSelection) selection).getFirstElement();
+            if (object instanceof IRepositoryNode) {
+                RepositoryNode node = (RepositoryNode) object;
+                if (node == null || node.getObject() == null || !(node.getObject() instanceof RepositoryNode)) {
+                    return node;
+                }
+                Property property = node.getObject().getProperty();
+                Property updatedProperty = null;
 
+                try {
+                    IProxyRepositoryFactory proxyRepositoryFactory = CoreRuntimePlugin.getInstance().getProxyRepositoryFactory();
+                    proxyRepositoryFactory.initialize();
+
+                    updatedProperty = proxyRepositoryFactory.getLastVersion(
+                            new Project(ProjectManager.getInstance().getProject(property.getItem())), property.getId())
+                            .getProperty();
+                } catch (PersistenceException e) {
+                    ExceptionHandler.process(e);
+                }
+
+                return node;
+
+            }
+            return null;
+        }
+
+    }
+
+    @Override
+    protected void doRun() {
+        activeProcessView();
+        super.doRun();
+    }
+    private void activeProcessView() {
+        IWorkbench workbench = PlatformUI.getWorkbench();
+        if (workbench != null) {
+            IWorkbenchWindow window = workbench.getActiveWorkbenchWindow();
+            if (window != null) {
+                IWorkbenchPage page = window.getActivePage();
+                if (page != null) {
+                    try {
+                        page.showView(PROCESS_VIEW_ID);
+                    } catch (PartInitException e) {
+                        log.error(e.getMessage(), e);
+                    }
+                }
+            }
+        }
+    }
     /**
      * DOC hbhong EditProcessAction constructor comment.
      * 
      * @param cAction
      */
     public RunProcessAction() {
-        super(new RunProcess());
+        super(new MDMRunProcess());
     }
 
     @Override
