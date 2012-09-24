7d1f52c4e88a67f35a2d08dec5a01009d242cb89
sbliu <sbliu@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-09-24 10:35:55 +0000

    merge r91225 from trunk to branch 5.1 TMDM-4293:verify if the object is really deleted, if true then allow to show the deleted icon on this node.
    git-svn-id: http://talendforge.org/svn/tom/branches/branch-5_1@91227 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.repository/src/main/java/org/talend/mdm/repository/ui/actions/bridge/DeleteAction.java
+++ org.talend.mdm.repository/src/main/java/org/talend/mdm/repository/ui/actions/bridge/DeleteAction.java
@@ -12,12 +12,15 @@
 // ============================================================================
 package org.talend.mdm.repository.ui.actions.bridge;
 
+import java.util.List;
+
 import org.talend.core.model.repository.IRepositoryViewObject;
 import org.talend.mdm.repository.core.bridge.AbstractBridgeRepositoryAction;
 import org.talend.mdm.repository.core.command.CommandManager;
 import org.talend.mdm.repository.core.command.ICommand;
 import org.talend.mdm.repository.i18n.Messages;
 import org.talend.mdm.repository.models.FolderRepositoryObject;
+import org.talend.repository.model.ERepositoryStatus;
 
 /**
  * DOC hbhong class global comment. Detailled comment
@@ -29,17 +32,25 @@ public class DeleteAction extends AbstractBridgeRepositoryAction {
         setText(Messages.RemoveFromRepositoryAction_removeFromRepository);
     }
 
+    @Override
     public String getGroupName() {
         return GROUP_EDIT;
     }
 
+    @Override
     protected void doRun() {
         super.doRun();
         commonViewer.refresh();
-        for (Object obj : getSelectedObject()) {
+
+        List<Object> selectedObject = getSelectedObject();
+        for (Object obj : selectedObject) {
             if (obj instanceof IRepositoryViewObject && !(obj instanceof FolderRepositoryObject)) {
                 IRepositoryViewObject viewObj = (IRepositoryViewObject) obj;
-                CommandManager.getInstance().pushCommand(ICommand.CMD_DELETE, viewObj.getId(), viewObj.getLabel());
+
+                ERepositoryStatus repositoryStatus = viewObj.getRepositoryStatus();
+                if (repositoryStatus == ERepositoryStatus.DELETED) {
+                    CommandManager.getInstance().pushCommand(ICommand.CMD_DELETE, viewObj.getId(), viewObj.getLabel());
+                }
             }
         }
     }
