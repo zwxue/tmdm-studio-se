01d8614cee41c225a6419464cd12f5e477fbf929
sbliu <sbliu@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-11-21 03:31:46 +0000

    TMDM-4270:refactor the code as commented.
    git-svn-id: http://talendforge.org/svn/tom/trunk@94049 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.mdm.workbench/src/com/amalto/workbench/service/IUpdateLastServerService.java
@@ -0,0 +1,33 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2012 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package com.amalto.workbench.service;
+
+import org.talend.core.IService;
+
+import com.amalto.workbench.utils.MDMServerDef;
+
+
+/**
+ * created by liusongbo on 2012-11-20
+ * 
+ */
+public interface IUpdateLastServerService extends IService {
+
+    /**
+     * @param ser
+     * @param repositoryViewObjName
+     * @param repositoryViewObjType the valid value see constants in class TreeObject,like
+     * TreeObject.WORKFLOW_PROCESS,TreeObject.DATA_MODEL
+     */
+    public void updateLastServerDefInfo(MDMServerDef ser, String repositoryViewObjName, int repositoryViewObjType);
+}
