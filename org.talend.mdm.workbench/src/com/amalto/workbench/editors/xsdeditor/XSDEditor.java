c0d7f69c4a308e71eeadc60077d2ef755a91b9d5
zlhuang <zlhuang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-12-21 07:55:44 +0000

    Merge r95796 from trunk to 5.1 TMDM-5108: Modify the last modification, and make the InputStreamRead closed in the finally.
    git-svn-id: http://talendforge.org/svn/tom/branches/branch-5_1@95799 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.workbench/src/com/amalto/workbench/editors/xsdeditor/XSDEditor.java
+++ org.talend.mdm.workbench/src/com/amalto/workbench/editors/xsdeditor/XSDEditor.java
@@ -95,14 +95,18 @@ public class XSDEditor extends InternalXSDMultiPageEditor implements IServerObje
 
     public void setXObject(TreeObject xobject) {
         this.xobject = xobject;
+        InputStreamReader reader = null;
         try {// temporarily store the file data for restore
             IFile file = getXSDFile(xobject);
-            InputStreamReader reader = new InputStreamReader(file.getContents());
+            reader = new InputStreamReader(file.getContents());
             fileContents = IOUtils.toByteArray(reader, "utf-8"); //$NON-NLS-1$
-            IOUtils.closeQuietly(reader);
 
         } catch (Exception e) {
             log.error(e.getMessage(), e);
+        } finally {
+            if (reader != null) {
+                IOUtils.closeQuietly(reader);
+            }
         }
     }
 
@@ -128,6 +132,7 @@ public class XSDEditor extends InternalXSDMultiPageEditor implements IServerObje
     @Override
     public void doSave(IProgressMonitor monitor) {
     	boolean savedSuccess=true;
+        InputStreamReader reader = null;
         try {
             if (getSelectedPage() instanceof DataModelMainPage) {// save DataModelMainPage's contents to file
                 DataModelMainPage mainPage = (DataModelMainPage) getSelectedPage();
@@ -153,9 +158,8 @@ public class XSDEditor extends InternalXSDMultiPageEditor implements IServerObje
                 fileContents = xsd.getBytes("utf-8"); //$NON-NLS-1$
             } else {
                 IFile file = getXSDFile(xobject);
-                InputStreamReader reader = new InputStreamReader(file.getContents());
+                reader = new InputStreamReader(file.getContents());
                 fileContents = IOUtils.toByteArray(reader, "utf-8"); //$NON-NLS-1$
-                IOUtils.closeQuietly(reader);
             }
         } catch (Exception e) {
             log.error(e.getMessage(), e);
@@ -163,6 +167,9 @@ public class XSDEditor extends InternalXSDMultiPageEditor implements IServerObje
         	if(savedSuccess){
         		super.doSave(monitor);
         	}
+            if (reader != null) {
+                IOUtils.closeQuietly(reader);
+            }
             monitor.done();
         }
     }
