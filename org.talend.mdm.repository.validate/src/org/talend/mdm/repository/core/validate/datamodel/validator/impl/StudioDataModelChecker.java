880fefed3c5c5bb7cacb431684d08b3fba727a5b
fhuaulme <fhuaulme@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-03-13 10:10:43 +0000

    TMDM-5109: Leaves StudioDataModelChecker for compatibility with existing code.
    git-svn-id: http://talendforge.org/svn/tom/trunk@99516 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.mdm.repository.validate/src/org/talend/mdm/repository/core/validate/datamodel/validator/impl/StudioDataModelChecker.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (C) 2006-2013 Talend Inc. - www.talend.com
+ *
+ * This source code is available under agreement available at
+ * %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+ *
+ * You should have received a copy of the agreement
+ * along with this program; if not, write to Talend SA
+ * 9 rue Pages 92150 Suresnes, France
+ */
+package org.talend.mdm.repository.core.validate.datamodel.validator.impl;
+
+import org.talend.mdm.repository.core.validate.datamodel.validator.IModelBuilder;
+
+public class StudioDataModelChecker extends DataModelChecker {
+
+    public StudioDataModelChecker(IModelBuilder builder) {
+    }
+
+}
