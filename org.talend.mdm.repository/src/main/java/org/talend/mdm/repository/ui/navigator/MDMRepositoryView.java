d8be691cf8d4be0f2670762306254f5f08f65e8c
sbliu <sbliu@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-04-09 02:24:52 +0000

    TMDM-5520:refactor the code.
    git-svn-id: http://talendforge.org/svn/tom/trunk@100800 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.repository/src/main/java/org/talend/mdm/repository/ui/navigator/MDMRepositoryView.java
+++ org.talend.mdm.repository/src/main/java/org/talend/mdm/repository/ui/navigator/MDMRepositoryView.java
@@ -25,9 +25,6 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -478,20 +475,13 @@ public class MDMRepositoryView extends CommonNavigator implements ITabbedPropert
         return CONTRIBUTER_ID;
     }
 
-    public void refreshRootNode(ERepositoryObjectType... types) {
-        if (types != null) {
-            List<ERepositoryObjectType> typeList = new ArrayList<ERepositoryObjectType>(types.length);
-            typeList.addAll(Arrays.asList(types));
-
-            IRepositoryViewObject[] categoryViewObjects = RepositoryResourceUtil.getCategoryViewObjects();
-            if (categoryViewObjects != null) {
-                for (IRepositoryViewObject viewObj : categoryViewObjects) {
-                    if (typeList.contains(viewObj.getRepositoryObjectType())) {
-                        getCommonViewer().refresh(viewObj);
-                    }
-                }
+    public void refreshRootNode(ERepositoryObjectType type) {
+        IRepositoryViewObject[] categoryViewObjects = RepositoryResourceUtil.getCategoryViewObjects();
+        for (IRepositoryViewObject viewObj : categoryViewObjects) {
+            if (viewObj.getRepositoryObjectType().equals(type)) {
+                getCommonViewer().refresh(viewObj);
+                break;
             }
         }
-
     }
 }
