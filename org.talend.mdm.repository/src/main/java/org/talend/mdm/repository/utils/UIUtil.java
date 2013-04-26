071425f1289dd0f2ac4162f89bc6c9371003b86e
hbhong <hbhong@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-04-26 07:20:40 +0000

    TMDM-5579: fixed the following: 1.warning user overwriting when generating a process object 2.add select xpath button in process editor
    git-svn-id: http://talendforge.org/svn/tom/trunk@101903 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.repository/src/main/java/org/talend/mdm/repository/utils/UIUtil.java
+++ org.talend.mdm.repository/src/main/java/org/talend/mdm/repository/utils/UIUtil.java
@@ -12,7 +12,16 @@
 // ============================================================================
 package org.talend.mdm.repository.utils;
 
+import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PlatformUI;
+import org.talend.core.model.repository.IRepositoryViewObject;
+import org.talend.mdm.repository.core.IRepositoryNodeActionProvider;
+import org.talend.mdm.repository.core.IRepositoryNodeConfiguration;
+import org.talend.mdm.repository.extension.RepositoryNodeConfigurationManager;
+import org.talend.mdm.repository.ui.editors.IRepositoryViewEditorInput;
 
 /**
  * created by HHB on 2013-3-6 Detailled comment
@@ -31,5 +40,53 @@ public class UIUtil {
             }
         }
         return workInUI;
+    }
+
+    public static IEditorPart findOpenedEditor(IEditorInput input) {
+        if (input == null) {
+            throw new IllegalArgumentException();
+        }
+
+        IWorkbenchPage page = getCurrentPage();
+        if (page != null) {
+            return page.findEditor(input);
+        }
+
+        return null;
+    }
+
+    private static IWorkbenchPage getCurrentPage() {
+        IWorkbenchWindow window = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
+        if (window != null) {
+            IWorkbenchPage page = window.getActivePage();
+            return page;
+        }
+        return null;
+    }
+
+    // Note:only work for MDM objects
+    public static IEditorPart findOpenedEditor(IRepositoryViewObject viewObj) {
+        if (viewObj == null) {
+            throw new IllegalArgumentException();
+        }
+
+        IRepositoryNodeConfiguration configuration = RepositoryNodeConfigurationManager.getConfiguration(viewObj);
+        if (configuration != null) {
+            IRepositoryNodeActionProvider actionProvider = configuration.getActionProvider();
+            if (actionProvider != null) {
+                IRepositoryViewEditorInput editorInput = actionProvider.getOpenEditorInput(viewObj);
+                if (editorInput != null) {
+                    return findOpenedEditor(editorInput);
+                }
+            }
+        }
+        return null;
+    }
+
+    public static void closeEditor(IEditorPart editorPart, boolean save) {
+        IWorkbenchPage page = getCurrentPage();
+        if (page != null) {
+            page.closeEditor(editorPart, save);
+        }
     }
 }
