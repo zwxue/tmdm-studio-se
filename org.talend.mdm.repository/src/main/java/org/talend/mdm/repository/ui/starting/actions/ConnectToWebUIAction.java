f2c2de638329a6706191e9085ecd98b6070dde57
hbhong <hbhong@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-05-24 06:17:45 +0000

    TMDM-5781: fixed missing query server before connecting server
    git-svn-id: http://talendforge.org/svn/tom/trunk@103298 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.repository/src/main/java/org/talend/mdm/repository/ui/starting/actions/ConnectToWebUIAction.java
+++ org.talend.mdm.repository/src/main/java/org/talend/mdm/repository/ui/starting/actions/ConnectToWebUIAction.java
@@ -19,22 +19,30 @@ import java.util.List;
 import java.util.Properties;
 
 import org.apache.log4j.Logger;
+import org.eclipse.jface.dialogs.IDialogConstants;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.intro.IIntroSite;
 import org.eclipse.ui.intro.config.IIntroAction;
 import org.talend.core.model.repository.IRepositoryViewObject;
-import org.talend.mdm.repository.model.mdmproperties.MDMServerDefItem;
+import org.talend.mdm.repository.model.mdmmetadata.MDMServerDef;
 import org.talend.mdm.workbench.serverexplorer.core.ServerDefService;
+import org.talend.mdm.workbench.serverexplorer.ui.dialogs.SelectServerDefDialog;
 
 public class ConnectToWebUIAction implements IIntroAction {
 
     private static Logger log = Logger.getLogger(ConnectToWebUIAction.class);
+
     public void run(IIntroSite iintrosite, Properties properties) {
         String url = getUrl();
         try {
-            open(url);
+            if (url != null) {
+                open(url);
+            }
         } catch (IOException e) {
             log.error(e.getMessage(), e);
         }
+
     }
 
     private void open(String url) throws IOException {
@@ -45,18 +53,24 @@ public class ConnectToWebUIAction implements IIntroAction {
         }
     }
 
+    private SelectServerDefDialog getSelectServerDefDialog() {
+        Shell shell = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell();
+        return new SelectServerDefDialog(shell);
+    }
+
     private String getUrl() {
         String path = "/general/secure/";//$NON-NLS-1$
-        String defaultHostPort = "http://localhost:8180";//$NON-NLS-1$
-
+        String defaultHostPort = "http://localhost:8180"; //$NON-NLS-1$
         List<IRepositoryViewObject> viewObjects = ServerDefService.getAllServerDefViewObjects();
-        if (viewObjects == null || viewObjects.size() == 0)
+        if (viewObjects == null || viewObjects.size() == 0) {
             return defaultHostPort + path;
+        }
 
-        MDMServerDefItem serverDefItem = (MDMServerDefItem) (viewObjects.get(0).getProperty().getItem());
-        String host = serverDefItem.getServerDef().getHost();
-        String port = serverDefItem.getServerDef().getPort();
-
-        return "http://" + host + ":" + port + path;//$NON-NLS-1$//$NON-NLS-2$
+        SelectServerDefDialog selServerDlg = getSelectServerDefDialog();
+        if (selServerDlg.open() == IDialogConstants.OK_ID) {
+            MDMServerDef serverDef = selServerDlg.getSelectedServerDef();
+            return serverDef.getProtocol() + "://" + serverDef.getHost() + ":" + serverDef.getPort() + path; //$NON-NLS-1$ //$NON-NLS-2$
+        }
+        return null;
     }
 }
