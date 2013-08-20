e6a4a2f84030b99416c4cadcfc3a0bf04f65ec16
cgpiao <cgpiao@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-08-20 03:54:34 +0000

    TMDM-6204 : add datamodel name when creating process from workflow
    git-svn-id: http://talendforge.org/svn/tom/trunk@106790 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.repository/src/main/java/org/talend/mdm/repository/ui/widgets/XpathWidgetR.java
+++ org.talend.mdm.repository/src/main/java/org/talend/mdm/repository/ui/widgets/XpathWidgetR.java
@@ -12,6 +12,9 @@
 // ============================================================================
 package org.talend.mdm.repository.ui.widgets;
 
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
 import org.eclipse.jface.window.Window;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.widgets.Composite;
@@ -30,6 +33,8 @@ public class XpathWidgetR extends XpathWidget {
 
     private DataModelMainPage page;
     
+    private PropertyChangeSupport support = new PropertyChangeSupport(this);
+    
     /**
      * DOC hbhong XpathWidgetR constructor comment.
      * 
@@ -47,6 +52,7 @@ public class XpathWidgetR extends XpathWidget {
         super(buttonName, null, toolkit, parent, dialog, isButtonLeft, readOnly, dataModelName);
     }
 
+    
     /**
      * DOC hbhong XpathWidgetR constructor comment.
      * 
@@ -95,11 +101,24 @@ public class XpathWidgetR extends XpathWidget {
         dlg.open();
 
         if (dlg.getReturnCode() == Window.OK) {
+        	String xpath = descriptionText.getText();
+        	String dname = dataModelName;
             descriptionText.setText(dlg.getXpath());
             dataModelName = dlg.getDataModelName();
             dlg.close();
             setOutFocus();
+            support.firePropertyChange("XPath", xpath, descriptionText);
+            support.firePropertyChange("DataModel", dname, dataModelName);
         }
     }
 
+    public void addXpathListener(PropertyChangeListener listener){
+    	support.addPropertyChangeListener(listener);
+    }
+    
+    public void removeXpathListener(PropertyChangeListener listener){
+    	support.removePropertyChangeListener(listener);
+    }
+    
+    
 }
