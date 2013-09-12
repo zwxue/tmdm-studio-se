742094eb7c4c57f29e2e902aa74c205ef2a9c443
cgpiao <cgpiao@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-09-12 07:14:00 +0000

    TUP-978 : change to new common import wizard
    git-svn-id: http://talendforge.org/svn/tom/trunk@107871 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.mdm.repository/src/main/java/org/talend/mdm/repository/handler/MdmImportHandler.java
@@ -0,0 +1,6 @@
+package org.talend.mdm.repository.handler;
+
+import org.talend.repository.items.importexport.handlers.imports.ImportRepTypeHandler;
+
+public class MdmImportHandler extends ImportRepTypeHandler{
+}
--- org.talend.mdm.repository/src/main/java/org/talend/mdm/repository/ui/actions/ImportObjectAction.java
+++ org.talend.mdm.repository/src/main/java/org/talend/mdm/repository/ui/actions/ImportObjectAction.java
@@ -12,58 +12,75 @@
 // ============================================================================
 package org.talend.mdm.repository.ui.actions;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.core.runtime.IConfigurationElement;
+import org.eclipse.core.runtime.IExtension;
+import org.eclipse.core.runtime.IExtensionPoint;
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.jface.viewers.IStructuredSelection;
-import org.eclipse.jface.wizard.WizardDialog;
-import org.talend.mdm.repository.core.AbstractRepositoryAction;
-import org.talend.mdm.repository.i18n.Messages;
-import org.talend.mdm.repository.ui.navigator.MDMRepositoryView;
-import org.talend.mdm.repository.ui.wizards.imports.MDMImportRepositoryItemsWizard;
-
-import com.amalto.workbench.export.ExportItemsAction;
-import com.amalto.workbench.image.EImage;
-import com.amalto.workbench.image.ImageCache;
+import org.eclipse.ui.IWorkbenchWindowActionDelegate;
+import org.talend.mdm.repository.core.bridge.AbstractBridgeRepositoryAction;
+import org.talend.repository.ui.actions.AContextualAction;
 
 /**
- * DOC hywang class global comment. Detailled comment
+ * @author changguopiao
+ * 
  */
-public class ImportObjectAction extends AbstractRepositoryAction {
+public class ImportObjectAction extends AbstractBridgeRepositoryAction {
+	private static final String IMPORT_ACTIONSET_ID = "org.talend.repository.items.importexport.actionSet";
+	private static final String IMPORT_ACTION_ID = "org.talend.repository.items.importexport.actions.ImportItems";
 
-    private static Log log = LogFactory.getLog(ExportItemsAction.class);
+	private static AContextualAction cAction = getImportAction();
+	private static AContextualAction getImportAction() {
+		IExtensionPoint point = Platform.getExtensionRegistry()
+				.getExtensionPoint("org.eclipse.ui.actionSets");
+		if (null == point) {
+			return null;
+		}
+		for (IExtension ext : point.getExtensions()) {
+			IConfigurationElement[] configurationElements = ext
+					.getConfigurationElements();
+			for (IConfigurationElement ce : configurationElements) {
+				String id = ce.getAttribute("id"); //$NON-NLS-1$
+				if (IMPORT_ACTIONSET_ID.equals(id)) {
+					IConfigurationElement[] actions = ce.getChildren("action");
+					for (IConfigurationElement cle : actions) {
+						if (IMPORT_ACTION_ID.equals(cle.getAttribute("id"))) {
+							try {
+								return (AContextualAction) cle
+										.createExecutableExtension("class"); //$NON-NLS-1$
+							} catch (Exception e) {
+								e.printStackTrace();
+							}
+						}
+					}
+				}
+			}
+		}
+		return null;
+	}
 
-    private MDMRepositoryView view = null;
+	public ImportObjectAction() {
+		super(cAction);
+		if (null == cAction) {
+			throw new RuntimeException(
+					"need (org.talend.repository.items.importexport.ui) plugin");
+		}
+	}
 
-    public ImportObjectAction() {
-        super(Messages.ImportObjectAction_label);
-        setImageDescriptor(ImageCache.getImage(EImage.IMPORT.getPath()));
-    }
+	@Override
+	public String getGroupName() {
+		return GROUP_EXPORT;
+	}
 
-    protected void doRun() {
-
-        try {
-            ISelection selection = null;
-            // if (this.view != null) { // called from ServerView
-            view = MDMRepositoryView.show();
-            selection = view.getCommonViewer().getSelection();
-            // }
-            MDMImportRepositoryItemsWizard wizard = new MDMImportRepositoryItemsWizard((IStructuredSelection) selection);
-            WizardDialog dialog = new WizardDialog(view.getSite().getShell(), wizard);
-            dialog.create();
-            dialog.getShell().setText(Messages.ImportObjectAction_importRepositoryItem);
-            dialog.open();
-        } catch (Exception e) {
-            log.error(e.getMessage(), e);
-            MessageDialog.openError(view.getSite().getShell(), Messages.ImportObjectAction_error,
-                    Messages.ImportObjectAction_hasError + e.getLocalizedMessage());
-        }
-    }
-
-    @Override
-    public String getGroupName() {
-        return GROUP_EXPORT;
-    }
+	@Override
+	protected void doRun() {
+		ISelection selection = getSelectionProvider().getSelection();
+		if (cAction instanceof IWorkbenchWindowActionDelegate) {
+			IWorkbenchWindowActionDelegate actionDelegate = (IWorkbenchWindowActionDelegate) cAction;
+			actionDelegate.selectionChanged(null, selection);
+		}
+		super.doRun();
+		getCommonViewer().refresh();
+	}
 
 }
