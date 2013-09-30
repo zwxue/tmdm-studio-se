1bd5294205db3773fb63e3d11a08745d8876a248
sbliu <sbliu@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-09-30 02:55:26 +0000

    TMDM-5674:erase the poped save dialog when to set browser item view under the condition that the belonged model editor is in dirty.
    git-svn-id: http://talendforge.org/svn/tom/trunk@108766 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.workbench/src/com/amalto/workbench/actions/XSDNewBrowseItemViewAction.java
+++ org.talend.mdm.workbench/src/com/amalto/workbench/actions/XSDNewBrowseItemViewAction.java
@@ -18,7 +18,6 @@ import java.util.List;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.eclipse.jface.action.Action;
-import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.wizard.WizardDialog;
 import org.eclipse.swt.widgets.Event;
@@ -52,15 +51,6 @@ public class XSDNewBrowseItemViewAction extends Action {
 
     @Override
     public void run() {
-        if (page.isDirty()) {
-            boolean save = MessageDialog.openConfirm(page.getSite().getShell(), Messages.SaveResource,
-                    Messages.bind(Messages.modifiedChanges, page.getXObject().getDisplayName()));
-            if (save) {
-                pageSave();
-            } else {
-                return;
-            }
-        }
         IStructuredSelection selection = (IStructuredSelection) page.getTreeViewer().getSelection();
         declList.clear();
         List list = selection.toList();
