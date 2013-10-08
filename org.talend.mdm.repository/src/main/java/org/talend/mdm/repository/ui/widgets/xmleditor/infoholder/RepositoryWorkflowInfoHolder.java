1732f9dd59858f8fcaaf0354aa423abfd6dfdd3d
cgpiao <cgpiao@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-10-08 03:49:59 +0000

    TMDM-6386 : use diagram first pool name instead of workflow label name
    git-svn-id: http://talendforge.org/svn/tom/trunk@108936 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.repository/src/main/java/org/talend/mdm/repository/ui/widgets/xmleditor/infoholder/RepositoryWorkflowInfoHolder.java
+++ org.talend.mdm.repository/src/main/java/org/talend/mdm/repository/ui/widgets/xmleditor/infoholder/RepositoryWorkflowInfoHolder.java
@@ -12,11 +12,19 @@
 // ============================================================================
 package org.talend.mdm.repository.ui.widgets.xmleditor.infoholder;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 import java.util.LinkedList;
 import java.util.List;
 
 import org.apache.log4j.Logger;
-import org.talend.core.model.properties.Property;
+import org.bonitasoft.studio.model.process.Element;
+import org.bonitasoft.studio.model.process.MainProcess;
+import org.bonitasoft.studio.model.process.Pool;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.xmi.XMIResource;
+import org.eclipse.emf.ecore.xmi.impl.XMIResourceImpl;
+import org.talend.core.model.properties.ReferenceFileItem;
 import org.talend.core.model.repository.IRepositoryViewObject;
 import org.talend.mdm.repository.core.IServerObjectRepositoryType;
 import org.talend.mdm.repository.utils.RepositoryResourceUtil;
@@ -30,22 +38,44 @@ public class RepositoryWorkflowInfoHolder extends RepositoryExternalInfoHolder<W
     public RepositoryWorkflowInfoHolder() {
     }
 
-    @Override
-    public WorkflowInfo[] getExternalInfo() {
+	@Override
+	public WorkflowInfo[] getExternalInfo() {
 
-        List<WorkflowInfo> results = new LinkedList<WorkflowInfo>();
-        List<IRepositoryViewObject> viewObjs = RepositoryResourceUtil
-                .findAllViewObjects(IServerObjectRepositoryType.TYPE_WORKFLOW);
+		List<WorkflowInfo> results = new LinkedList<WorkflowInfo>();
+		List<IRepositoryViewObject> viewObjs = RepositoryResourceUtil
+				.findAllViewObjects(IServerObjectRepositoryType.TYPE_WORKFLOW);
 
-        for (IRepositoryViewObject viewObj : viewObjs) {
-            Property prop = viewObj.getProperty();
-            WorkflowInfo workflowInfo = new WorkflowInfo(prop.getLabel(), prop.getVersion());
-            results.add(workflowInfo);
-        }
+		for (IRepositoryViewObject viewObj : viewObjs) {
+			ReferenceFileItem item = (ReferenceFileItem) viewObj.getProperty()
+					.getItem().getReferenceResources().get(0);
+			XMIResource resource = new XMIResourceImpl();
+			InputStream stream = new ByteArrayInputStream(item.getContent()
+					.getInnerContent());
+			try {
+				resource.load(stream, null);
+				for (EObject obj : resource.getContents()) {
+					if (obj instanceof MainProcess) {
+						MainProcess mainProcess = (MainProcess) obj;
+						for (Element element : mainProcess.getElements()) {
+							if (element instanceof Pool) {
+								Pool pool = ((Pool) element);
+								WorkflowInfo workflowInfo = new WorkflowInfo(
+										mainProcess.getLabel(), pool.getName(),
+										mainProcess.getVersion());
+								results.add(workflowInfo);
+								break;
+							}
+						}
+					}
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
 
-        return results.toArray(new WorkflowInfo[0]);
+		return results.toArray(new WorkflowInfo[0]);
 
-    }
+	}
 
     @Override
     public String getId() {
--- org.talend.mdm.workbench/src/com/amalto/workbench/widgets/xmleditor/util/WorkflowInfo.java
+++ org.talend.mdm.workbench/src/com/amalto/workbench/widgets/xmleditor/util/WorkflowInfo.java
@@ -6,11 +6,20 @@ public class WorkflowInfo {
 
     private String version = "";//$NON-NLS-1$
 
+    private String poolName;
+    
     public WorkflowInfo(String name, String version) {
         this.name = name;
+        this.poolName = name;
         this.version = version;
     }
 
+    public WorkflowInfo(String name, String poolName ,String version) {
+    	this.name = name;
+    	this.poolName = poolName;
+        this.version = version;
+    }
+    
     public String getName() {
         return name;
     }
@@ -26,5 +35,13 @@ public class WorkflowInfo {
     public void setVersion(String version) {
         this.version = version;
     }
+
+	public String getPoolName() {
+		return poolName;
+	}
+
+	public void setPoolName(String poolName) {
+		this.poolName = poolName;
+	}
 
 }
