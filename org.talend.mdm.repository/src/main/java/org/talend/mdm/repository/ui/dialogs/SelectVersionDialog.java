38f42cd46e07dbd53336d8c095e56c1b7093ab0c
hbhong <hbhong@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-12-23 09:25:49 +0000

    merge r112444 from trunk to branch 5.3 TMDM-6843: Make the default "OK" button to disable status of SelectVersionDialog
    git-svn-id: http://talendforge.org/svn/tom/branches/branch-5_3@112446 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.repository/src/main/java/org/talend/mdm/repository/ui/dialogs/SelectVersionDialog.java
+++ org.talend.mdm.repository/src/main/java/org/talend/mdm/repository/ui/dialogs/SelectVersionDialog.java
@@ -13,6 +13,9 @@
 package org.talend.mdm.repository.ui.dialogs;
 
 import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.jface.dialogs.IDialogConstants;
+import org.eclipse.jface.viewers.ISelectionChangedListener;
+import org.eclipse.jface.viewers.SelectionChangedEvent;
 import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
@@ -25,46 +28,65 @@ import org.talend.core.model.repository.RepositoryObject;
 import org.talend.designer.core.ui.views.jobsettings.tabs.ProcessVersionComposite;
 import org.talend.repository.model.RepositoryNode;
 
-
 /**
- * DOC Administrator  class global comment. Detailled comment
+ * DOC Administrator class global comment. Detailled comment
  */
 public class SelectVersionDialog extends Dialog {
+
     private IRepositoryViewObject viewObj;
+
     private String title;
+
     private ProcessVersionComposite versionListComposite;
+
     /**
      * DOC Administrator SelectVersionDialog constructor comment.
+     * 
      * @param parentShell
      */
     public SelectVersionDialog(Shell parentShell, String title, IRepositoryViewObject viewObject) {
         super(parentShell);
-        this.title=title;
-        this.viewObj=viewObject;
+        this.title = title;
+        this.viewObj = viewObject;
     }
-    
+
+    @Override
     protected Control createDialogArea(Composite parent) {
         parent.getShell().setText(title);
         Composite composite = (Composite) super.createDialogArea(parent);
         composite.setLayoutData(new GridData(GridData.FILL_BOTH));
         composite.setLayout(new GridLayout());
         versionListComposite = new ProcessVersionComposite(composite, SWT.NULL, null, viewObj);
-        GridData gd=new GridData(GridData.FILL_BOTH);
-        gd.heightHint=150;
-        gd.widthHint=450;
+        GridData gd = new GridData(GridData.FILL_BOTH);
+        gd.heightHint = 150;
+        gd.widthHint = 450;
         versionListComposite.setLayoutData(gd);
-        
+        versionListComposite.getTableViewer().addSelectionChangedListener(new ISelectionChangedListener() {
+
+            public void selectionChanged(SelectionChangedEvent event) {
+                getButton(IDialogConstants.OK_ID).setEnabled(true);
+            }
+
+        });
         versionListComposite.refresh();
-        
+
         return composite;
     }
-    public IRepositoryViewObject getSelection(){
-        StructuredSelection selection = (StructuredSelection)versionListComposite.getSelection();
-        if(selection.getFirstElement() instanceof RepositoryNode){
+
+    public IRepositoryViewObject getSelection() {
+        StructuredSelection selection = (StructuredSelection) versionListComposite.getSelection();
+        if (selection.getFirstElement() instanceof RepositoryNode) {
             RepositoryNode node = (RepositoryNode) selection.getFirstElement();
             RepositoryObject repositoryObj = new RepositoryObject(node.getObject().getProperty());
             return repositoryObj;
-        }            
+        }
         return null;
     }
+
+    @Override
+    protected void createButtonsForButtonBar(Composite parent) {
+        super.createButtonsForButtonBar(parent);
+        getButton(IDialogConstants.OK_ID).setEnabled(false);
+    }
+
 }
