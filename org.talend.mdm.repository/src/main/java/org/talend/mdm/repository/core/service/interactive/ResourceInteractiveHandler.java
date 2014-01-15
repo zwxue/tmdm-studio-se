ff4839a970cb36bd48bd90af5e42aaac26460fdf
sbliu <sbliu@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2014-01-15 09:26:10 +0000

    TMDM-6867:encode the image file's name that will be removed from server.
    git-svn-id: http://talendforge.org/svn/tom/branches/branch-5_3@113423 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.repository/src/main/java/org/talend/mdm/repository/core/service/interactive/ResourceInteractiveHandler.java
+++ org.talend.mdm.repository/src/main/java/org/talend/mdm/repository/core/service/interactive/ResourceInteractiveHandler.java
@@ -12,6 +12,8 @@
 // ============================================================================
 package org.talend.mdm.repository.core.service.interactive;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.rmi.RemoteException;
 
 import org.apache.log4j.Logger;
@@ -65,7 +67,7 @@ public class ResourceInteractiveHandler extends AbstractInteractiveHandler {
     private boolean processImage(AbstractDeployCommand cmd, boolean deleteFile) {
         IRepositoryViewObject viewObj = cmd.getViewObject();
         MDMServerDef serverDef = cmd.getServerDef();
-        String uripre = serverDef.getProtocol() + serverDef.getHost() + ":" + serverDef.getPort(); //$NON-NLS-1$ 
+        String uripre = serverDef.getProtocol() + serverDef.getHost() + ":" + serverDef.getPort(); //$NON-NLS-1$
         Item item = viewObj.getProperty().getItem();
         WSResourceE rs = ((WSResourceItem) item).getResource();
         String fileExtension = rs.getFileExtension();
@@ -77,7 +79,14 @@ public class ResourceInteractiveHandler extends AbstractInteractiveHandler {
             String url = "/imageserver/secure/ImageUploadServlet";//$NON-NLS-1$
             if (deleteFile) {
                 if (imageCatalog != null) {
-                    String uri = "upload/" + imageCatalog + '/' + name;//$NON-NLS-1$
+                    String encodedName = name;
+                    try {
+                        encodedName = URLEncoder.encode(name, "utf-8"); //$NON-NLS-1$
+                    } catch (UnsupportedEncodingException e) {
+                        log.error(e.getMessage(), e);
+                    }
+
+                    String uri = "upload/" + imageCatalog + '/' + encodedName;//$NON-NLS-1$
                     url = "/imageserver/secure/ImageDeleteServlet?uri=" + uri;//$NON-NLS-1$
                 } else {
                     return false;
