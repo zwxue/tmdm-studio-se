861d315da354f189184a2a14eec67e0a78d28ffa
pbabin <pbabin@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2014-01-28 04:59:21 +0000

    TMDM-6829: Reverting r113529 (was Remove JCA connectors from build)
    git-svn-id: http://talendforge.org/svn/tom/trunk@113741 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.ws/src/com/amalto/workbench/webservices/XtentisPort.java
+++ org.talend.mdm.ws/src/com/amalto/workbench/webservices/XtentisPort.java
@@ -608,6 +608,13 @@ public interface XtentisPort {
         WSRecoverDroppedItem wsRecoverDroppedItem
     );
 
+    @WebResult(name = "WSConnectorInteractionResponse", targetNamespace = "urn-com-amalto-xtentis-webservice", partName = "wsConnectorInteractionResponse")
+    @WebMethod
+    public WSConnectorInteractionResponse connectorInteraction(
+        @WebParam(partName = "wsConnectorInteraction", name = "WSConnectorInteraction", targetNamespace = "urn-com-amalto-xtentis-webservice")
+        WSConnectorInteraction wsConnectorInteraction
+    );
+
     @WebResult(name = "WSString", targetNamespace = "urn-com-amalto-xtentis-webservice", partName = "dummy")
     @WebMethod
     public WSString ping(
