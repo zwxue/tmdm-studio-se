bc11e1df3a19b30358ad2fc92ee01bcbb0d6d1a8
sbliu <sbliu@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2014-03-31 07:45:09 +0000

    merge r115735 from trunk to branch 5.4 TMDM-7101:change the way to judge if the transferred string is valid.
    git-svn-id: http://talendforge.org/svn/tom/branches/branch-5_4@115738 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.mdm.repository/src/main/java/org/talend/mdm/repository/utils/ValidateUtil.java
+++ org.talend.mdm.repository/src/main/java/org/talend/mdm/repository/utils/ValidateUtil.java
@@ -16,11 +16,21 @@ import java.util.regex.Pattern;
 
 public class ValidateUtil {
 
+    /*
+     * the character '-' is allowed,and the original pattern regex = "\\w*(-|\\w*)+\\w+";
+     */
     public static boolean matchCommonRegex(String newText) {
-        String regex = "\\w*(-|\\w*)+\\w+";//$NON-NLS-1$
         String tailRegex = ".*\\w+";//$NON-NLS-1$
+        boolean match = Pattern.compile(tailRegex).matcher(newText).matches();
+        if (match) {
+            String replace = newText.replace("-", ""); //$NON-NLS-1$//$NON-NLS-2$
+            String pattern_hasInvalidChar = "\\w*\\W+\\w+"; //$NON-NLS-1$
+            if (!Pattern.compile(pattern_hasInvalidChar).matcher(replace).matches()) {
+                return true;
+            }
+        }
 
-        return matches(regex, tailRegex, newText);
+        return false;
     }
 
     public static boolean matchCustomFormRegex(String newText) {
