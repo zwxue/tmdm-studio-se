c330e8b73291d318a51eeb09b757142fabfa2dde
sbliu <sbliu@talend.com>
2014-11-24 18:02:46 +0800

    TMDM-7399: fix NPE when unit test runs.

--- test/plugins/org.talend.mdm.repository.test/src/main/java/org/talend/mdm/repository/ui/actions/trigger/NewTriggerActionTest.java
+++ test/plugins/org.talend.mdm.repository.test/src/main/java/org/talend/mdm/repository/ui/actions/trigger/NewTriggerActionTest.java
@@ -4,10 +4,13 @@ import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.jface.resource.JFaceResources;
 import org.junit.Rule;
 import org.junit.Test;
+import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.rule.PowerMockRule;
 import org.powermock.reflect.Whitebox;
@@ -18,6 +21,9 @@ import org.talend.core.runtime.CoreRuntimePlugin;
 import org.talend.mdm.repository.extension.RepositoryNodeConfigurationManager;
 import org.talend.mdm.repository.model.mdmproperties.ContainerItem;
 import org.talend.mdm.repository.model.mdmproperties.MdmpropertiesFactory;
+import org.talend.mdm.repository.model.mdmproperties.WSRoutingRuleItem;
+import org.talend.mdm.repository.model.mdmproperties.impl.MdmpropertiesFactoryImpl;
+import org.talend.mdm.repository.model.mdmproperties.impl.WSRoutingRuleItemImpl;
 import org.talend.mdm.repository.ui.actions.AbstractSimpleAddActionTest;
 import org.talend.mdm.repository.utils.RepositoryResourceUtil;
 import org.talend.repository.ProjectManager;
@@ -27,7 +33,7 @@ import com.amalto.workbench.image.ImageCache;
 
 @PrepareForTest({ ImageDescriptor.class, JFaceResources.class, ImageCache.class, ItemState.class, CoreRuntimePlugin.class,
         ProjectManager.class, RepositoryNodeConfigurationManager.class, ProxyRepositoryFactory.class,
-        RepositoryResourceUtil.class, ExAdapterManager.class })
+        RepositoryResourceUtil.class, ExAdapterManager.class, MdmpropertiesFactoryImpl.class })
 public class NewTriggerActionTest extends AbstractSimpleAddActionTest {
 
     @Rule
@@ -46,6 +52,16 @@ public class NewTriggerActionTest extends AbstractSimpleAddActionTest {
         ItemState itemState = mock(ItemState.class);
         when(mockContainerItem.getState()).thenReturn(itemState);
         when(mockContainerItem.getState().getPath()).thenReturn(""); //$NON-NLS-1$
+
+        WSRoutingRuleItem item = MdmpropertiesFactory.eINSTANCE.createWSRoutingRuleItem();
+        WSRoutingRuleItem spyRoutingRuleItem = spy(item);
+        Resource mockResource = mock(Resource.class);
+        ResourceSet mockResourceSet = mock(ResourceSet.class);
+        when(mockResource.getResourceSet()).thenReturn(mockResourceSet);
+        when(spyRoutingRuleItem.eResource()).thenReturn(mockResource);
+        PowerMockito.whenNew(WSRoutingRuleItemImpl.class).withNoArguments()
+                .thenReturn((WSRoutingRuleItemImpl) spyRoutingRuleItem);
+
         // run
         Item addedItem = spyAction.createServerObject("abc"); //$NON-NLS-1$
         assertThat(addedItem, notNullValue());
