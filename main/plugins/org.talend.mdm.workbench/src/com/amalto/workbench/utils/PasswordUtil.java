2e0c625e142bcb30647d2b028dbe12a3a0697bbc
hbhong <hbhong@talend.com>
2014-11-26 18:04:50 +0800

    TMDM-7974: Using "CryptoHelper" class to encrypt password in branch 6.0

--- main/plugins/org.talend.mdm.workbench/src/com/amalto/workbench/utils/PasswordUtil.java
+++ main/plugins/org.talend.mdm.workbench/src/com/amalto/workbench/utils/PasswordUtil.java
@@ -14,7 +14,6 @@ package com.amalto.workbench.utils;
 
 import org.apache.commons.codec.binary.Base64;
 import org.apache.log4j.Logger;
-import org.talend.commons.utils.PasswordEncryptUtil;
 import org.talend.utils.security.CryptoHelper;
 
 public class PasswordUtil {
@@ -30,10 +29,6 @@ public class PasswordUtil {
         if (algorithm != null && algorithm.equals(ALGORITHM_COMMON)) {
             try {
                 String decryptedPassword = CryptoHelper.getDefault().decrypt(encodedPassword);
-                // failed in decrypting and try to old implement
-                if (decryptedPassword == null) {
-                    decryptedPassword = PasswordEncryptUtil.decryptPassword(encodedPassword);
-                }
                 return decryptedPassword;
             } catch (Exception e) {
                 log.error(e.getMessage(), e);
