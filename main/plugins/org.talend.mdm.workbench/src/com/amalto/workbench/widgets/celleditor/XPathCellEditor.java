856d05682cbc4c9283e8c92215fa21a3133e0aa7
hbhong <hbhong@talend.com>
2015-06-24 13:49:44 +0800

    TMDM-8604: Fixed user can input illegal character in xpath column when defining a validation rule

--- main/plugins/org.talend.mdm.workbench/src/com/amalto/workbench/widgets/celleditor/XPathCellEditor.java
+++ main/plugins/org.talend.mdm.workbench/src/com/amalto/workbench/widgets/celleditor/XPathCellEditor.java
@@ -12,6 +12,9 @@
 // ============================================================================
 package com.amalto.workbench.widgets.celleditor;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import org.eclipse.jface.window.Window;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
@@ -21,66 +24,86 @@ import com.amalto.workbench.models.infoextractor.IAllDataModelHolder;
 
 public class XPathCellEditor extends EditableDialogCellEditor {
 
-	public interface IXPathUpdate {
+    public interface IXPathUpdate {
 
-		public String updateXPath(String xpath);
-	}
+        public String updateXPath(String xpath);
+    }
 
-	private IAllDataModelHolder allDataModelHolder;
+    private IAllDataModelHolder allDataModelHolder;
 
-	private IXPathUpdate xpathUpdate = null;
+    private IXPathUpdate xpathUpdate = null;
 
-	private boolean lock;
+    private boolean lock;
 
-	public XPathCellEditor(Composite parent,
-			IAllDataModelHolder allDataModelHolder) {
-		super(parent);
-		this.allDataModelHolder = allDataModelHolder;
-	}
+    public XPathCellEditor(Composite parent, IAllDataModelHolder allDataModelHolder) {
+        super(parent);
+        this.allDataModelHolder = allDataModelHolder;
+    }
 
-	public XPathCellEditor(Composite parent,
-			IAllDataModelHolder allDataModelHolder, IXPathUpdate xpathUpdate) {
-		this(parent, allDataModelHolder);
-		this.xpathUpdate = xpathUpdate;
-	}
+    public XPathCellEditor(Composite parent, IAllDataModelHolder allDataModelHolder, IXPathUpdate xpathUpdate) {
+        this(parent, allDataModelHolder);
+        this.xpathUpdate = xpathUpdate;
+    }
 
-	public IAllDataModelHolder getAllDataModelHolder() {
-		return allDataModelHolder;
-	}
+    public IAllDataModelHolder getAllDataModelHolder() {
+        return allDataModelHolder;
+    }
 
-	public void setAllDataModelHolder(IAllDataModelHolder allDataModelHolder) {
+    public void setAllDataModelHolder(IAllDataModelHolder allDataModelHolder) {
 
-		if (allDataModelHolder == null)
-			return;
+        if (allDataModelHolder == null) {
+            return;
+        }
 
-		this.allDataModelHolder = allDataModelHolder;
-	}
+        this.allDataModelHolder = allDataModelHolder;
+    }
 
-	@Override
-	protected Object openDialogBox(Control cellEditorWindow) {
+    @Override
+    protected Object openDialogBox(Control cellEditorWindow) {
 
-		SelectXPathDialog dialog = new SelectXPathDialog(
-				cellEditorWindow.getShell(), allDataModelHolder,
-				allDataModelHolder.getDefaultDataModel(),
-				allDataModelHolder.getDefaultEntity());
+        SelectXPathDialog dialog = new SelectXPathDialog(cellEditorWindow.getShell(), allDataModelHolder,
+                allDataModelHolder.getDefaultDataModel(), allDataModelHolder.getDefaultEntity());
 
-		if (dialog.open() != Window.OK)
-			return null;
-		dialog.setLock(lock);
-		String xpath = dialog.getSelectedXPath();
-		if (xpathUpdate != null) {
-			return xpathUpdate.updateXPath(xpath);
-		}
-		return xpath;
+        if (dialog.open() != Window.OK) {
+            return null;
+        }
+        dialog.setLock(lock);
+        String xpath = dialog.getSelectedXPath();
+        if (xpathUpdate != null) {
+            return xpathUpdate.updateXPath(xpath);
+        }
+        return xpath;
 
-	}
+    }
 
-	public boolean isLock() {
-		return lock;
-	}
+    public boolean isLock() {
+        return lock;
+    }
 
-	public void setLock(boolean lock) {
-		this.lock = lock;
-	}
+    public void setLock(boolean lock) {
+        this.lock = lock;
+    }
+
+    @Override
+    protected boolean validate() {
+        String newValue = txtEdit.getText().trim();
+        return validateXpath(newValue);
+    }
+
+    private boolean validateXpath(String value) {
+        if (value.length() == 0) {
+            return true;
+        }
+        Pattern pattern = Pattern.compile("\\.|([a-zA-Z]|\\d|\\/)+"); //$NON-NLS-1$
+        Matcher matcher = pattern.matcher(value);
+        boolean matches = matcher.matches();
+
+        return matches;
+    }
+
+    @Override
+    protected void restoreValue() {
+        updateContents((String) doGetValue());
+    }
 
 }
