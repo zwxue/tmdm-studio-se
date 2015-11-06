59822ac49d5fe7d1161835b967246de909d40e59
Haiting Yin <htyin@talend.com>
2015-11-06 18:10:47 +0800

    TMDM-7714  MDM.getLanguageVariant () doesn't return the expected result when the field to extract contains the character ":".

---
+++ test/plugins/org.talend.designer.routines.mdmprovider.test/src/routines/MDMTest.java
@@ -0,0 +1,59 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2015 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package routines;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+import org.junit.Test;
+
+
+/**
+ * DOC class global comment. Detailled comment
+ */
+
+@SuppressWarnings("nls")
+public class MDMTest {
+
+    @Test
+    public void testGetLanguageVariant() {
+        String result1 = MDM.getLanguageVariant("EN", "[EN:US :: Las Angeles][ZH:China : Beijing :]");
+        assertEquals("US :: Las Angeles", result1);
+
+        String result2 = MDM.getLanguageVariant("ZH", "[EN:US :: Las Angeles][ZH:China : Beijing :]");
+        assertEquals("China : Beijing :", result2);
+
+        String result3 = MDM.getLanguageVariant("EN", "[EN:Product&Store][ZH:Product#Store]");
+        assertEquals("Product&Store", result3);
+
+        String result4 = MDM.getLanguageVariant("ZH", "[EN:Product&Store][ZH:Product#Store]");
+        assertEquals("Product#Store", result4);
+
+        String result5 = MDM.getLanguageVariant("FR", "[EN: Las Angeles][ZH: Beijing]");
+        assertNull(result5);
+
+        String result6 = MDM.getLanguageVariant("EN", "[EN Las Angeles][ZH: Beijing]");
+        assertNull(result6);
+    }
+
+    @Test
+    public void testSetLanguageVariant() {
+        String result7 = MDM.setLanguageVariant("FR", "France#French", "China&Chinese", "ZH", true);
+        assertEquals("[FR:France#French][ZH:China&Chinese]", result7);
+
+        String result8 = MDM.setLanguageVariant("FR", "France#French", "England:English", null, true);
+        assertEquals("[EN:England:English][FR:France#French]", result8);
+    }
+
+}
+
