ea9987b066d7d4aa55d54b8f4aff63df9410aec0
sbliu <sbliu@talend.com>
2016-01-19 18:26:55 +0800

    TMDM-9103:fix unit test.

--- test/plugins/org.talend.mdm.repository.test/src/main/java/org/talend/mdm/repository/ui/actions/datamodel/NewDataModelActionTest.java
+++ test/plugins/org.talend.mdm.repository.test/src/main/java/org/talend/mdm/repository/ui/actions/datamodel/NewDataModelActionTest.java
@@ -2,22 +2,22 @@ package org.talend.mdm.repository.ui.actions.datamodel;
 
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
+import static org.mockito.Matchers.*;
 import static org.mockito.Mockito.*;
 
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.jface.resource.JFaceResources;
-import org.junit.Rule;
 import org.junit.Test;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.rule.PowerMockRule;
 import org.powermock.reflect.Whitebox;
 import org.talend.core.model.properties.Item;
 import org.talend.core.model.properties.ItemState;
 import org.talend.core.repository.model.ProxyRepositoryFactory;
 import org.talend.core.runtime.CoreRuntimePlugin;
+import org.talend.mdm.repository.core.service.IMatchRuleMapInfoService;
 import org.talend.mdm.repository.extension.RepositoryNodeConfigurationManager;
 import org.talend.mdm.repository.model.mdmproperties.ContainerItem;
 import org.talend.mdm.repository.model.mdmproperties.MdmpropertiesFactory;
@@ -26,24 +26,32 @@ import org.talend.mdm.repository.model.mdmproperties.impl.MdmpropertiesFactoryIm
 import org.talend.mdm.repository.model.mdmproperties.impl.WSDataModelItemImpl;
 import org.talend.mdm.repository.ui.actions.AbstractSimpleAddActionTest;
 import org.talend.mdm.repository.utils.RepositoryResourceUtil;
+import org.talend.mdm.repository.utils.ServiceUtil;
 import org.talend.repository.ProjectManager;
 
+import com.amalto.workbench.MDMWorbenchPlugin;
 import com.amalto.workbench.exadapter.ExAdapterManager;
 import com.amalto.workbench.image.ImageCache;
 
 @PrepareForTest({ ImageDescriptor.class, JFaceResources.class, ImageCache.class, ItemState.class, CoreRuntimePlugin.class,
         ProjectManager.class, RepositoryNodeConfigurationManager.class, ProxyRepositoryFactory.class,
-        RepositoryResourceUtil.class, ExAdapterManager.class, MdmpropertiesFactoryImpl.class })
+        RepositoryResourceUtil.class, ExAdapterManager.class, MdmpropertiesFactoryImpl.class,
+        MDMWorbenchPlugin.class, ServiceUtil.class })
 public class NewDataModelActionTest extends AbstractSimpleAddActionTest {
-
-    @Rule
-    public PowerMockRule powerMockRule = new PowerMockRule();
 
     @Test
     public void testCreateServerObject() throws Exception {
         //
         ContainerItem newItem = MdmpropertiesFactory.eINSTANCE.createContainerItem();
         ContainerItem mockContainerItem = spy(newItem);
+
+        PowerMockito.mockStatic(MDMWorbenchPlugin.class);
+        when(MDMWorbenchPlugin.getImageDescriptor(anyString())).thenReturn(mock(ImageDescriptor.class));
+
+        IMatchRuleMapInfoService mapInfoService = mock(IMatchRuleMapInfoService.class);
+        PowerMockito.mockStatic(ServiceUtil.class);
+        PowerMockito.when(ServiceUtil.getService(IMatchRuleMapInfoService.class)).thenReturn(mapInfoService);
+
         // new action
         NewDataModelAction action = new NewDataModelAction();
         NewDataModelAction spyAction = spy(action);
