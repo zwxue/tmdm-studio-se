b85047768091283ef2bd7ca45eb9ae4794963383
sbliu <sbliu@talend.com>
2016-02-24 18:53:39 +0800

    TMDM-9176: fix project path to build right file path under git project.

--- main/plugins/org.talend.mdm.repository/src/main/java/org/talend/mdm/repository/ui/dialogs/SelectImportedModulesDialog2.java
+++ main/plugins/org.talend.mdm.repository/src/main/java/org/talend/mdm/repository/ui/dialogs/SelectImportedModulesDialog2.java
@@ -15,11 +15,14 @@ package org.talend.mdm.repository.ui.dialogs;
 import java.net.URL;
 import java.util.List;
 
+import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.swt.widgets.Shell;
 import org.talend.core.model.properties.ItemState;
+import org.talend.core.model.properties.Project;
+import org.talend.core.model.properties.Property;
 import org.talend.core.model.repository.IRepositoryViewObject;
 import org.talend.mdm.repository.core.IServerObjectRepositoryType;
 import org.talend.mdm.repository.core.service.RepositoryQueryService;
@@ -27,6 +30,7 @@ import org.talend.mdm.repository.core.service.RepositoryWebServiceAdapter;
 import org.talend.mdm.repository.model.mdmmetadata.MDMServerDef;
 import org.talend.mdm.repository.utils.RepositoryResourceUtil;
 import org.talend.mdm.workbench.serverexplorer.ui.dialogs.SelectServerDefDialog;
+import org.talend.repository.ProjectManager;
 
 import com.amalto.workbench.dialogs.SelectImportedModulesDialog;
 import com.amalto.workbench.models.TreeObject;
@@ -71,7 +75,7 @@ public class SelectImportedModulesDialog2 extends SelectImportedModulesDialog {
         if (null == path) {
             return null;
         }
-        if (path.startsWith("type:")) {
+        if (path.startsWith("type:")) { //$NON-NLS-1$
             String modelName = path.substring(5);
             IRepositoryViewObject rvobject = RepositoryResourceUtil.findViewObjectByName(
                     IServerObjectRepositoryType.TYPE_DATAMODEL, modelName);
@@ -81,12 +85,14 @@ public class SelectImportedModulesDialog2 extends SelectImportedModulesDialog {
             String prjLabel = rvobject.getProjectLabel();
 
             String parentPath = buildParentPath(rvobject);
-            if (!parentPath.startsWith("/")) {
-                parentPath = "/" + parentPath;
+            if (!parentPath.startsWith("/")) { //$NON-NLS-1$
+                parentPath = "/" + parentPath; //$NON-NLS-1$
             }
             String sep = "" + IPath.SEPARATOR; //$NON-NLS-1$
 
-            String fileName = "file:///" + ResourcesPlugin.getWorkspace().getRoot().getLocation().toOSString() + sep
+            IPath plocation = getProjectLocation(rvobject);
+
+            String fileName = "file:///" + plocation.toOSString() + sep //$NON-NLS-1$
                     + prjLabel.toUpperCase() + sep
                     + "MDM" + sep + "datamodel" + parentPath + rvobject.getLabel() + "_" + rvobject.getVersion() + ".xsd"; //$NON-NLS-1$//$NON-NLS-2$//$NON-NLS-3$ //$NON-NLS-4$
             return super.getSourceURL(fileName);
@@ -105,5 +111,13 @@ public class SelectImportedModulesDialog2 extends SelectImportedModulesDialog {
         }
 
         return path;
+    }
+
+    private IPath getProjectLocation(IRepositoryViewObject viewObj) {
+        Property property = viewObj.getProperty();
+        Project project = ProjectManager.getInstance().getProject(property);
+        IProject eclipseProject = ResourcesPlugin.getWorkspace().getRoot().getProject(project.getTechnicalLabel());
+        IPath location = eclipseProject.getLocation().removeLastSegments(1);
+        return location;
     }
 }
