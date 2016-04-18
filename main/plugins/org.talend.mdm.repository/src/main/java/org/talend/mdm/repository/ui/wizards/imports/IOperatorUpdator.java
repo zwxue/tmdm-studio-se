80d28deca389f407ddfb421ba3654ce87a8e5452
sbliu <sbliu@talend.com>
2016-04-18 15:38:41 +0800

    TMDM-9322: check and update operator in data model,role and view when importing items from local or open old workspace.

---
+++ main/plugins/org.talend.mdm.repository/src/main/java/org/talend/mdm/repository/ui/wizards/imports/IOperatorUpdator.java
@@ -0,0 +1,24 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2016 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.mdm.repository.ui.wizards.imports;
+
+import org.talend.core.model.properties.Item;
+
+/**
+ * created by liusongbo on Apr 14, 2016
+ *
+ */
+public interface IOperatorUpdator {
+
+    public abstract boolean updateConditionOperator(Item item);
+}
