b1945c9d912ae1042669d78a27b49a19ae376501
sbliu <sbliu@talend.com>
2016-12-19 10:52:25 +0800

    TMDM-8795: Add or correct Copyright. (#42)

--- main/plugins/org.talend.mdm.repository/src/main/java/org/talend/mdm/repository/core/impl/jobmodel/TISProcessNodeResourceProvider.java
+++ main/plugins/org.talend.mdm.repository/src/main/java/org/talend/mdm/repository/core/impl/jobmodel/TISProcessNodeResourceProvider.java
@@ -1,22 +1,13 @@
 // ============================================================================
 //
-// Talend Community Edition
-//
 // Copyright (C) 2006-2016 Talend Inc. - www.talend.com
 //
-// This library is free software; you can redistribute it and/or
-// modify it under the terms of the GNU Lesser General Public
-// License as published by the Free Software Foundation; either
-// version 2.1 of the License, or (at your option) any later version.
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
 //
-// This library is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-// Lesser General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program; if not, write to the Free Software
-// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
 //
 // ============================================================================
 package org.talend.mdm.repository.core.impl.jobmodel;
--- main/plugins/org.talend.mdm.workbench/src/com/amalto/workbench/compare/Utilities.java
+++ main/plugins/org.talend.mdm.workbench/src/com/amalto/workbench/compare/Utilities.java
@@ -1,10 +1,15 @@
-/*******************************************************************************
- * Copyright (c) 2000, 2008 IBM Corporation and others. All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0 which accompanies this distribution, and is
- * available at http://www.eclipse.org/legal/epl-v10.html
- * 
- * Contributors: IBM Corporation - initial API and implementation
- *******************************************************************************/
+// ============================================================================
+//
+// Copyright (C) 2006-2016 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
 package com.amalto.workbench.compare;
 
 import java.io.BufferedReader;
