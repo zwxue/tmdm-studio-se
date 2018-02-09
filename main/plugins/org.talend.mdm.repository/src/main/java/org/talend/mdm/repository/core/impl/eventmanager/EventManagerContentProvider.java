4c92bceee9309fb189eb234c2529acd3193ec5e9
Irene Wang <iwang@talend.com>
2018-02-09 15:43:41 +0100

    DEVOPS-3791 Update copyright year to 2018

--- main/plugins/org.talend.mdm.repository/src/main/java/org/talend/mdm/repository/core/impl/eventmanager/EventManagerContentProvider.java
+++ main/plugins/org.talend.mdm.repository/src/main/java/org/talend/mdm/repository/core/impl/eventmanager/EventManagerContentProvider.java
@@ -2,7 +2,7 @@
 //
 // Talend Community Edition
 //
-// Copyright (C) 2006-2017 Talend Inc. - www.talend.com
+// Copyright (C) 2006-2018 Talend Inc. - www.talend.com
 //
 // This library is free software; you can redistribute it and/or
 // modify it under the terms of the GNU Lesser General Public
