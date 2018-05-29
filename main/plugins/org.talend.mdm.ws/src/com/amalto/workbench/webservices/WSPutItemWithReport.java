4e1ed13b13f8e06aada8d224aa3e5a69bf5f7484
sbliu <sbliu@talend.com>
2018-05-29 17:39:42 +0800

    TMDM-10163 Before Save Process to return a warning message (#161)
    Update webservice stub class.

--- main/plugins/org.talend.mdm.ws/src/com/amalto/workbench/webservices/WSPutItemWithReport.java
+++ main/plugins/org.talend.mdm.ws/src/com/amalto/workbench/webservices/WSPutItemWithReport.java
@@ -17,7 +17,10 @@ import javax.xml.bind.annotation.XmlType;
  *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
  *       &lt;sequence>
  *         &lt;element name="invokeBeforeSaving" type="{http://www.w3.org/2001/XMLSchema}boolean" minOccurs="0"/>
+ *         &lt;element name="message" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *         &lt;element name="messageType" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
  *         &lt;element name="source" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *         &lt;element name="warningApprovedBeforeSave" type="{http://www.w3.org/2001/XMLSchema}boolean" minOccurs="0"/>
  *         &lt;element name="wsPutItem" type="{http://www.talend.com/mdm}WSPutItem" minOccurs="0"/>
  *       &lt;/sequence>
  *     &lt;/restriction>
@@ -28,15 +31,19 @@ import javax.xml.bind.annotation.XmlType;
  * 
  */
 @XmlAccessorType(XmlAccessType.FIELD)
-@XmlType(name = "WSPutItemWithReport", propOrder = {
-    "invokeBeforeSaving",
-    "source",
-    "wsPutItem"
-})
+@XmlType(name = "WSPutItemWithReport")
 public class WSPutItemWithReport {
 
     protected Boolean invokeBeforeSaving;
+
+    protected String message;
+
+    protected String messageType;
+
     protected String source;
+
+    protected Boolean warningApprovedBeforeSave = false;
+
     protected WSPutItem wsPutItem;
 
     /**
@@ -63,7 +70,7 @@ public class WSPutItemWithReport {
      * @return
      *     possible object is
      *     {@link Boolean }
-     *     
+     * 
      */
     public Boolean isInvokeBeforeSaving() {
         return invokeBeforeSaving;
@@ -75,10 +82,58 @@ public class WSPutItemWithReport {
      * @param value
      *     allowed object is
      *     {@link Boolean }
-     *     
+     * 
      */
     public void setInvokeBeforeSaving(Boolean value) {
         this.invokeBeforeSaving = value;
+    }
+
+    /**
+     * Gets the value of the message property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     * 
+     */
+    public String getMessage() {
+        return message;
+    }
+
+    /**
+     * Sets the value of the message property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     * 
+     */
+    public void setMessage(String value) {
+        this.message = value;
+    }
+
+    /**
+     * Gets the value of the messageType property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     * 
+     */
+    public String getMessageType() {
+        return messageType;
+    }
+
+    /**
+     * Sets the value of the messageType property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     * 
+     */
+    public void setMessageType(String value) {
+        this.messageType = value;
     }
 
     /**
@@ -87,7 +142,7 @@ public class WSPutItemWithReport {
      * @return
      *     possible object is
      *     {@link String }
-     *     
+     * 
      */
     public String getSource() {
         return source;
@@ -99,10 +154,34 @@ public class WSPutItemWithReport {
      * @param value
      *     allowed object is
      *     {@link String }
-     *     
+     * 
      */
     public void setSource(String value) {
         this.source = value;
+    }
+
+    /**
+     * Gets the value of the warningApprovedBeforeSave property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link Boolean }
+     * 
+     */
+    public Boolean isWarningApprovedBeforeSave() {
+        return warningApprovedBeforeSave;
+    }
+
+    /**
+     * Sets the value of the warningApprovedBeforeSave property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link Boolean }
+     * 
+     */
+    public void setWarningApprovedBeforeSave(Boolean value) {
+        this.warningApprovedBeforeSave = value;
     }
 
     /**
@@ -111,7 +190,7 @@ public class WSPutItemWithReport {
      * @return
      *     possible object is
      *     {@link WSPutItem }
-     *     
+     * 
      */
     public WSPutItem getWsPutItem() {
         return wsPutItem;
@@ -123,7 +202,7 @@ public class WSPutItemWithReport {
      * @param value
      *     allowed object is
      *     {@link WSPutItem }
-     *     
+     * 
      */
     public void setWsPutItem(WSPutItem value) {
         this.wsPutItem = value;
